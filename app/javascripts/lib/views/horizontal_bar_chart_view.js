f409a9401c673eebf597bf492e25975b195b70ab
Paolo Zaccagnini <paozac@gmail.com>
2011-05-03 17:45:16 +0200

    converted horizontal bar charts to the new backbone system

--- app/javascripts/lib/views/horizontal_bar_chart_view.js
+++ app/javascripts/lib/views/horizontal_bar_chart_view.js
@@ -1,19 +1,44 @@
 
 var HorizontalBarChartView = BaseChartView.extend({
+  cached_results : null,
+  
   initialize : function() {
     this.initialize_defaults();
   },
+  
   render : function() {
     // SEB: maybe needs a better way to remove jqplot objects.
     //      => possible js memory leak
     $('#current_chart').empty().css('height', this.HEIGHT);
-    InitializeHorizontalBar("current_chart", 
-      this.model.results(), 
-      true, 
-      'PJ', 
-      this.axis_scale(), 
-      this.model.colors(), 
-      this.model.labels());
+      InitializeHorizontalBar(
+        "current_chart",
+        this.results(),
+        true,
+        'PJ',
+        this.axis_scale(),
+        this.model.colors(),
+        this.model.labels()
+      );
+  },
+  
+  // the horizontal graph expects data in this format:
+  // [value, 1], [value, 2], ...
+  // TODO: refactoring - PZ Tue 3 May 2011 17:44:14 CEST
+  results : function() {
+    if (this.cached_results) return this.cached_results;
+    var model_results = this.model.results();    
+    var out = []
+    for(i = 0; i < model_results.length; i++) {
+      out.push([model_results[i][0][1], parseInt(i)+1]);
+    }
+    this.cached_results = out;
+    return out;
+  },
+  
+  axis_scale : function() {
+    var values = _.map(this.results(), function(i){ return i[0]});
+    var max = _.max(values);
+    return [0, max * 1.1];
   }
 });
 

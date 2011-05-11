884bd63c29e65915b9a5099fe39fc5b663befc75
Paolo Zaccagnini <paozac@gmail.com>
2011-05-11 14:15:00 +0200

    turned show_intro_at_least_once into a filter to simplify testing

--- spec/controllers/costs_controller_spec.rb
+++ spec/controllers/costs_controller_spec.rb
@@ -9,4 +9,13 @@ describe CostsController do
       response.should be_success
     end
   end
+
+  describe "GET show" do
+    it "should be successful" do
+      controller.class.skip_before_filter :show_intro_at_least_once
+      get :show
+      response.should be_success
+      response.should render_template('show')
+    end
+  end
 end

e79fe098096ae623a0e4bfcc56229322a7297539
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-05-17 10:22:02 +0200

    policy_bar_chart_view.js refactroings

--- app/javascripts/lib/views/policy_bar_chart_view.js
+++ app/javascripts/lib/views/policy_bar_chart_view.js
@@ -9,20 +9,12 @@ var PolicyBarChartView = BaseChartView.extend({
     //      => possible js memory leak
     $('#current_chart').empty().css('height', this.HEIGHT);
 
-    console.log(this.result_serie1());
-    console.log(this.result_serie2());
-    console.log(this.ticks());
-    console.log(this.model.colors());
-    console.log(this.model.labels());
-    console.log(this.axis_scale());
-    //function InitializePolic  yBar(id,serie1,serie2,ticks,groups,unit,axis_values,colors,labels){
     InitializePolicyBar("current_chart", 
       this.result_serie1(), 
       this.result_serie2(), 
       this.ticks(), 
       this.model.series.length, 
-      // DEBT: not correctly implemented
-      '%', //this.parsed_unit(), 
+      this.parsed_unit(), 
       this.axis_scale(),
       this.model.colors(), 
       this.model.labels());

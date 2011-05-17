33cea4f95a72761aee3fb6440f780834e81b1d1a
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-05-17 10:58:10 +0200

    TRASH: application_controller modules and unused gql methods

--- app/controllers/constraints_controller.rb
+++ app/controllers/constraints_controller.rb
@@ -1,15 +1,5 @@
 class ConstraintsController < ApplicationController
   respond_to :html, :js
-  # before_filter :load_view_settings, :on => [:show]
-  def index
-    respond_to do |format|
-      format.json do
-        render :update do |page|
-          controller.page_update_constraints(page)
-        end
-      end
-    end
-  end
 
   # TODO constraints/1 is called with JS format. Should be HTML
   def show

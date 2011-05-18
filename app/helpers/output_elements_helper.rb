d544707d2d97d2bbd887f6b5bf0733a3a35e0434
Paolo Zaccagnini <paozac@gmail.com>
2011-05-18 16:11:28 +0200

    refactored contraints charts js code, one more popup is working now

--- app/helpers/output_elements_helper.rb
+++ app/helpers/output_elements_helper.rb
@@ -1,12 +1,30 @@
 module OutputElementsHelper
   def output_element_serie(serie)
     s = {      
-      :gquery_key => serie.gquery, # New column!
+      :gquery_key => serie.gquery,
       :color      => serie.converted_color,
       :label      => serie.title_translated,
       :group      => serie.group
     }
 
     "output_element.series.add(#{s.to_json});"
+  end
+  
+  # Used in the constraint popups
+  def js_for_output_element(id, dom_id)
+    element = OutputElement.find(id) rescue nil
+    return unless element
+    
+    chart_options = element.options_for_js.merge(:container => dom_id).to_json
+    series_options = element.allowed_output_element_series.map{|s| output_element_serie(s)}.join
+    
+    out = <<EOJS
+    
+    var output_element = new Chart(#{chart_options});
+    #{series_options}
+    charts.add(output_element);
+    
+EOJS
+    out.html_safe
   end
 end

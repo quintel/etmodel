0c0a23df2edc9c346b686dfc2544726ed4bb6cc0
Paolo Zaccagnini <paozac@gmail.com>
2011-05-19 15:05:51 +0200

    policy goals output formatting

--- app/javascripts/lib/models/policy_goal.js
+++ app/javascripts/lib/models/policy_goal.js
@@ -1,12 +1,14 @@
 var PolicyGoal = Backbone.Model.extend({
   initialize : function() {
     window.policy_goals.add(this);
-    this.reached_query = new Gquery({ key: this.get("reached_query")}); // returns boolean
+    
+    this.success_query = new Gquery({ key: this.get("success_query")}); // returns boolean
     this.value_query   = new Gquery({ key: this.get("value_query")});   // returns value
+    this.target_query  = new Gquery({ key: this.get("target_query")});  // returns value
   },
   
-  success : function() {
-    var res = this.reached_query.result()[0][1];
+  success_value : function() {
+    var res = this.success_query.result()[0][1];
     return res;
   },
   
@@ -15,23 +17,52 @@ var PolicyGoal = Backbone.Model.extend({
     return res;
   },
   
+  target_value : function() {
+    var res = this.target_query.result()[0][1];
+    return res;
+  },
+  
+  // DEBT: we could use a BB view
   update_view : function() {
-    var success = this.success() === true;
+    var success = this.success_value() === true;
     var template = $("<span>")
     template.append(success ? 'V' : 'X');
     template.css('color', success ? 'green' : 'red');
     this.dom_element().find(".check").html(template);
     
-    var current_value = this.current_value();
+    var current_value = this.format_value(this.current_value());
     this.dom_element().find(".you").html(current_value);    
+    
+    var target_value = this.format_value(this.target_value());
+    this.dom_element().find(".target").html(target_value);    
   },
   
+  // TODO
   is_set : function() {
     return false;
   },
   
   dom_element : function() {
     return $("#goal_" + this.get("goal_id"));
+  },
+  
+  // TODO: move to metric.js as much as possible
+  format_value : function(n) {
+    var out = null;
+    switch(this.get('display_fmt')) {
+      case 'percentage' :
+        out = "" + Metric.round_number(n * 100, 2) + "%";
+        break;
+      case 'number':
+        out = Metric.round_number(n, 2);
+        break;
+      case 'number_with_unit':
+        out = "" + Metric.round_number(n, 2) + " " + this.get('unit');
+        break;
+      default:
+        out = n;
+    }
+    return out;
   }
 
 });

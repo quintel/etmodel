8cb1e3cdfcb888d642eb839713373a537120a4ac
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-05-20 17:34:32 +0200

    making specs pass

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,50 +2,6 @@
 # Class for all user settings that should persist over a session.
 #
 class Setting
-  ####### Complexities
-
-  LEVELS = {
-    1 => 'simple',
-    2 => 'medium',
-    3 => 'advanced',
-    4 => 'municipalities',
-    5 => 'watt_nu',
-    6 => 'new_municipality_view',
-    7 => 'ameland_advanced',
-    8 => 'network'
-  }
-
-
-  ##
-  # @untested 2011-01-24 robbert
-  # 
-  def all_levels
-    LEVELS
-  end
-
-  ##
-  # @tested 2010-11-30 seb
-  # 
-  def complexity=(param)
-    self[:complexity] = param.andand.to_i
-  end
-
-
-  ##
-  # @untested 2011-01-09 seb
-  # 
-  def complexity_key
-    LEVELS[self.complexity.to_i]
-  end
-
-  ##
-  # @tested 2010-11-30 seb
-  # 
-  def simple?;    self.complexity == 1; end
-  def medium?;    self.complexity == 2; end
-  def advanced?;  self.complexity == 3; end
-
-  #######
   
   DEFAULT_ATTRIBUTES = {
     :show_municipality_introduction => true,
@@ -114,5 +70,49 @@ class Setting
       send("#{key}=", value)
     end
   end
+  ####### Complexities
+
+  LEVELS = {
+    1 => 'simple',
+    2 => 'medium',
+    3 => 'advanced',
+    4 => 'municipalities',
+    5 => 'watt_nu',
+    6 => 'new_municipality_view',
+    7 => 'ameland_advanced',
+    8 => 'network'
+  }
+
+
+  ##
+  # @untested 2011-01-24 robbert
+  # 
+  def all_levels
+    LEVELS
+  end
+
+  ##
+  # @tested 2010-11-30 seb
+  # 
+  def complexity=(param)
+    @complexity = param.andand.to_i
+  end
+
+
+  ##
+  # @untested 2011-01-09 seb
+  # 
+  def complexity_key
+    LEVELS[self.complexity.to_i]
+  end
+
+  ##
+  # @tested 2010-11-30 seb
+  # 
+  def simple?;    self.complexity == 1; end
+  def medium?;    self.complexity == 2; end
+  def advanced?;  self.complexity == 3; end
+
+  #######
 
 end

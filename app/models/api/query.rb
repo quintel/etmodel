473a1e723eeee92e63bc12aeeedeb8b5e54e9be8
Paolo Zaccagnini <paozac@gmail.com>
2011-05-25 11:20:48 +0200

    house selection tools seems to be working. Very slow though

---
+++ app/models/api/query.rb
@@ -0,0 +1,28 @@
+# TODO: add caching, multiple queries, etc
+class Api::Query
+  include HTTParty
+  base_uri APP_CONFIG[:httparty_base_uri]
+  
+  attr_accessor :query, :api_session_id
+  
+  def execute
+    return false unless query
+    url = "/api/v2/api_scenarios/#{api_session_id}.json"
+    request_params = { result: [query]}
+    response = self.class.get(url, :query => request_params)
+    @result = response["result"]
+  end
+  
+  # To be used on queries that return a single value
+  # (query => result)
+  def fetch_single_value
+    execute
+    @result.values.first
+  end
+  
+  # useful for debugging
+  def fresh_session_id
+    response = self.class.get("/api/v2/api_scenarios/new.json")
+    self.api_session_id = response["api_scenario"]["api_session_key"]
+  end
+end

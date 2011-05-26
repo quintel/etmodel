b8799019e6b93c223f902e856ba6ba8905ec5f5c
Paolo Zaccagnini <paozac@gmail.com>
2011-05-26 13:47:00 +0200

    TRASH: removed useless helper methods

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,4 +1,5 @@
 module DateHelper
+  # TODO: replace with standard localization API
   def format_date_nicely_with_time(date)	   
      @month_names = [nil] + %w(januari februari maart april mei juni juli augustus september oktober november december)
      @month = date.strftime("%m").to_i

f2119a9418cadbd20a33300c9b26803f2013e771
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-05-26 12:06:29 +0200

    update deploy scripts;

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,49 +8,25 @@ set :stage, :production
 
 #### UNCOMMENT roles when we setup server
 
-#task :staging do
-#  set :domain, "80.255.251.233"
-#  role :web, domain # Your HTTP server, Apache/etc
-#  role :app, domain # This may be the same as your `Web` server
-#  role :db,  domain, :primary => true # This is where Rails migrations will run
-#  set :branch, "staging"
-#  set :server_type, 'staging'
-#end
-#
-#task :testing do
-#  set :domain, "80.255.251.241"
-#  role :web, domain # Your HTTP server, Apache/etc
-#  role :app, domain # This may be the same as your `Web` server
-#  role :db,  domain, :primary => true # This is where Rails migrations will run
-#  set :branch, "testing"
-#  set :server_type, 'testing'
-#end
-##
-# makes the following work. Overrides the staging configuration with production configuration.
-# cap prod deploy
-#
-#task :prod do
-#  set :domain, "80.255.251.223"
-#  role :web, domain # Your HTTP server, Apache/etc
-#  role :app, domain # This may be the same as your `Web` server
-#  role :db,  domain, :primary => true # This is where Rails migrations will run
-#  set :branch, "master"
-#  set :server_type, 'production'
-#end
+task :production do
+  set :domain, "46.137.109.15"
+  role :web, domain # Your HTTP server, Apache/etc
+  role :app, domain # This may be the same as your `Web` server
+  role :db,  domain, :primary => true # This is where Rails migrations will run
+  set :branch, "master"
+  set :server_type, 'production'
+end
 
-set :user, 'root'
+set :user, 'ubuntu'
 
-
-set :deploy_to, "/var/rails/apps/#{application}"
-set :config_files, "/var/rails/config_files"
+set :deploy_to, "/home/ubuntu/apps/#{application}"
+set :config_files, "/home/ubuntu/config_files/#{application}"
 
 set :scm, :git
 set :repository,  "git@github.com:dennisschoenmakers/etmodel.git"
 set :scm, "git"
 set :deploy_via, :remote_cache
 set :chmod755, "app config db lib public vendor script script/* public/disp*"  	# Some files that will need proper permissions set :use_sudo, false
-set :git_enable_submodules, 1
-# ssh_options[:forward_agent] = true
-
-
+ssh_options[:forward_agent] = true
 set :use_sudo,     false
+set :rvm_ruby_string, '1.9.2'

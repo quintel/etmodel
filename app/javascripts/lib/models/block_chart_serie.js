c72572ea2975da16b8c5f0e8174cb11e2a498f7b
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-05-27 15:41:56 +0200

    changed block_chart_serie.js

--- app/javascripts/lib/models/block_chart_serie.js
+++ app/javascripts/lib/models/block_chart_serie.js
@@ -1,8 +1,8 @@
 var BlockChartSerie = Backbone.Model.extend({
   initialize : function() {
-    var cost_gql_query = this.get('gquery_key') + "total_cost_per_mje)),MJ_TO_MHW)";
+    var cost_gql_query = this.get('gquery_key') + "total_cost_per_mwh_electricity)),1)";
     var investment_gql_query = this.get('gquery_key') + 
-      "overnight_investment_co2_capture_per_mj_s+overnight_investment_ex_co2_per_mj_s)),DIVIDE(1,MILLIONS))";
+      "initial_investment_costs_per_mw_electricity)),DIVIDE(1,MILLIONS))";
 
     this.set({
       gquery_cost : new Gquery({key : cost_gql_query}),

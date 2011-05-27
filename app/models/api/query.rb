531c48aaceec7aa536f07de53dd2eae87e912abb
Paolo Zaccagnini <paozac@gmail.com>
2011-05-27 15:24:45 +0200

    ruby gql client: run more queries with a single request

--- app/models/api/query.rb
+++ app/models/api/query.rb
@@ -1,23 +1,60 @@
-# TODO: add caching, multiple queries, etc
+# This is how we receive the data (array):
+#
+# {
+#  "AREA(number_of_existing_households)" => [[2010, 6228292.0], [2040, 6228292.0]],
+#  "AREA(number_households)"=>[[2010, 7242200.0], [2040, 7242200.0]]
+# }
+#
+# or scalar:
+#
+# {
+#   "present:DIVIDE(blah blah blah)" => 0.002075001971257579
+# }
+#
+
 class Api::Query
   include HTTParty
   base_uri APP_CONFIG[:api_url]
   
-  attr_accessor :query, :api_session_id
-  
-  def execute
-    return false unless query
-    url = "/api_scenarios/#{api_session_id}.json"
-    request_params = { result: [query]}
-    response = self.class.get(url, :query => request_params)
-    @result = response["result"]
+  attr_accessor :queries, :api_session_id
+
+  def initialize
+    self.queries = []
+  end
+
+  # returns data as scalar value
+  def fetch_single_value(q)
+    res = execute[q]
+    if res.is_a?(Array)
+      res[0][1].to_f
+    else
+      res.to_f
+    end
+  rescue
+    nil
+  end
+
+  # returns data in this format: {2010 => 1.0, 2040 => 1.0}
+  def fetch_pair(q)
+    res = execute[q]
+    {
+      res[0][0] => res[0][1][0].to_f,
+      res[1][0] => res[1][1][0].to_f,
+    }
+  rescue
+    nil
   end
   
-  # To be used on queries that return a single value
-  # (query => result)
-  def fetch_single_value
-    execute
-    @result.values.first
+  def execute
+    @result ||= execute!
+  end
+
+  def execute!
+    return false unless queries.any? and api_session_id
+    url = "/api_scenarios/#{api_session_id}.json"
+    request_params = { result: queries }
+    response = self.class.get(url, :query => request_params)
+    @result = response["result"]
   end
   
   # useful for debugging

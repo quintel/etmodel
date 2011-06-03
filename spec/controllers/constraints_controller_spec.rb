5f282a8361cddf6fb19cfa120ff77d3ca915f20e
Paolo Zaccagnini <paozac@gmail.com>
2011-06-03 14:18:28 +0200

    tests to check all constraint popups

--- spec/controllers/constraints_controller_spec.rb
+++ spec/controllers/constraints_controller_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
 describe ConstraintsController do
+  render_views
+  
   describe "on GET show" do
     before { get :show, :id => 1 }
-    
+  
     it { should respond_with(:success) }
     it { should render_template(:show) }
   end
 
-  describe "on GET iframe" do
-    before { get :iframe, :id => 1 }
+  1.upto(8) do |i|
+    describe "on GET iframe/#{i}" do
+      before { get :iframe, :id => i }
     
-    it { should respond_with(:success) }
-    it { should render_template(:iframe) }
+      it { should respond_with(:success) }
+      it { should render_template(:iframe) }
+      it { should render_template("constraint_#{i}") }
+    end
   end
 end

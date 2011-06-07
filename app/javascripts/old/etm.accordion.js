f7d3bc56e175f312df35e3d8af517f45e2c12870
Paolo Zaccagnini <paozac@gmail.com>
2011-06-07 17:09:41 +0200

    restored link to revert to default chart. closes #50

--- app/javascripts/old/etm.accordion.js
+++ app/javascripts/old/etm.accordion.js
@@ -26,11 +26,14 @@ $(document).ready(function() {
       });
     }
 
+    // Here we load the new default chart
     $('.ui-accordion').bind('accordionchange', function(ev,ui) {
       if (ui.newHeader.length > 0){
         var output_element_id = ui.newHeader.attr('id').match(/\d+$/);
         if (output_element_id != '32') {
+          window.charts.current_default_chart = output_element_id;
           window.charts.load(output_element_id);
+          $("a.default_charts").hide();
         }
       }
     });

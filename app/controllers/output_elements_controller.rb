1e81cafbb77ac2c48f803fd30db4c0053b31217c
Paolo Zaccagnini <paozac@gmail.com>
2011-06-09 12:25:00 +0200

    reimplemented html_table charts. closes #60

--- app/controllers/output_elements_controller.rb
+++ app/controllers/output_elements_controller.rb
@@ -2,6 +2,13 @@ class OutputElementsController < ApplicationController
   def show
     @output_element = OutputElement.find(params[:id])
 
+    if @output_element.html_table?
+      @api = Api::Client.new
+      @api.api_session_id = Current.setting.api_session_key
+      # queries caching
+      @api.queries = @output_element.allowed_output_element_series.map(&:key)
+    end
+
     respond_to do |format|
       format.js { render }
     end
--- app/javascripts/lib/views/html_table_chart_view.js
+++ app/javascripts/lib/views/html_table_chart_view.js
@@ -1,39 +1,10 @@
-
 var HtmlTableChartView = BaseChartView.extend({
-  cached_results : null,
-  
   initialize : function() {
     this.initialize_defaults();
   },
-  
+
   render : function() {
     this.clear_container();
-    this.create_table(this.model);
-  },
-  
-  // Horrible. PZ Mon 9 May 2011 14:38:37 CEST
-  create_table : function(data) {
-    var results = this.model.series_hash();
-    
-    var table     = $('<table class="dynamic_table">');
-    var text      = [];
-    var row_count = results.length;
-    var ct        = 0;
-    
-    // way faster
-    for (var i = 0; i < row_count; i++) {
-      text[ct++] = "<tr><td>";
-      text[ct++] = results[i].label;
-      text[ct++] = "</td><td>";
-      text[ct++] = results[i].present_value;
-      text[ct++] = "</td><td>";
-      text[ct++] = results[i].future_value;
-      text[ct++] = "</td></tr>";
-    }
-    
-    table.append(text.join(''));
-
-    this.model.container_node().append(table);
+    this.model.container_node().html(window.table_content);
   }
-  
 });

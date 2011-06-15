592d289350a977ab373b90ed0858edaeaafda635
Paolo Zaccagnini <paozac@gmail.com>
2011-06-15 14:31:25 +0200

    admin: fixed ViewNodesController#create

--- spec/controllers/admin/view_nodes_controller_spec.rb
+++ spec/controllers/admin/view_nodes_controller_spec.rb
@@ -14,4 +14,23 @@ describe Admin::ViewNodesController do
       response.should render_template(:index)
     end
   end
+  
+  describe "POST create" do
+    describe " a root node" do
+      it "should be created" do
+        post :create, :view_node => { :key => 'a_root_node' }
+        assigns(:view_node).should be_a(ViewNode::Root)
+        response.should be_redirect
+      end
+    end
+  end
+  
+  describe "GET show" do
+    before do
+      get :show, :id => root_node.id
+    end
+    
+    it { should respond_with(:success) }
+    it { should render_template :show }
+  end
 end

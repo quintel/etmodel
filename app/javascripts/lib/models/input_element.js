1776525d918127ccc5adfaa3abb0a9048fb99222
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-06-23 09:42:27 +0200

    update user_values silently from user_values.json. no  a double api request is sent anymore. Fixes #117

--- app/javascripts/lib/models/input_element.js
+++ app/javascripts/lib/models/input_element.js
@@ -94,7 +94,7 @@ var InputElementList = Backbone.Collection.extend({
       input_element.set_label(values.full_label); 
       var user_value = values.user_value;
       var default_value = (_.isUndefined(user_value) || _.isNaN(user_value) || _.isNull(user_value)) ? values.start_value : user_value;
-      input_element.set({user_value : default_value});
+      input_element.set({user_value : default_value}, {silent : true});
     });
   },
 

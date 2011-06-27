1067b77c0d1bf1d06a91684ee7ee27b45fdf520a
Robbert Dol <robbert.dol@quintel.com>
2011-06-27 15:31:14 +0200

    added some functions to show the target for co2

--- app/javascripts/lib/views/vertical_bar_chart_view.js
+++ app/javascripts/lib/views/vertical_bar_chart_view.js
@@ -20,14 +20,34 @@ var VerticalBarChartView = BaseChartView.extend({
   },
 
   results : function() {
+    var results = this.results_with_1990();
+    var smallest_scale = 3
+    
+    var target_serie = this.model.target_series()[0]
+    
+    var result = target_serie.result()[1][1]; // target_series has only present or future value
+    // result = Metric.scaled_value(result, 2, 3);
+
+    var x = parseFloat(target_serie.get('position'));
+    results.push([[x - 0.4, result], [x + 0.4, result]]);
+    return results;
+  },
+  //custom function for showing 1990
+  results_with_1990 : function(){
     var result = this.model.results();
     return [[result[0][1][1],result[1][0][1],result[1][1][1]]];
   },
-
   ticks : function() {
+    // added 1990 in the code here, this is the only charts that uses this.
     return [1990,App.settings.get("start_year"), App.settings.get("end_year")];
   },
   filler : function() {
-    return [];
+    // add this filler to create a dummy value. This is needed because the target line must be added to the end of the serie
+    return [{}];
+  },
+  axis_scale : function(){
+    return [0,this.axis_max_value( _.flatten(this.results()))];
   }
 });
+
+

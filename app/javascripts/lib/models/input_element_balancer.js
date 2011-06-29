c325d857bf94df3fd9746086ae6aa84f97dca290
Anthony Williams <hi@antw.me>
2011-06-29 17:11:15 +0100

    AAAAAAARRRRRRRGH!!!
    JavaScript needs an implementation of Decimal. Seriously. (Too many)
    hours of wrestling with floating-point arithmetic has _finally_ resulted
    in a slider balancer which actually works with > 2 sliders, and with
    different step values.
    
    The new implementation seeks to balance all sliders in the group
    simultaneously. For example, given three sliders:
    
        Slider 1: 100
        Slider 2:   0
        Slider 3:   0
    
    Changing the slider 1 value to 80 now increases the value of slider two
    and three to 10, instead of just changing one slider to 20. This should
    have been easy, but thanks to the vast stupidity which is floating-point
    arithmetic, it wasn't. Take one slider whose value is 100, and reduce
    its value to 99.9; that means that slider two has to be increased to
    0.1, right? No.
    
        > 100 - 99.9
          0.09999999999999432
    
    So, finally we have Balancer.prototype.snapValue which adjusts floating
    point numbers to match the precision of the smallest snap value of the
    sliders, which seems to work nicely in all my tests.
    
    Other adjustments include recalculating the flex amount after assigning
    to each slider to stop it from bouncing between positive and negative
    amounts on each iteration, before finally giving up after 20 iterations,
    and a couple of short-circuit `break`s to avoid unnecessary iterations.
    
    So yeah. That was my day. Now a little song:
    
                     Floating-point arithmetic does suck,
                          It involves mostly luck,
                              And I hate it.
    
    I can't write songs.

--- app/javascripts/lib/models/input_element_balancer.js
+++ app/javascripts/lib/models/input_element_balancer.js
@@ -21,10 +21,13 @@
   function Balancer (options) {
     _.bindAll(this, 'onBegin', 'onChange', 'onCommit', 'onAbort');
 
-    this.options = _.clone(options || {});
-    this.views   = [];
-    this.quinns  = [];
-    this.max     = this.__getMax();
+    this.options   = _.clone(options || {});
+    this.max       = this.__getMax();
+    this.precision = 1;
+
+    this.views  = [];
+    this.quinns = [];
+    this.brutes = [];
 
     this.masterId     = null;
     this.subordinates = null;
@@ -70,7 +73,8 @@
     this.views.push(inputView);
     this.quinns.push(inputView.quinn);
 
-    this.max = this.__getMax();
+    this.max       = this.__getMax();
+    this.precision = this.__getPrecision();
 
     return this;
   };
@@ -80,11 +84,13 @@
    * subordinates.
    */
   Balancer.prototype.doBalance = function (newValue, quinn) {
-    var self          = this,
-        iterations    = 20,
+    var iterations    = 20,
         amountChanged = newValue - quinn.value,
 
-        flex, sliders, sLength, prevValue, i;
+        originalValues, originalQuinns,
+
+        flexPerSlider, flex, sliders, sLength, slider,
+        previousFlex, nextIterationSliders, i;
 
     // Return quickly; if the amount changed is larger than max, then the
     // change is impossible
@@ -92,45 +98,93 @@
       return false;
     }
 
-    // Adjust the subordinate sliders to balance; max 20 iterations.
-    while (iterations--) {
-      // Determine which sliders can be altered; of the subordinates, some may
-      // already be at their maximum or minimum values, so there's no point
-      // changing those.
-      sliders = _.select(this.subordinates, function (sub) {
-        if (amountChanged >= 0) {
-          // Master slider is being increased.
-          return (sub.value > sub.selectable[0]);
-        } else {
-          // Master slider is being reduced.
-          return (sub.value < sub.selectable[1]);
-        }
-      });
-
-      if ((sLength = sliders.length) === 0) {
-        // All subordinates are at their min or max value; the change is
-        // impossible.
-        return false;
+    // Determine which sliders can be altered; of the subordinates, some may
+    // already be at their maximum or minimum values, so there's no point
+    // changing those.
+    sliders = _.select(this.subordinates, function (sub) {
+      if (amountChanged >= 0) {
+        // Master slider is being increased.
+        return (sub.value > sub.selectable[0]);
+      } else {
+        // Master slider is being reduced.
+        return (sub.value < sub.selectable[1]);
       }
+    });
 
-      // Flex is the balance maximum value, minus the value of those
-      // sliders which may be altered.
-      flex = this.max - (_.sum(_.pluck(sliders, 'value')) + newValue);
+    if ((sLength = sliders.length) === 0) {
+      // All subordinates are at their min or max value; the change is not
+      // possible.
+      return false;
+    }
+
+    originalQuinns = sliders;
+    originalValues = _.reduce(sliders, function (vals, quinn) {
+      vals[quinn.balanceId] = quinn.value;
+      return vals;
+    }, {});
+
+    // Flex is the balance maximum value, minus the value of those sliders
+    // which may be altered.
+    // Flex is the amount of "value" which needs to be adjusted for. e.g.
+    //
+    //    max: 100
+    //    slider 1: 0
+    //    slider 2: 100
+    //
+    //  If slider 1 is moved to 25, the Flex is -25, since in order to balance
+    //  the sliders we need to subtract 25 from the subordinate sliders.
+    //
+    flex = _.sum(_.pluck(sliders, 'value')) + newValue;
+    flex = this.snapValue(this.max - flex);
+
+    while (iterations--) {
+      nextIterationSliders = [];
 
       for (i = 0; i < sLength; i++) {
-        // Adjust each slider.
-        prevValue = sliders[i].value;
-        sliders[i].__setValue(prevValue + flex);
+        // The amount of flex to be given to each slider. Calculated each time
+        // we balance a slider since the previous slider may have used up all
+        // of the flex if it was less than the smallest slider step value (with
+        // a step value of 0.1, in a three-slider group, a per-subordinate
+        // change of 0.05 would round UP to 0.1, leaving no flex for the
+        // second slider).
+        flexPerSlider = this.snapValue(flex / (sLength - i));
 
-        // Reduce the value which subsequent iterations much change.
-        amountChanged -= (prevValue - sliders[i].value);
+        slider    = sliders[i];
+        prevValue = slider.value;
+
+        slider.__setValue(this.snapValue(prevValue + flexPerSlider));
+
+        // Reduce the flex by the amount by which the slider was changed, in
+        // case more iterations are required.
+        flex = this.snapValue(flex - (slider.value - prevValue));
+
+        // Finally, if this slider still can be moved further as it isn't at
+        // it's min or max selectable value, it may be used again in the next
+        // iteration.
+        if ((flex < 0 && slider.value !== slider.selectable[0]) ||
+            (flex > 0 && slider.value !== slider.selectable[1])) {
+          nextIterationSliders.push(slider);
+        } else if (flex === 0) {
+          break; // We're done!
+        }
       }
 
-      // All done? In some cases the remaining amount may be
-      // 1.0e-15, hence the -1 > n < n
-      if (amountChanged < 1 && amountChanged > -1) {
-        return true;
+      sliders = nextIterationSliders;
+      sLength = sliders.length;
+
+      // Can't go any further; either we used up all the flex successfully
+      // balancing the sliders, or the flex was unchanged from the previous
+      // iterations, in which case balancing isn't possible.
+      if (this.snapValue(flex) === 0.0 || previousFlex === flex) {
+        break;
       }
+
+      previousFlex = flex;
+    }
+
+    if (this.snapValue(flex) !== 0.0) {
+      this.__revert(originalQuinns, originalValues);
+      return false;
     }
   };
 
@@ -141,6 +195,17 @@
     return _.reduce(this.quinns, function (sum, quinn) {
       return sum + quinn.value;
     });
+  };
+
+  /**
+   * Given a float, "snaps" it to match the precision (i.e. if the smallest
+   * step value of any of the balanced sliders is 0.1, it will round the
+   * number to the nearest 0.1). Used heavily in doBalance to account for the
+   * _vast_ number of floating point errors which occur when a group contains
+   * three or more sliders.
+   */
+  Balancer.prototype.snapValue = function (value) {
+    return parseFloat(value.toFixed(this.precision));
   };
 
   // ### Events.
@@ -221,6 +286,21 @@
   };
 
   /**
+   * Since many sliders use a fractional step value, and floating point
+   * arithmetic is a nightmare, determine the number of decimal places used in
+   * the smallest slider step value, so that we can do all balancing using
+   * integers.
+   */
+  Balancer.prototype.__getPrecision = function () {
+    var precision = _.max(_.map(this.quinns, function (quinn) {
+      var strStep = quinn.options.step.toString().split('.');
+      return parseInt((strStep[1] || '').length, 10);
+    }));
+
+    return precision || 0;
+  };
+
+  /**
    * Finishes editing slider values by resetting this.masterId and
    * this.subordinates, and calling 'method' on the subordinates.
    */
@@ -239,6 +319,36 @@
 
     for (i = 0; i < subLength; i++) {
       this.subordinates[i][method]();
+    }
+  };
+
+  /**
+   * Given an array of sliders, returns an object with their values keyed on
+   * the balance ID assigned to the slider.
+   */
+  Balancer.prototype.__originalValues = function (quinns) {
+    var values  = {},
+        qLength = quinns.length,
+        i;
+
+    for (i = 0; i < qLength; i++) {
+      values[quinns[i].balanceId] = quinns[i].value;
+    }
+
+    return values;
+  };
+
+  /**
+   * Given an array of subordinates, and a hash of balance IDs and values,
+   * restores each slider to it's value prior to being changed.
+   *
+   * Used when balancing fails.
+   */
+  Balancer.prototype.__revert = function (quinns, values) {
+    var length = quinns.length, i;
+
+    for (i = 0; i < length; i++) {
+      quinns[i].__setValue(values[quinns[i].balanceId]);
     }
   };
 

9c8bb5f941919c36b3f8f6bf7710da818c126cb8
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-06 12:42:15 +0700

    fixing deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,38 +2,30 @@ require 'bundler/capistrano'
 # require 'thinking_sphinx/deploy/capistrano'
 require 'hoptoad_notifier/capistrano'
 
-
-
+set :application, "etmodel"
 set :stage, :production
+set :domain, "46.137.109.15"
+set :server_type, 'production'
+
+role :web, domain # Your HTTP server, Apache/etc
+role :app, domain # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
 
 #### UNCOMMENT roles when we setup server
 
 task :production do
-  set :application, "etmodel"
-
-  set :domain, "46.137.109.15"
-  role :web, domain # Your HTTP server, Apache/etc
-  role :app, domain # This may be the same as your `Web` server
-  role :db,  domain, :primary => true # This is where Rails migrations will run
   set :branch, "master"
-  set :server_type, 'production'
+  set :deploy_to, "/home/ubuntu/apps/#{application}"
+  set :config_files, "/home/ubuntu/config_files/#{application}"
 end
 
 task :staging do
-  set :application, "etmodel_staging"
-
-  set :domain, "46.137.109.15"
-  role :web, domain # Your HTTP server, Apache/etc
-  role :app, domain # This may be the same as your `Web` server
-  role :db,  domain, :primary => true # This is where Rails migrations will run
   set :branch, "staging"
-  set :server_type, 'production'
+  set :deploy_to, "/home/ubuntu/apps/#{application}_staging"
+  set :config_files, "/home/ubuntu/config_files/#{application}_staging"
 end
 
 set :user, 'ubuntu'
-
-set :deploy_to, "/home/ubuntu/apps/#{application}"
-set :config_files, "/home/ubuntu/config_files/#{application}"
 
 set :scm, :git
 set :repository,  "git@github.com:dennisschoenmakers/etmodel.git"

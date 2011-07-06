8524367b99007b46336b42f76753dfbf2bc61db8
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-06 15:14:04 +0700

    updating capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,27 +4,34 @@ require 'hoptoad_notifier/capistrano'
 
 set :application, "etmodel"
 set :stage, :production
-set :domain, "46.137.109.15"
 set :server_type, 'production'
-
-role :web, domain # Your HTTP server, Apache/etc
-role :app, domain # This may be the same as your `Web` server
-role :db,  domain, :primary => true # This is where Rails migrations will run
 
 #### UNCOMMENT roles when we setup server
 
 task :production do
   set :branch, "master"
+  set :domain, "46.137.109.15"
+
   set :application_key, "#{application}"
   set :deploy_to, "/home/ubuntu/apps/#{application_key}"
   set :config_files, "/home/ubuntu/config_files/#{application_key}"
+
+  role :web, domain # Your HTTP server, Apache/etc
+  role :app, domain # This may be the same as your `Web` server
+  role :db,  domain, :primary => true # This is where Rails migrations will run
 end
 
 task :staging do
+  set :domain, "ec2-46-137-68-95.eu-west-1.compute.amazonaws.com"
+
   set :application_key, "#{application}_staging"
   set :branch, "staging"
   set :deploy_to, "/home/ubuntu/apps/#{application_key}"
   set :config_files, "/home/ubuntu/config_files/#{application_key}"
+
+  role :web, domain # Your HTTP server, Apache/etc
+  role :app, domain # This may be the same as your `Web` server
+  role :db,  domain, :primary => true # This is where Rails migrations will run
 end
 
 

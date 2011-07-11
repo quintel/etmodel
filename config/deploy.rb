2a1598bed2737b4676f1c047c52ed262c8379eb7
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-11 14:40:31 +0700

    config/deploy.rb use staging/production branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,9 +23,9 @@ end
 
 task :staging do
   set :domain, "ec2-46-137-68-95.eu-west-1.compute.amazonaws.com"
+  set :branch, "staging"
 
   set :application_key, "#{application}_staging"
-  set :branch, "staging"
   set :deploy_to, "/home/ubuntu/apps/#{application_key}"
   set :config_files, "/home/ubuntu/config_files/#{application_key}"
 

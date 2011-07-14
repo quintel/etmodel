36c7ba6c14c94d09fcdb605bb237719cf720ee9c
Robbert Dol <robbert.dol@quintel.com>
2011-07-14 18:08:50 +0200

    made the unit dynamic and more detailed

--- app/javascripts/lib/views/horizontal_stacked_bar_chart_view.js
+++ app/javascripts/lib/views/horizontal_stacked_bar_chart_view.js
@@ -3,7 +3,7 @@ var HorizontalStackedBarChartView = BaseChartView.extend({
   cached_results : null,
   
   initialize : function() {
-    console.info("loading")
+    console.info("loading charts");
     this.initialize_defaults();
   },
   
@@ -14,11 +14,11 @@ var HorizontalStackedBarChartView = BaseChartView.extend({
         this.model.get("container"),
         this.results(),
         this.ticks(),
-        false,
-        "KG/GJ",
+        this.model.get('show_point_label'),
+        this.model.get('unit'),
         this.axis_scale(),
         this.colors(),
-        [ "Coal" ,"Oil" ,"Gas","Uranium" ]
+        this.labels()
       );
   },
   
@@ -26,6 +26,7 @@ var HorizontalStackedBarChartView = BaseChartView.extend({
   // [[value1, 1], [value1, 2]],[[value2, 1], [value2, 2]] ...
   results : function() {
       var series = {};
+      // parse all the results into a hash by group and add the serie number to it
       this.model.series.each(function(serie) {
         var group = serie.get('group');
         if (group) {
@@ -33,7 +34,8 @@ var HorizontalStackedBarChartView = BaseChartView.extend({
           series[group].push([serie.result_pairs()[0],(series[group].length + 1)]);
         }
       });
-      out = _.map(series, function(values, group) {return values})
+      //jqplot needs and array so we create this by mapping the hash
+      out = _.map(series, function(values, group) {return values;});
 
      return out;
   },
@@ -43,21 +45,23 @@ var HorizontalStackedBarChartView = BaseChartView.extend({
   },
   
   axis_scale : function() {
-    var min = this.model.get('min_axis_value')
+    var min = this.model.get('min_axis_value');
+    var max = this.model.get('max_axis_value');
     if (min == undefined)  console.info('minimal axis value not defined');
-    var max = this.model.get('max_axis_value')
     if (max == undefined)  console.info('maximal axis value not defined');
     return [min, max];
   },
     
   ticks : function() {
-    var groups = this.model.series.map(function(serie) {return serie.get('group')});
+    var groups = this.model.series.map(function(serie) {return serie.get('group');});
     return _.uniq(groups);
   },
   colors : function() {
     return _.uniq(this.model.colors());
   },
-  
+  labels : function() {
+    return _.uniq(this.model.labels());
+  }
   
 });
 

d182ebd794c686bcd2a35547511dc1f4916d4b76
Paolo Zaccagnini <paozac@gmail.com>
2011-07-15 16:03:49 +0200

    descriptions popups. closes #184

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -3,13 +3,13 @@ class DescriptionsController < ApplicationController
   #RD: REFACTOR: It does not make sense that in the first the params[:output] is used to set the type and in the second, it is params[:type].
   
   def show
-    @description = Description.where(:describable_id => params[:id], :describable_type => params[:output]).first
-    if @description.nil? || @description.describable.andand.title_for_description.nil?
-      logger.fatal "No description for %s" % params[:id]
+    @description = Description.find(params[:id]) rescue nil
+    if @description.nil? || @description.describable.title_for_description.blank?
       render :text => 'Description is not yet available.'
     end
   end
 
+  # TODO: is this used?
   def charts
     params[:type] = 'OutputElement'
     @description = Description.where(:describable_id => params[:id], :describable_type => params[:type]).first

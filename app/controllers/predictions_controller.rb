1570290dba6ce36e6ad4cbc78ff86c7ddfa6ef18
Paolo Zaccagnini <paozac@gmail.com>
2011-07-26 16:19:03 +0200

    ajax request

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,5 +1,11 @@
 class PredictionsController < ApplicationController
   def index
     @input_element = InputElement.find(params[:input_element_id])
+    @prediction = @input_element.predictions.first
+  end
+  
+  def show
+    @prediction = Prediction.find params[:id]
+    render :layout => false if request.xhr?    
   end
 end

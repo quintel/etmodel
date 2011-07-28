e78f78e423d93066b6e0db75b4dd032727484fb4
Paolo Zaccagnini <paozac@gmail.com>
2011-07-28 13:59:28 +0200

    thinking-sphinx capistrano recipes

---
+++ lib/capistrano/sphinx.rb
@@ -0,0 +1,26 @@
+namespace :sphinx do
+  desc "Link up Sphinx's indexes."
+  task :symlink_indexes, :roles => [:app] do
+    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+  end
+  
+  desc "Rebuild and restart"
+  task :rebuild_and_restart, :roles => [:app] do
+    # The silly TS capistrano recipes are repeating some steps twice
+    stop_gracefully
+    rake_on_current "thinking_sphinx:configure"
+    rake_on_current "thinking_sphinx:index"
+    rake_on_current "thinking_sphinx:start"
+    # thinking_sphinx.configure
+    # thinking_sphinx.index
+    # thinking_sphinx.start
+  end
+  
+  task :stop_gracefully do
+    begin
+      rake_on_current "thinking_sphinx:stop"
+    rescue
+      puts "ThinkingSphinx is not running. No stop required."
+    end
+  end
+end

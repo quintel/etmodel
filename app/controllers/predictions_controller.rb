62030013eb76d0843115d395f50b26c629c80135
Paolo Zaccagnini <paozac@gmail.com>
2011-08-01 17:36:48 +0200

    backcasting comments

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,11 +1,28 @@
 class PredictionsController < ApplicationController
   def index
     @input_element = InputElement.find(params[:input_element_id])
-    @prediction = @input_element.predictions.first
+    @prediction = @input_element.predictions.find(params[:prediction_id]) || @input_element.predictions.first
+    @comment = Comment.new
+    @comment.commentable = @prediction#(:commentable_id => @prediction.id, :commentable_type => 'Prediction')
   end
   
   def show
     @prediction = Prediction.find params[:id]
+    @comment = Comment.new
+    @comment.commentable = @prediction#(:commentable_id => @prediction.id, :commentable_type => 'Prediction')
     render :layout => false if request.xhr?    
+  end
+  
+  def comment
+    @prediction = Prediction.find(params[:id])
+    @comment = Comment.new(params[:comment])
+    @comment.user_id = current_user.id if current_user
+    @comment.commentable = @prediction
+    if @comment.save
+      flash[:notice] = "Your comment has been added"
+    else
+      flash[:error] = "Error saving the comment"
+    end
+    redirect_to predictions_path(:input_element_id => @prediction.input_element_id, :prediction_id => @prediction.id)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,18 @@
 class Comment < ActiveRecord::Base
+  belongs_to :commentable, :polymorphic => true
+  belongs_to :user
+  
+  validates :body, :presence => true
+  
+  attr_accessible :body, :name, :email
+  
+  def author_name
+    if user
+      user.name
+    elsif name
+      name
+    else
+      "[anonymous]"
+    end
+  end
 end

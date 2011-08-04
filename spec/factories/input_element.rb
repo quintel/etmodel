8d114e18b410a742364efe0bb55c9e7ea6af0864
Paolo Zaccagnini <paozac@gmail.com>
2011-08-04 15:21:20 +0200

    gem updates

--- spec/factories/input_element.rb
+++ spec/factories/input_element.rb
@@ -1,5 +1,6 @@
 Factory.define :input_element do |f|
   f.name 'an_input_element'
   f.sequence(:key) {|n| "input_element_#{n}" }
-  f.input_id f.id #TODO:  quick fix
+  f.input_id 0
+  f.after_create {|i| i.update_attribute(:input_id, i.id)}
 end

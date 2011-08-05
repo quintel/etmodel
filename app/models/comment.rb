a459029d1a764117f45e47458ab3dad313d4faae
Paolo Zaccagnini <paozac@gmail.com>
2011-08-05 16:17:09 +0200

    backcasting: new fields

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer(4)      not null, primary key
+#  commentable_id   :integer(4)
+#  commentable_type :integer(4)
+#  user_id          :integer(4)
+#  email            :string(255)
+#  name             :string(255)
+#  title            :string(255)
+#  body             :text
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :user

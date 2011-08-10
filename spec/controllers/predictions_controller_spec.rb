b8b362f88cd07888e6b74d95ee8798481222855a
Paolo Zaccagnini <paozac@gmail.com>
2011-08-10 16:17:33 +0200

    backcasting: created standalone share page
    ajax-based comments

--- spec/controllers/predictions_controller_spec.rb
+++ spec/controllers/predictions_controller_spec.rb
@@ -1,24 +1,33 @@
 require 'spec_helper'
 
 describe PredictionsController do
-    render_views
-    let(:prediction) { Factory :prediction}
+  render_views
+  let(:prediction) { Factory :prediction}
 
-    describe "GET index" do
-       before do
-         xhr :get, :index, :input_element_id => prediction.input_element.id
-       end
+  describe "GET index" do
+    before do
+      xhr :get, :index, :input_element_id => prediction.input_element.id
+    end
 
-       it { response.should be_success }
-       it { response.should render_template :index }
-     end
+    it { response.should be_success }
+    it { response.should render_template :index }
+  end
 
-    describe "GET show" do
-       before do
-         xhr :get, :show, :id => prediction.id
-       end
+  describe "GET show" do
+    before do
+      xhr :get, :show, :id => prediction.id
+    end
 
-       it { response.should be_success }
-       it { response.should render_template :show }
-     end
+    it { response.should be_success }
+    it { response.should render_template :show }
+  end
+  
+  describe "GET share" do
+    before do
+      get :share, :id => prediction.id
+    end
+    
+    it { should respond_with(:success)}
+    it { should render_template :index}
+  end
 end

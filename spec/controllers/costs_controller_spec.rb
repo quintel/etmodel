fda4e246c24e7340975486abc1b6bf64f37b6c27
Paolo Zaccagnini <paozac@gmail.com>
2011-09-09 14:58:14 +0200

    costs controller: removed intro action (the view was removed in a previous commit)

--- spec/controllers/costs_controller_spec.rb
+++ spec/controllers/costs_controller_spec.rb
@@ -3,16 +3,8 @@ require 'spec_helper'
 describe CostsController do
   render_views
     
-  describe "GET intro" do
-    it "should be successful" do
-      get :intro
-      response.should be_success
-    end
-  end
-
   describe "GET show" do
     it "should be successful" do
-      controller.class.skip_before_filter :show_intro_at_least_once
       get :show
       response.should be_success
       response.should render_template('show')

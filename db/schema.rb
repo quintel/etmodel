72ba602405fd2ae9f4649b986e4ca28d21c9927d
Paolo Zaccagnini <paozac@gmail.com>
2011-09-30 13:55:22 +0200

    committed schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110930092147) do
+ActiveRecord::Schema.define(:version => 20110930095616) do
 
   create_table "area_dependencies", :force => true do |t|
     t.string  "dependent_on"
@@ -282,8 +282,8 @@ ActiveRecord::Schema.define(:version => 20110930092147) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "sub_header"
-    t.string   "sub_header2"
+    t.string   "general_sub_header"
+    t.string   "group_sub_header"
     t.string   "subheader_image"
     t.string   "key"
     t.boolean  "house_selection"

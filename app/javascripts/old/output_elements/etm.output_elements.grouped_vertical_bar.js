28b36c28bbe52b16064e36bb64203c3ce7e37c3a
Robbert Dol <robbert.dol@quintel.com>
2011-10-04 09:45:39 +0200

    changed jqplot config for grouped bar chart & added some translations

--- app/javascripts/old/output_elements/etm.output_elements.grouped_vertical_bar.js
+++ app/javascripts/old/output_elements/etm.output_elements.grouped_vertical_bar.js
@@ -1,4 +1,5 @@
-function InitializeGroupedVerticalBar(id,serie1,serie2,ticks,groups,unit,axis_values,colors,labels){
+function InitializeGroupedVerticalBar(id,series,ticks,groups,unit,axis_values,colors,labels){
+  console.log(ticks,groups,unit,axis_values,colors,labels);
   var series_with_hack;
   var series_default;
   var serie_colors;
@@ -8,21 +9,21 @@ function InitializeGroupedVerticalBar(id,serie1,serie2,ticks,groups,unit,axis_va
   var renderer_options;
   // this hack will push the labels for the top series off of the page so they don't appear.
   series_with_hack = [{pointLabels:{ypadding: -15}},{pointLabels:{ypadding:9000}}];
-  renderer_options = {groups: groups,barWidth: 35};
+  renderer_options = {groups: groups,barWidth: 20};
 
   series_default = {
     renderer: $.jqplot.BarRenderer,
     rendererOptions: renderer_options,
-    pointLabels:{stackedValue: true},
+    pointLabels:{show:false},
     yaxis:'y2axis',
     shadow: false
   };
-  serie_colors = [colors[0],"#CCCCCC"];
+  serie_colors = ["#4F81BD"];
   axes_defaults =  {tickOptions: {fontSize:font_size}};
-  x_tick_options = {showGridline:false,markSize:0,pad:3.00};
-  y_tick_options = {formatString:'%d\%'};
+  x_tick_options = {showGridline:false,markSize:0,pad:1.00};
+  y_tick_options = {formatString:'%d&nbsp;' + unit};
 
-  $.jqplot(id, [serie1,serie2], {
+  $.jqplot(id, [series], {
     grid: default_grid,
       stackSeries: true,
       seriesColors: serie_colors,
@@ -39,7 +40,9 @@ function InitializeGroupedVerticalBar(id,serie1,serie2,ticks,groups,unit,axis_va
           tickOptions: x_tick_options
         },
         y2axis:{
-          ticks: axis_values,
+          ticks: 5,
+          min_value: axis_values[0],
+          max_value: axis_values[1],
           tickOptions: y_tick_options
         }
       }

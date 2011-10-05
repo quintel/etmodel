aced8d2defa9f3b24a593e178bd99450d32223c1
Robbert Dol <robbert.dol@quintel.com>
2011-10-05 09:39:38 +0200

    fixed the layout of the search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   # Are we sure we want to use the etm layout? Maybe we should only if the
   # user has already started working on a scenario - PZ Mon 4 Jul 2011 10:45:00 CEST
-  layout 'etm'
+  layout 'pages'
 
   include ApplicationController::HasDashboard
   

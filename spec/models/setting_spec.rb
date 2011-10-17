a925ab26504214ed648c39d85665c756678e9235
Paolo Zaccagnini <paozac@gmail.com>
2011-10-17 11:10:38 +0200

    fixed broken tests
    fixed gql#search controller spec
    fixed predictions_controller
    removed tests for setting#already_shown? method, that has been removed

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -206,29 +206,6 @@ describe Setting do
     end
   end
 
-  describe "#already_shown?" do
-    before do
-      @setting = Setting.default
-    end
-
-    it "should not be false the first, true the 2nd time" do
-      @setting.already_shown?('demand/intro').should be_false
-      @setting.already_shown?('demand/intro').should be_true
-    end
-
-    it "should reset already_shown? when calling reset! so everything is false again" do
-      @setting.already_shown?('demand/intro')
-      @setting.reset!
-      @setting.already_shown?('demand/intro').should be_false
-    end
-
-    it "should reset already_shown? when resetting scenario so everything is false again" do
-      @setting.already_shown?('demand/intro')
-      @setting.reset_scenario
-      @setting.already_shown?('demand/intro').should be_false # again
-    end
-  end  
-
   describe "regression tests" do
     describe "DEFAULT_ATTRIBUTES" do
       it "should not persist default values that are objects, e.g. Array" do

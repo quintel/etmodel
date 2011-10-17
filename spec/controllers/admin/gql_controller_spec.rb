e026ac40335a7744bf16d213fdd864a82f47e571
Paolo Zaccagnini <paozac@gmail.com>
2011-10-17 11:10:38 +0200

    fixed broken tests
    fixed gql#search controller spec
    fixed predictions_controller
    removed tests for setting#already_shown? method, that has been removed

--- spec/controllers/admin/gql_controller_spec.rb
+++ spec/controllers/admin/gql_controller_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe Admin::GqlController do
-
+  before(:each) do
+    controller.class.skip_before_filter :restrict_to_admin
+  end
+  
   describe "GET 'search'" do
     it "should be successful" do
       get 'search'

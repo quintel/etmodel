ee1a4150d1aacd3458454d0e8c1c5175749ceb5a
Robbert Dol <robbert.dol@quintel.com>
2011-10-26 16:56:30 +0200

    renamed filler to better name

--- app/javascripts/lib/views/vertical_bar_chart_view.js
+++ app/javascripts/lib/views/vertical_bar_chart_view.js
@@ -11,7 +11,7 @@ var VerticalBarChartView = BaseChartView.extend({
     InitializeVerticalBar(this.model.get("container"), 
       this.results(), 
       this.ticks(),
-      this.filler(),
+      this.serie_settings_filler(),
       this.model.get('show_point_label'),
       'MT', // this.parsed_unit(),
       this.model.colors(),
@@ -21,11 +21,8 @@ var VerticalBarChartView = BaseChartView.extend({
   results : function() {
     var results = this.results_with_1990();
     var smallest_scale = 3
-    
     var target_serie = this.model.target_series()[0]
-    
     var result = target_serie.result()[1][1]; // target_series has only present or future value
-    // result = Metric.scaled_value(result, 2, 3);
 
     var x = parseFloat(target_serie.get('target_line_position'));
     results.push([[x - 0.4, result], [x + 0.4, result]]);
@@ -40,7 +37,7 @@ var VerticalBarChartView = BaseChartView.extend({
     // added 1990 in the code here, this is the only charts that uses this.
     return [1990,App.settings.get("start_year"), App.settings.get("end_year")];
   },
-  filler : function() {
+  serie_settings_filler : function() {
     // add this filler to create a dummy value. This is needed because the target line must be added to the end of the serie
     return [{}];
   }

2ee05993cac583b706d128cfc5ab6570e428ae49
Anthony Williams <hi@antw.me>
2011-11-02 18:17:30 +0000

    Submit dashboard choices to the server.

--- app/javascripts/lib/views/dashboard_changer_view.js
+++ app/javascripts/lib/views/dashboard_changer_view.js
@@ -31,7 +31,7 @@
      * dashboard" element.
      */
     initialize: function (options) {
-      _.bindAll(this, 'render', 'cancel', 'commit');
+      _.bindAll(this, 'render', 'cancel', 'commit', 'onComplete', 'onError');
 
       $(options.triggerEl).fancybox({
         // Rerender the view whenever the fancybox is shown, and use it's
@@ -63,22 +63,45 @@
      * overlay.
      */
     commit: function (event) {
-      var checkedEls = this.$('input[name^=dash]:checked'),
-          checkedLen = checkedEls.length,
-          element;
+      var formEl    = this.$('form'),
+          inputEls  = this.$('input[name^=dash]:checked'),
+          inputsLen = inputEls.length,
+          data      = { dash: {} },
 
-      while (checkedLen--) {
-        element = $(checkedEls[checkedLen]);
+          name, input, i;
 
-        console.log(element.attr('name'), ' -> ', element.val(),
-                    element.parent('label').text());
+      for (i = 0; i < inputsLen; i++) {
+        input = $(inputEls[i]);
+        name  = input.attr('name').replace(/^dash\[(.*)\]$/, '$1');
+
+        // Remove the extra stuff from the name so that we are left with only
+        // the name of the dashboard group. e.g. dash[costs] => costs
+        data.dash[name] = input.val();
       }
 
-      this.$('.commit .indicator').fadeIn('fast');
+      // HTTP method override, auth token, etc.
+      data._method            = $('[name=_method]', formEl).val();
+      data.authenticity_token = $('[name=authenticity_token]', formEl).val();
+      data.utf8               = $('[name=utf8]', formEl).val();
+
+      $.ajax({
+        url:       formEl.attr('action'),
+        type:     'POST',
+        data:      data,
+        dataType: 'json'
+
+      }).done(this.onComplete)
+        .error(this.onError);
+
       this.$('.commit button').animate({ opacity: 0.4 });
 
-      // Simulate a slightly delayed HTTP request.
-      window.setTimeout(function () { $.fancybox.close(); }, 2000);
+      // The ajax indicator is shown after a short delay; this way it doesn't
+      // begin fading in, only for the request to complete very quickly, and
+      // the user wonder what suddenly popped up. This way it only appears if
+      // the request seems to be lagging a bit.
+      window.setTimeout(_.bind(function () {
+        this.$('.commit .indicator').fadeIn('fast');
+      }, this), 500);
 
       // The commit function may be used as a callback for a DOM event.
       if (event) { event.preventDefault(); }
@@ -93,6 +116,24 @@
 
       // The cancel function may be used as a callback for a DOM event.
       if (event) { event.preventDefault(); }
+    },
+
+    // #### XHR Callbacks
+
+    /**
+     * Called upon successful completion of the XHR request.
+     */
+    onComplete: function (data, textStatus, jqXHR) {
+      $.fancybox.close();
+
+      console.log('done', data, textStatus, jqXHR);
+    },
+
+    /**
+     * Called if an error occurred during the XHR request.
+     */
+    onError: function (jqXHR, textStatus, error) {
+      console.log('error', jqXHR, textStatus, error);
     }
 
   });

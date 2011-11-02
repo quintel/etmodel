4a0e5451fd55546d02173976439265a767db866c
Paolo Zaccagnini <paozac@gmail.com>
2011-11-02 15:53:20 +0100

    removed tests for ConstraintsController#iframe
    the action was removed

--- spec/controllers/constraints_controller_spec.rb
+++ spec/controllers/constraints_controller_spec.rb
@@ -9,18 +9,4 @@ describe ConstraintsController do
     it { should respond_with(:success) }
     it { should render_template(:show) }
   end
-
-  # DEBT: test other popups. They require a huge setup, though
-  [1,2,4,6].each do |i|
-    describe "on GET iframe/#{i}" do
-      before do
-        Current.setting.stub_chain(:api_session_key).and_return(1234)
-        get :iframe, :id => i
-      end
-    
-      it { should respond_with(:success) }
-      it { should render_template(:iframe) }
-      it { should render_template("constraint_#{i}") }
-    end
-  end
 end

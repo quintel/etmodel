ccbc34ad7530f733011c85feb22e7882742e9cbb
Anthony Williams <hi@antw.me>
2011-11-03 15:51:02 +0000

    (MIGRATION) Add the "household energy cost" constraint.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111103141558) do
+ActiveRecord::Schema.define(:version => 20111103154307) do
 
   create_table "area_dependencies", :force => true do |t|
     t.string  "dependent_on"

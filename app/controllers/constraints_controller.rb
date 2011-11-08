d608c629e3cfc79fee3406e1e87f1dd46b7cf725
Anthony Williams <hi@antw.me>
2011-11-08 15:37:34 +0000

    Remove unused ivar from ConstraintsController#show.

--- app/controllers/constraints_controller.rb
+++ app/controllers/constraints_controller.rb
@@ -1,15 +1,14 @@
 class ConstraintsController < ApplicationController
-  def show    
+  def show
     @api_client = Api::Client.new
     api_key = Current.setting.api_session_id rescue nil
-  
+
     # be sure we're using the current session id
     # The client is smart enough to fetch a fresh session_id if missing
     @api_client.api_session_id = api_key
-    
+
     @constraint = Constraint.find(params[:id])
-    @constraint_id = params[:id]
-    
+
     render :layout => 'constraint_iframe'
-  end  
+  end
 end

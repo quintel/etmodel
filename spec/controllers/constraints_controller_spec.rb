01538a845f5a82ea4f074e64ed106eafe7ce80f2
Anthony Williams <hi@antw.me>
2011-11-08 15:03:59 +0000

    Name constraint templates using the key, not ID.
    IDs are somewhat volatile and it's easy for databases -- particularly in
    development -- to get out of sync when using auto-incrementing IDs.
    
    This change renames the constraint partial templates to use the key of
    the constraint which should be the same for everyone, regardless of the
    ID of the record.
    
    (Case in point, the new "household_energy_cost" had id:10 when I was
    working on it; a fresh export of the production DB followed by a
    migration gave it id:9).

--- spec/controllers/constraints_controller_spec.rb
+++ spec/controllers/constraints_controller_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe ConstraintsController do
   render_views
-  
+
   describe "on GET show" do
-    before { get :show, :id => 1 }
-  
+    let(:constraint) { Constraint.find_by_key('total_primary_energy') }
+
+    before { get :show, :id => constraint.id }
+
     it { should respond_with(:success) }
     it { should render_template(:show) }
   end

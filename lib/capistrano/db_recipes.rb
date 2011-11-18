67a8d2134bdea6153573b6afef07af3f3a514467
Paolo Zaccagnini <paozac@gmail.com>
2011-11-18 11:34:04 +0100

    updated deployment script. NOT TESTED!

--- lib/capistrano/db_recipes.rb
+++ lib/capistrano/db_recipes.rb
@@ -1,7 +1,7 @@
 namespace :db do
   desc "Move production db to staging db, overwriting everything"
-  task :prod2staging do    
-    warning("You know what you're doing, right? You will overwrite the staging db!")
+  task :prod2staging_all do    
+    warning("You know what you're doing, right? You will overwrite the entire staging db!")
     
     puts "Loading production environment"
       production
@@ -15,44 +15,41 @@ namespace :db do
     puts "And filling staging with production data"
       load_sql_into_db(file)
   end
+  
+  desc "Copy users, press_releases and saved_scenarios from production to staging"
+  task :prod2staging_safe_tables do
+    warning "users, press_releases and saved_scenarios tables on staging will be overwritten with production data"
+    puts "Loading production environment"
+      production
+    puts "Dumping tables #{db_name} to /tmp"
+      tables = %w{
+        users
+        press_releases
+        saved_scenarios
+      }
+      file = dump_db_to_tmp(tables)
+    puts "Loading staging environment"
+      staging
+      puts "target database: #{db_name}"
+    puts "And filling staging with production data"
+      load_sql_into_db(file)    
+  end  
 
-  desc "Move staging db to production db, overwriting only the safe tables"
-  task :staging2prod do
-    puts "this recipe is not ready yet"
-    # tables = %w{
-    #   areas
-    #   carriers
-    #   converter_positions
-    #   converters
-    #   converters_groups
-    #   dataset_carrier_data
-    #   dataset_converter_data
-    #   dataset_link_data
-    #   dataset_slot_data
-    #   gql_test_cases
-    #   gqueries
-    #   gquery_groups
-    #   graphs
-    #   groups
-    #   inputs
-    #   links
-    #   query_table_cells
-    #   query_tables
-    #   slots
-    #   versions
-    # }
-    # # We're not updating scenarios!
-    # 
-    # warning("You know what you're doing, right? You will overwrite most production tables!")
-    # 
-    # puts "Loading staging environment"
-    #   staging
-    #   # partial dump of staging
-    #   file = dump_db_to_tmp(tables)
-    #   get file
-    # puts "Loading production environment"
-    #   production
-    #   load_sql_into_db(file)
+  desc "Move staging db to production db, overwriting everything"
+  task :staging2prod_all do    
+    warning("You know what you're doing, right? You will overwrite the entire production db!")
+    
+    puts "Loading staging environment"
+      staging
+    puts "Dumping database #{db_name} to /tmp"
+      file = dump_db_to_tmp
+    puts "Loading production environment"
+      production
+      puts "target database: #{db_name}"
+    puts "I should now be emptying production"
+      db.empty
+    puts "And filling production with staging data"
+      load_sql_into_db(file)
   end
   
   desc "Empty db - be sure you know what you're doing"

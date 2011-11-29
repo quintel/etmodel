6199231c4b4eb95f9ffbc002af469dce513ffb2b
Paolo Zaccagnini <paozac@gmail.com>
2011-11-29 14:45:44 +0100

    ajax proxy: added missing HTTP methods

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -3,9 +3,8 @@ require 'uri'
 
 # Simple proxy to prevent AJAX Cross Domain Policy restrictions.
 # Usage: GET /proxy/api_scenarios/new.json
-# 
+#
 # TODO: adapt js code and browser detection
-# TODO: add other HTTP methods
 # TODO: error handling
 class ProxyController < ApplicationController
   def index
@@ -13,10 +12,27 @@ class ProxyController < ApplicationController
     url = params[:url]
     uri = URI.parse("#{base_host}/#{url}")
     http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    # request['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest'
-    
-    response = http.request(request)    
+
+    method = request.method.downcase.to_sym
+
+    case method
+    when :get
+      req = Net::HTTP::Get.new(uri.request_uri)
+    when :post
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req.set_form_data(request.request_parameters)
+    when :put
+      req = Net::HTTP::Put.new(uri.request_uri)
+      req.set_form_data(request.request_parameters)
+    when :delete
+      req = Net::HTTP::Delete.new(uri.request_uri)
+    else
+      raise 'Invalid HTTP Method'
+    end
+
+    # req['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest'
+
+    response = http.request(req)
     render :text => response.body, :content_type => response['content-type']
   end
 end

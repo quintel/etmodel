9d1db9238b9e6e213a285838e2ca5e46e17946a7
Paolo Zaccagnini <paozac@gmail.com>
2011-12-08 16:07:51 +0100

    bio_footprint dashboard popup uses js now

--- app/controllers/constraints_controller.rb
+++ app/controllers/constraints_controller.rb
@@ -1,14 +1,6 @@
 class ConstraintsController < ApplicationController
   def show
-    @api_client = Api::Client.new
-    api_key = Current.setting.api_session_id rescue nil
-
-    # be sure we're using the current session id
-    # The client is smart enough to fetch a fresh session_id if missing
-    @api_client.api_session_id = api_key
-
     @constraint = Constraint.find(params[:id])
-
     render :layout => 'constraint_iframe'
   end
 end

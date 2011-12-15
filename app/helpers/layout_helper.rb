b62308733935bd6dac33bb88df41e7d63dd62e80
Paolo Zaccagnini <paozac@gmail.com>
2011-12-15 14:14:02 +0100

    cleaned up sidebar items views and translations

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,4 +1,5 @@
 module LayoutHelper
+  # TODO: clean up
   def search_result_description(result)
     if d = result.andand.description
       if d.content.present?
@@ -8,38 +9,42 @@ module LayoutHelper
   end
 
   def back_to_model_link
-    if Current.setting.last_etm_controller_name.blank?
-      link_to t("Home"), "/"
+    last_etm_controller_name = Current.setting.last_etm_controller_name
+    if last_etm_controller_name.blank?
+      link_to t("Home"), root_path
     else
-      link_to t("back to model"), :controller => Current.setting.last_etm_controller_name, :action => session[:last_etm_action_name] 
+      link_to t("back to model"), :controller => last_etm_controller_name,
+                                  :action => session[:last_etm_action_name]
     end
   end
-    
+
   def current_tutorial_movie
     SidebarItem.find_by_key(params[:id]).andand.send("#{I18n.locale}_vimeo_id")
   end
-  
+
   def country_option(code, opts = {})
     current = Current.setting.region == code
     selected = current ? "selected='true'" : nil
     %Q{<option value="#{code}" #{selected}>#{I18n.t(code)} #{"(test)" if opts[:test]}</option>}.html_safe
   end
-  
+
   def percentage_bar(item)
-    unless item.percentage_bar_query.blank?
-      # need client for showing % bars
-      if client ||= Api::Client.new
-        client.api_session_id = Current.setting.api_session_id
-      end
-      val = client.simple_query(item.percentage_bar_query)
-      # multiplied by 90 because of interface limits, there should be some space left for the value
-      haml_tag :span, :class=>'bar',:style => "width: #{(val * 90).round(2)}%", :alt =>"#{t("sidebar_item.alt")}"
-      haml_tag :span, :class=>'value',:style => "left: #{(val * 90).round(2)}%" do
-        haml_concat "#{(val * 100).round}%"
-      end
+    return if item.percentage_bar_query.blank?
+    # need client for showing % bars
+    if client ||= Api::Client.new
+      client.api_session_id = Current.setting.api_session_id
+    end
+    val = client.simple_query(item.percentage_bar_query)
+    # multiplied by 90 because of interface limits, there should be some space left for the value
+    haml_tag :span, :class =>'bar',
+                    :style => "width: #{(val * 90).round(2)}%",
+                    :alt => I18n.t('sidebar_items.alt')
+    haml_tag :span, :class =>'value',
+                    :style => "left: #{(val * 90).round(2)}%" do
+      haml_concat "#{(val * 100).round}%"
     end
   end
-  
+
   # Tries mapping a hex string to a human readable colour name
   #
   def color_to_string(hex)

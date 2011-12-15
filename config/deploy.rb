db774c4a8ab9e02c8f81f9c89bc4903353e6625c
Paolo Zaccagnini <paozac@gmail.com>
2011-12-15 11:13:20 +0100

    Capistrano cleanup. The nginx box is now staging, while the old staging now uses the apache branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,9 +22,11 @@ task :production do
   role :db,  domain, :primary => true # This is where Rails migrations will run
 end
 
-task :old_staging do
+# This is the previous staging server. It is kept for safety reasons, it should
+# be dropped as soon as possible
+task :apache do
   set :domain, "79.125.109.178"
-  set :branch, "staging"
+  set :branch, "apache"
 
   set :application_key, "#{application}_staging"
   set :deploy_to, "/home/ubuntu/apps/#{application_key}"
@@ -40,29 +42,11 @@ task :old_staging do
   role :db,  domain, :primary => true # This is where Rails migrations will run
 end
 
-
-task :release do
-  set :domain, "ec2-46-51-146-235.eu-west-1.compute.amazonaws.com"
-  set :branch, "release"
-
-  set :application_key, "#{application}_rc"
-  set :deploy_to, "/home/ubuntu/apps/#{application}"  ## this is a copy of production, so serverconfig stays the same
-  set :config_files, "/home/ubuntu/config_files/#{application}" ## this is a copy of production, so serverconfig stays the same
-
-  set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
-  set :db_pass, "Energy2.0"
-  set :db_name, application_key
-  set :db_user, application_key
-
-  role :web, domain # Your HTTP server, Apache/etc
-  role :app, domain # This may be the same as your `Web` server
-  role :db,  domain, :primary => true # This is where Rails migrations will run
-end
-
-
-task :nginx do
+# This is the new nginx-based beta
+#
+task :staging do
   set :domain, "46.137.123.187"
-  set :branch, "nginx"
+  set :branch, "staging"
 
   set :application_key, "#{application}_nginx"
   set :deploy_to, "/home/ubuntu/apps/#{application_key}"  ## this is a copy of production, so serverconfig stays the same

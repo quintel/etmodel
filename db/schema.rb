7f1bc8a48a866b75a4993aecfc615748acd05753
Paolo Zaccagnini <paozac@gmail.com>
2012-01-04 15:59:00 +0100

    output_elements: removed old columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120104141918) do
+ActiveRecord::Schema.define(:version => 20120104145715) do
 
   create_table "area_dependencies", :force => true do |t|
     t.string  "dependent_on"
@@ -143,8 +143,6 @@ ActiveRecord::Schema.define(:version => 20120104141918) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "under_construction",     :default => false
-    t.integer  "legend_columns"
-    t.string   "legend_location"
     t.string   "unit"
     t.boolean  "percentage"
     t.string   "group"

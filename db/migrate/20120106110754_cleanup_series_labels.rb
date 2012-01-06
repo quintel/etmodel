175714fcd835ce432f250f43c24dbca386520c6f
Paolo Zaccagnini <paozac@gmail.com>
2012-01-06 12:31:54 +0100

    clean up series labels translations

---
+++ db/migrate/20120106110754_cleanup_series_labels.rb
@@ -0,0 +1,19 @@
+class CleanupSeriesLabels < ActiveRecord::Migration
+  def self.up
+    OutputElementSerie.find_each do |s|
+      unless s.label.blank?
+        new_label = s.label.parameterize.gsub('-', '_')
+        s.update_attribute :label, new_label
+      end
+      
+      unless s.group.blank?
+        new_group = s.group.parameterize.gsub('-', '_')
+        s.update_attribute :group, new_group
+      end
+      
+    end
+  end
+
+  def self.down
+  end
+end

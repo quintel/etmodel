a8cce4deca4a1263c1078335e3e3e709d14af977
Paolo Zaccagnini <paozac@gmail.com>
2012-01-13 15:14:44 +0100

    nevermind, production shouldn't need it

--- config/initializers/pry.rb
+++ config/initializers/pry.rb
@@ -1,10 +1,12 @@
 Etm::Application.configure do
-  # Use Pry instead of IRB
-  silence_warnings do
-    begin
-      require 'pry'
-      IRB = Pry
-    rescue LoadError
+  unless Rails.env.production?
+    # Use Pry instead of IRB
+    silence_warnings do
+      begin
+        require 'pry'
+        IRB = Pry
+      rescue LoadError
+      end
     end
   end
 end

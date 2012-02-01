e852d5cbd53dca2202303737ed307d1506546ec1
Paolo Zaccagnini <paozac@gmail.com>
2012-02-01 16:14:46 +0100

    admin can change other users role

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,41 +4,43 @@ module Admin
     def index
       @users = User.all
     end
-    
+
     def show
     end
-    
+
     def edit
     end
-    
+
     def update
       if @user.update_attributes(params[:user])
+        @user.role_id = params[:user][:role_id] and @user.save if current_user.try(:admin?)
         redirect_to [:admin, @user], :notice => 'User updated'
       else
         render :edit
       end
     end
-    
+
     def new
       @user = User.new
     end
-    
+
     def create
       @user = User.new params[:user]
       if @user.save
+        @user.role_id = params[:user][:role_id] and @user.save if current_user.try(:admin?)
         redirect_to [:admin, @user], :notice => 'User added'
       else
         render :new
       end
     end
-    
+
     def destroy
       @user.destroy
       redirect_to admin_users_path, :notice => 'User deleted'
     end
-    
+
     private
-    
+
       def find_user
         @user = User.find params[:id]
       end

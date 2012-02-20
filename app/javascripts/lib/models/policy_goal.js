908e1c9a421fd85fb0696531576821fde1fae69f
Paolo Zaccagnini <paozac@gmail.com>
2012-02-20 17:06:39 +0100

    policy goals: added score calculation
    refactoring

--- app/javascripts/lib/models/policy_goal.js
+++ app/javascripts/lib/models/policy_goal.js
@@ -1,37 +1,41 @@
 var PolicyGoal = Backbone.Model.extend({
   initialize : function() {
     window.policy_goals.add(this);
-    
+
     this.success_query    = new Gquery({ key: this.get("success_query")});
     this.value_query      = new Gquery({ key: this.get("value_query")});
     this.target_query     = new Gquery({ key: this.get("target_query")});
     this.user_value_query = new Gquery({ key: this.get("user_value_query")});
   },
-  
+
   // goal achieved? true/false
   success_value : function() {
-    return this.success_query.get('future_value');;
+    return this.success_query.get('future_value');
   },
-  
+
   successful : function() {
     return (this.success_value() === true);
   },
 
   // numeric value
   current_value : function() {
-    return this.value_query.get('future_value');;
+    return this.value_query.get('future_value');
   },
-  
+
+  start_value: function() {
+    return this.value_query.get('present_value');
+  },
+
   // goal, numeric value
   target_value : function() {
-    return this.target_query.get('future_value');;
+    return this.target_query.get('future_value');
   },
-  
+
   // returns true if the user has set a goal
   is_set : function() {
-    return this.user_value_query.get('future_value');;
+    return this.user_value_query.get('future_value');
   },
-  
+
   // DEBT: we could use a BB view
   update_view : function() {
     var success = this.successful();
@@ -46,18 +50,19 @@ var PolicyGoal = Backbone.Model.extend({
     } else {
       this.dom_element().find(".target").html(I18n.t('policy_goals.not_set'));
     }
-    
+
     var current_value = this.format_value(this.current_value());
-    this.dom_element().find(".you").html(current_value);    
+    this.dom_element().find(".you").html(current_value);
+    console.log("Score for " + this.get('key') + ': ' + this.score());
   },
-  
+
   dom_element : function() {
     return $("#goal_" + this.get("goal_id"));
   },
-  
+
   format_value : function(n) {
     var out = null;
-    
+
     switch(this.get('display_fmt')) {
       case 'percentage' :
         out = Metric.ratio_as_percentage(n, false, 2);
@@ -72,8 +77,26 @@ var PolicyGoal = Backbone.Model.extend({
         out = n;
     }
     return out;
+  },
+
+  score: function() {a
+    if (!this.is_set()) return false;
+    var base = this.start_value();
+    var current = this.current_value();
+    var target = this.target_value();
+    var ampl = 100;
+    var t = current - base;
+    var a = 2 * Math.abs(base - target);
+    var score = 2 * ampl * Math.abs( (t / a) - Math.floor( (t / a) + 0.5));
+    
+    if(t > a || t < 0){ score *= -1; }
+   
+    if((t < -0.5 * a) || (t > 1.5 * a)){ score = -100; }
+
+    return score;
+ 
   }
-  
+
 });
 
 var PolicyGoalList = Backbone.Collection.extend({
--- app/models/policy_goal.rb
+++ app/models/policy_goal.rb
@@ -16,14 +16,30 @@
 
 class PolicyGoal < ActiveRecord::Base
   include AreaDependent
+  include GqlHelper
 
   has_one :area_dependency, :as => :dependable, :dependent => :destroy
   has_paper_trail
-  
+
   accepts_nested_attributes_for :area_dependency
 
   scope :gquery_contains, lambda{|search| where([
     "query LIKE :q OR reached_query LIKE :q OR target_query LIKE :q OR user_value_query LIKE :q",
     {:q => "%#{search}%"}]
   )}
+
+  # attributes used by the backbone js object def js_attributes
+  def js_attributes
+    {
+      :goal_id          => id,
+      :name             => I18n.t("policy_goals.#{key}"),
+      :success_query    => gquery_id(reached_query).to_s,
+      :value_query      => gquery_id(query).to_s,
+      :target_query     => gquery_id(target_query).to_s,
+      :user_value_query => gquery_id(user_value_query).to_s,
+      :unit             => unit,
+      :display_fmt      => display_format,
+      :key              => key
+    }
+  end
 end

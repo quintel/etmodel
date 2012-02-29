94bbd37fdd71f2cc4312f2ce519719135cdc985c
Paolo Zaccagnini <paozac@gmail.com>
2012-02-29 10:16:05 +0100

    sphinx: capistrano cleanup. updated server configuration

--- lib/capistrano/sphinx.rb
+++ lib/capistrano/sphinx.rb
@@ -1,9 +1,10 @@
 namespace :sphinx do
   desc "Link up Sphinx's indexes."
-  task :symlink_indexes, :roles => [:app] do
+  task :symlink_stuff, :roles => [:app] do
     run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+    run "ln -s #{shared_path}/config/sphinx.yml #{release_path}/config/"
   end
-  
+
   desc "Rebuild and restart"
   task :rebuild_and_restart, :roles => [:app] do
     # The silly TS capistrano recipes are repeating some steps twice
@@ -11,11 +12,8 @@ namespace :sphinx do
     rake_on_current "thinking_sphinx:configure"
     rake_on_current "thinking_sphinx:index"
     rake_on_current "thinking_sphinx:start"
-    # thinking_sphinx.configure
-    # thinking_sphinx.index
-    # thinking_sphinx.start
   end
-  
+
   task :stop_gracefully do
     begin
       rake_on_current "thinking_sphinx:stop"

5f1f20197bef47eaa65f2fc209f76e3ee3d9476b
Paolo Zaccagnini <paozac@gmail.com>
2012-03-01 13:05:08 +0100

    charts: converted vertical_stacked_bar_chart to cs

--- app/javascripts/lib/views/vertical_stacked_bar_chart_view.js
+++ app/javascripts/lib/views/vertical_stacked_bar_chart_view.js
@@ -1,48 +1,50 @@
+/* DO NOT MODIFY. This file was compiled Thu, 01 Mar 2012 12:04:30 GMT from
+ * /Users/paozac/Sites/etmodel/app/coffeescripts/lib/views/vertical_stacked_bar_chart_view.coffee
+ */
 
-var VerticalStackedBarChartView = BaseChartView.extend({
-  initialize : function() {
-    this.initialize_defaults();
-  },
+(function() {
 
-  render : function() {
-    this.clear_container();
-    InitializeVerticalStackedBar(this.model.get("container"), 
-      this.results(), 
-      this.ticks(),
-      this.filler(),
-      this.model.get('show_point_label'),
-      this.parsed_unit(),
-      this.model.colors(),
-      this.model.labels());
-  },
-
-  results : function() {
-    var results = this.results_without_targets();
-    var smallest_scale = null; 
-
-    if (!this.model.get('percentage')) {
-      results = _.map(results, function(x) {
-        return _.map(x, function(value) {
-          return value;
+  this.VerticalStackedBarChartView = BaseChartView.extend({
+    initialize: function() {
+      return this.initialize_defaults();
+    },
+    render: function() {
+      this.clear_container();
+      return InitializeVerticalStackedBar(this.model.get("container"), this.results(), this.ticks(), this.filler(), this.model.get('show_point_label'), this.parsed_unit(), this.model.colors(), this.model.labels());
+    },
+    results: function() {
+      var result, results, serie, smallest_scale, x, _i, _len, _ref;
+      results = this.results_without_targets();
+      smallest_scale = null;
+      if (!this.model.get('percentage')) {
+        results = _.map(results, function(x) {
+          return _.map(x, function(value) {
+            return value;
+          });
         });
+      }
+      _ref = this.model.target_series();
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        serie = _ref[_i];
+        result = serie.result()[0][1];
+        x = parseFloat(serie.get('target_line_position'));
+        results.push([[x - 0.4, result], [x + 0.4, result]]);
+      }
+      return results;
+    },
+    results_without_targets: function() {
+      return _.map(this.model.non_target_series(), function(serie) {
+        return serie.result_pairs();
       });
+    },
+    filler: function() {
+      return _.map(this.model.non_target_series(), function(serie) {
+        return {};
+      });
+    },
+    ticks: function() {
+      return [App.settings.get("start_year"), App.settings.get("end_year")];
     }
+  });
 
-    _.each(this.model.target_series(), function(serie) {
-      var result = serie.result()[0][1]; // target_series has only present or future value
-      var x = parseFloat(serie.get('target_line_position'));
-      results.push([[x - 0.4, result], [x + 0.4, result]]);
-    });
-    return results;
-  },
-
-  results_without_targets : function() {
-    return _.map(this.model.non_target_series(), function(serie) { return serie.result_pairs(); });
-  },
-  filler : function() {
-    return _.map(this.model.non_target_series(), function(serie) { return {}; });
-  },
-  ticks : function() {
-    return [App.settings.get("start_year"), App.settings.get("end_year")];
-  }
-});
+}).call(this);

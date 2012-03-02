3cc12e438afa5478931b691bce874607743c0e2f
Paolo Zaccagnini <paozac@gmail.com>
2012-03-02 14:56:53 +0100

    charts: converted series models

---
+++ app/coffeescripts/lib/models/block_chart_serie.coffee
@@ -0,0 +1,21 @@
+class @BlockChartSerie extends Backbone.Model
+  initialize : ->
+    # the block chart has a different behavious; its gqueries follow a naming
+    # convention
+    key = @get 'gquery_key'
+    cost_gql_query = "costs_of_#{key}_in_overview_costs_of_electricity_production"
+    investment_gql_query = "investment_for_#{key}_in_overview_costs_of_electricity_production"
+
+    @set
+      gquery_cost :       new Gquery({key : cost_gql_query}),
+      gquery_investment : new Gquery({key : investment_gql_query})
+
+  result : ->
+    return [
+      @get('id'),
+      @get('gquery_cost').get('present_value'),
+      @get('gquery_investment').get('present_value')
+    ]
+
+class @BlockChartSeries extends Backbone.Collection
+  model : BlockChartSerie
--- app/javascripts/lib/models/chart_serie.js
+++ app/javascripts/lib/models/chart_serie.js
@@ -1,19 +1,55 @@
-var ChartSerie = Backbone.Model.extend({
-  initialize : function() {
-    var gquery = new Gquery({key : this.get('gquery_key')});
-    this.set({gquery : gquery});
-  },
+/* DO NOT MODIFY. This file was compiled Fri, 02 Mar 2012 13:56:03 GMT from
+ * /Users/paozac/Sites/etmodel/app/coffeescripts/lib/models/chart_serie.coffee
+ */
 
-  result : function() {
-    return this.get('gquery').result();
-  },
+(function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
-  result_pairs : function() {
-    var result = this.result();
-    return [result[0][1],result[1][1]];
-  }
-});
+  this.ChartSerie = (function(_super) {
 
-var ChartSeries = Backbone.Collection.extend({
-  model : ChartSerie
-});
+    __extends(ChartSerie, _super);
+
+    function ChartSerie() {
+      ChartSerie.__super__.constructor.apply(this, arguments);
+    }
+
+    ChartSerie.prototype.initialize = function() {
+      var gquery;
+      gquery = new Gquery({
+        key: this.get('gquery_key')
+      });
+      return this.set({
+        gquery: gquery
+      });
+    };
+
+    ChartSerie.prototype.result = function() {
+      return this.get('gquery').result();
+    };
+
+    ChartSerie.prototype.result_pairs = function() {
+      var result;
+      result = this.result();
+      return [result[0][1], result[1][1]];
+    };
+
+    return ChartSerie;
+
+  })(Backbone.Model);
+
+  this.ChartSeries = (function(_super) {
+
+    __extends(ChartSeries, _super);
+
+    function ChartSeries() {
+      ChartSeries.__super__.constructor.apply(this, arguments);
+    }
+
+    ChartSeries.prototype.model = ChartSerie;
+
+    return ChartSeries;
+
+  })(Backbone.Collection);
+
+}).call(this);

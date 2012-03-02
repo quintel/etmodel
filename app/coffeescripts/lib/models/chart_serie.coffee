a7f655262bdb35f0db0009092f63eee04676a131
Paolo Zaccagnini <paozac@gmail.com>
2012-03-02 14:56:53 +0100

    charts: converted series models

---
+++ app/coffeescripts/lib/models/chart_serie.coffee
@@ -0,0 +1,14 @@
+class @ChartSerie extends Backbone.Model
+  initialize : ->
+    gquery = new Gquery({key : this.get('gquery_key')})
+    @set({gquery : gquery})
+
+  result: ->
+    @get('gquery').result()
+
+  result_pairs: ->
+    result = @result()
+    [result[0][1],result[1][1]]
+
+class @ChartSeries extends Backbone.Collection
+  model : ChartSerie
--- app/javascripts/lib/models/block_chart_serie.js
+++ app/javascripts/lib/models/block_chart_serie.js
@@ -1,25 +1,54 @@
-var BlockChartSerie = Backbone.Model.extend({
-  initialize : function() {
-    // the block chart has a different behavious; its gqueries follow a naming
-    // convention
-    var cost_gql_query       = "costs_of_"      + this.get('gquery_key') + "_in_overview_costs_of_electricity_production";
-    var investment_gql_query = "investment_for_" + this.get('gquery_key') + "_in_overview_costs_of_electricity_production";
+/* DO NOT MODIFY. This file was compiled Fri, 02 Mar 2012 13:56:03 GMT from
+ * /Users/paozac/Sites/etmodel/app/coffeescripts/lib/models/block_chart_serie.coffee
+ */
 
-    this.set({
-      gquery_cost :       new Gquery({key : cost_gql_query}),
-      gquery_investment : new Gquery({key : investment_gql_query})
-    });
-  },
-  
-  result : function() {
-    return [
-      this.get('id'), 
-      this.get('gquery_cost').get('present_value'), 
-      this.get('gquery_investment').get('present_value')
-    ];
-  }
-});
+(function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
-var BlockChartSeries = Backbone.Collection.extend({
-  model : BlockChartSerie
-});
+  this.BlockChartSerie = (function(_super) {
+
+    __extends(BlockChartSerie, _super);
+
+    function BlockChartSerie() {
+      BlockChartSerie.__super__.constructor.apply(this, arguments);
+    }
+
+    BlockChartSerie.prototype.initialize = function() {
+      var cost_gql_query, investment_gql_query, key;
+      key = this.get('gquery_key');
+      cost_gql_query = "costs_of_" + key + "_in_overview_costs_of_electricity_production";
+      investment_gql_query = "investment_for_" + key + "_in_overview_costs_of_electricity_production";
+      return this.set({
+        gquery_cost: new Gquery({
+          key: cost_gql_query
+        }),
+        gquery_investment: new Gquery({
+          key: investment_gql_query
+        })
+      });
+    };
+
+    BlockChartSerie.prototype.result = function() {
+      return [this.get('id'), this.get('gquery_cost').get('present_value'), this.get('gquery_investment').get('present_value')];
+    };
+
+    return BlockChartSerie;
+
+  })(Backbone.Model);
+
+  this.BlockChartSeries = (function(_super) {
+
+    __extends(BlockChartSeries, _super);
+
+    function BlockChartSeries() {
+      BlockChartSeries.__super__.constructor.apply(this, arguments);
+    }
+
+    BlockChartSeries.prototype.model = BlockChartSerie;
+
+    return BlockChartSeries;
+
+  })(Backbone.Collection);
+
+}).call(this);

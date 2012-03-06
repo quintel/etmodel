5d08dfbb806eaf1d882db089009036c19274e838
Paolo Zaccagnini <paozac@gmail.com>
2012-03-06 13:36:25 +0100

    charts: converted bezier chart to cs

--- app/javascripts/lib/views/bezier_chart_view.js
+++ app/javascripts/lib/views/bezier_chart_view.js
@@ -1,4 +1,4 @@
-/* DO NOT MODIFY. This file was compiled Thu, 01 Mar 2012 14:34:15 GMT from
+/* DO NOT MODIFY. This file was compiled Tue, 06 Mar 2012 12:33:47 GMT from
  * /Users/paozac/Sites/etmodel/app/coffeescripts/lib/views/bezier_chart_view.coffee
  */
 
@@ -12,6 +12,8 @@
     __extends(BezierChartView, _super);
 
     function BezierChartView() {
+      this.chart_opts = __bind(this.chart_opts, this);
+      this.render_chart = __bind(this.render_chart, this);
       this.render = __bind(this.render, this);
       BezierChartView.__super__.constructor.apply(this, arguments);
     }
@@ -22,7 +24,7 @@
 
     BezierChartView.prototype.render = function() {
       this.clear_container();
-      return InitializeBezier(this.model.get("container"), this.scaled_results(), this.model.get("growth_chart"), this.parsed_unit(), this.model.colors(), this.model.labels());
+      return this.render_chart();
     };
 
     BezierChartView.prototype.scaled_results = function() {
@@ -33,6 +35,102 @@
           return [pair[0], Metric.scale_value(pair[1], scale)];
         });
       });
+    };
+
+    BezierChartView.prototype.formatted_results = function() {
+      return this.plot_series(this.scaled_results(), App.settings.get('start_year'), App.settings.get('end_year'), this.model.get('growth_chart'));
+    };
+
+    BezierChartView.prototype.render_chart = function() {
+      return $.jqplot(this.container_id(), this.formatted_results(), this.chart_opts());
+    };
+
+    BezierChartView.prototype.chart_opts = function() {
+      return {
+        grid: this.defaults.grid,
+        legend: this.create_legend({
+          num_columns: 2
+        }),
+        stackSeries: false,
+        seriesDefaults: {
+          renderer: $.jqplot.BezierCurveRenderer,
+          pointLabels: {
+            show: false
+          },
+          yaxis: 'y2axis'
+        },
+        seriesColors: this.model.colors(),
+        axesDefaults: this.defaults.stacked_line_axis_default,
+        axes: {
+          xaxis: {
+            numberTicks: 2,
+            tickOptions: {
+              showGridline: false
+            },
+            ticks: [App.settings.get('start_year'), App.settings.get('end_year')]
+          },
+          y2axis: {
+            rendererOptions: {
+              forceTickAt0: true
+            },
+            borderColor: '#CCCCCC',
+            tickOptions: {
+              formatString: "%.0f" + (this.parsed_unit())
+            }
+          }
+        }
+      };
+    };
+
+    BezierChartView.prototype.plot_series = function(series, start_x, end_x, growth) {
+      var end_value, end_y, result, serie, start_value, start_y, _i, _len;
+      result = [];
+      start_value = 0;
+      end_value = 0;
+      for (_i = 0, _len = series.length; _i < _len; _i++) {
+        serie = series[_i];
+        start_y = serie[0][1];
+        start_value += start_y;
+        end_y = serie[1][1];
+        end_value += end_y;
+        if (growth) {
+          if (start_value > end_value) {
+            result.push([[start_x, start_value], this.set_decrease_ex_curve(start_x, end_x, start_value, end_value).concat([end_x, end_value])]);
+          } else {
+            result.push([[start_x, start_value], this.set_growth_ex_curve(start_x, end_x, start_value, end_value).concat([end_x, end_value])]);
+          }
+        } else {
+          result.push([[start_x, start_value], this.set_s_curve(start_x, end_x, start_value, end_value).concat([end_x, end_value])]);
+        }
+      }
+      return result;
+    };
+
+    BezierChartView.prototype.set_s_curve = function(start_x, end_x, start_y, end_y) {
+      var end_handle_x, end_handle_y, start_handle_x, start_handle_y;
+      start_handle_x = start_x + ((end_x - start_x) / 2);
+      start_handle_y = start_y;
+      end_handle_x = end_x - ((end_x - start_x) / 4);
+      end_handle_y = end_y;
+      return [start_handle_x, start_handle_y, end_handle_x, end_handle_y];
+    };
+
+    BezierChartView.prototype.set_growth_ex_curve = function(start_x, end_x, start_y, end_y) {
+      var end_handle_x, end_handle_y, start_handle_x, start_handle_y;
+      start_handle_x = start_x + ((end_x - start_x) / 2);
+      start_handle_y = start_y;
+      end_handle_x = end_x;
+      end_handle_y = end_y;
+      return [start_handle_x, start_handle_y, end_handle_x, end_handle_y];
+    };
+
+    BezierChartView.prototype.set_decrease_ex_curve = function(start_x, end_x, start_y, end_y) {
+      var end_handle_x, end_handle_y, start_handle_x, start_handle_y;
+      start_handle_x = start_x + ((end_x - start_x) / 2);
+      start_handle_y = end_y;
+      end_handle_x = end_x;
+      end_handle_y = end_y;
+      return [start_handle_x, start_handle_y, end_handle_x, end_handle_y];
     };
 
     return BezierChartView;

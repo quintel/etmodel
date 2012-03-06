894560b29f40559eb6b6eb90710ef68ffab40499
Paolo Zaccagnini <paozac@gmail.com>
2012-03-06 10:51:41 +0100

    charts: converted policy bar

--- app/javascripts/output_elements.js
+++ app/javascripts/output_elements.js
@@ -1,5 +1,4 @@
 
 //= require <old/etm.output_elements>
-//= require <old/output_elements/etm.output_elements.policy_bar>
 //= require <old/output_elements/etm.output_elements.vertical_stacked_bar>
 //= require <old/output_elements/etm.output_elements.bezier>

4dfb2b9e40bfe9f77e46a0bf42f57fa3f5e84cbe
Paolo Zaccagnini <paozac@gmail.com>
2012-03-19 13:52:51 +0100

    search: fail gracefully if TS fails

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,15 @@
 class SearchController < ApplicationController
-  # Are we sure we want to use the etm layout? Maybe we should only if the
-  # user has already started working on a scenario - PZ Mon 4 Jul 2011 10:45:00 CEST
   layout 'pages'
 
   include ApplicationController::HasDashboard
-  
+
   def index
     @query = params[:q]
 
-    @pages          = PageTitle.search(@query).compact
-    @input_elements = InputElement.search(@query).compact
-    @slides         = Slide.search(@query).compact
+    # the rescue clauses are used to hide Riddle::ConnectionError
+    # exceptions from the user
+    @pages          = PageTitle.search(@query).compact rescue []
+    @input_elements = InputElement.search(@query).compact rescue []
+    @slides         = Slide.search(@query).compact rescue []
   end
 end

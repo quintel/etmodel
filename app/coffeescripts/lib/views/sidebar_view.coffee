7686dcf9d35670724b70cb4e3987ad83e1391e99
Paolo Zaccagnini <paozac@gmail.com>
2012-03-20 15:29:10 +0100

    sidebar: converted to cs and added animation

---
+++ app/coffeescripts/lib/views/sidebar_view.coffee
@@ -0,0 +1,19 @@
+class @SidebarView extends Backbone.View
+  bootstrap: ->
+    for item in $("#sidebar ul li")
+      gquery = $(item).attr('data-gquery')
+      new Gquery({key: gquery}) if gquery
+
+  update_bars: ->
+    for item in $("#sidebar ul li")
+      key = $(item).attr('data-gquery')
+      next unless key
+      gquery = gqueries.with_key(key)[0]
+      return unless gquery
+      result = gquery.get('future_value')
+      percentage = "#{Math.round(result * 100)}%"
+      padded_percentage = "#{Math.round(result * 90)}%"
+      $(item).find(".bar").animate({width: padded_percentage})
+      $(item).find(".value").html(percentage).animate({left: padded_percentage})
+
+window.sidebar = new SidebarView()
--- app/javascripts/lib/views/sidebar_view.js
+++ app/javascripts/lib/views/sidebar_view.js
@@ -1,31 +1,62 @@
-var SidebarView = Backbone.View.extend({
-  bootstrap: function() {
-    _.each(
-      $("#sidebar ul li"), function(item){
-        var gquery = $(item).attr('data-gquery');
-        if(gquery) {
-          new Gquery({key: gquery});
+/* DO NOT MODIFY. This file was compiled Tue, 20 Mar 2012 14:27:01 GMT from
+ * /Users/paozac/Sites/etmodel/app/coffeescripts/lib/views/sidebar_view.coffee
+ */
+
+(function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  this.SidebarView = (function(_super) {
+
+    __extends(SidebarView, _super);
+
+    function SidebarView() {
+      SidebarView.__super__.constructor.apply(this, arguments);
+    }
+
+    SidebarView.prototype.bootstrap = function() {
+      var gquery, item, _i, _len, _ref, _results;
+      _ref = $("#sidebar ul li");
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        item = _ref[_i];
+        gquery = $(item).attr('data-gquery');
+        if (gquery) {
+          _results.push(new Gquery({
+            key: gquery
+          }));
+        } else {
+          _results.push(void 0);
         }
       }
-    );
-  },
+      return _results;
+    };
 
-  update_bars: function() {
-    _.each(
-      $("#sidebar ul li"), function(item){
-        var key = $(item).attr('data-gquery');
-        if(key) {
-          var gquery = gqueries.with_key(key)[0];
-          if (!gquery) return;
-          var result = gquery.get('future_value');
-          var percentage = "" + Math.round(result * 100) + "%";
-          var padded_percentage = "" + Math.round(result * 90) + "%";
-          $(item).find(".bar").css('width', padded_percentage);
-          $(item).find(".value").html(percentage).css('left', padded_percentage);
-        }
+    SidebarView.prototype.update_bars = function() {
+      var gquery, item, key, padded_percentage, percentage, result, _i, _len, _ref;
+      _ref = $("#sidebar ul li");
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        item = _ref[_i];
+        key = $(item).attr('data-gquery');
+        if (!key) next;
+        gquery = gqueries.with_key(key)[0];
+        if (!gquery) return;
+        result = gquery.get('future_value');
+        percentage = "" + (Math.round(result * 100)) + "%";
+        padded_percentage = "" + (Math.round(result * 90)) + "%";
+        $(item).find(".bar").animate({
+          width: padded_percentage
+        });
+        $(item).find(".value").html(percentage).animate({
+          left: padded_percentage
+        });
       }
-    );
-  }
-});
+    };
 
-window.sidebar = new SidebarView();
+    return SidebarView;
+
+  })(Backbone.View);
+
+  window.sidebar = new SidebarView();
+
+}).call(this);

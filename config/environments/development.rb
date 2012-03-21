66f27d4606b0c6333c7ea3a93148c8b80a638c7f
Paolo Zaccagnini <paozac@gmail.com>
2012-03-21 14:24:47 +0100

    disabled caching in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,13 +12,12 @@ Etm::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local = true
-  config.action_view.debug_rjs                         = true
+  config.action_view.debug_rjs = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-
-  config.action_controller.perform_caching             = true
+  config.action_controller.perform_caching = false
   #config.cache_store = :memory_store
   config.cache_store = :mem_cache_store
   #config.cache_store = :file_store, '/tmp/cache'

fb15885ed959758d3e36831f1a3712345a909503
Paolo Zaccagnini <paozac@gmail.com>
2012-03-22 12:52:11 +0100

    TRASH: scenario#edit and #udpate
    not used

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,14 +1,9 @@
 class ScenariosController < ApplicationController
   layout 'pages'
-  helper :all
 
   before_filter :ensure_valid_browser
-  before_filter :find_scenario, :only => [:show, :load, :edit]
+  before_filter :find_scenario, :only => [:show, :load]
   before_filter :require_user, :only => [:index, :new]
-
-  # included here, so that we don't mess with the before_filter order
-  include ApplicationController::HasDashboard
-
 
   def index
     if current_user.admin?
@@ -20,12 +15,7 @@ class ScenariosController < ApplicationController
     end
   end
 
-  def edit
-
-  end
-
   def show
-
   end
 
   def new
@@ -44,87 +34,33 @@ class ScenariosController < ApplicationController
   #
   # POST /scenarios/
   #
-  # @tested 2010-12-22 jape
-  #
   def create
     @scenario = Api::Scenario.create(params[:saved_scenario])
     attributes = {:scenario_id => @scenario.id}
     @saved_scenario = current_user.saved_scenarios.create!(attributes)
-    redirect_to scenarios_url
+    redirect_to scenarios_path
   end
 
   # Loads a scenario from a id.
   #
   # GET /scenarios/:id/load
   #
-  # @tested 2010-12-22 jape
-  #
   def load
-    # TODO: Check if this is the correct behaviour.
-    # when the user loads a predefined scenario or opens a scenario he's
-    # been playing with on the mixer he has to work with a copy, right?
-    # I have therefore added the clone parameter (which is handled on the ETE)
-    # The Scenario life-cycle is convoluted - PZ Tue 31 May 2011 16:11:52 CEST
-    #
-    # => ANSWER: by setting api_session_id to nil (done in load_from_scenario)
-    #            a new ETengine scenario session will be created, and Scenario is untouched.
-    #
     if @scenario.nil?
       redirect_to start_path, :notice => "Scenario not found" and return
     end
     Current.setting = Setting.load_from_scenario(@scenario)
+    # more logic could be added here, such as setting the round only if the
+    # scenario name contains the "wattnu" string or something like that
     Current.setting.current_round = 3 if wattnu?
     redirect_to start_path
   end
-
 
   def change_complexity
     # DEBT: Remove. Should be in settings controller, in the standard update action
     Current.setting.complexity = params[:scenario][:complexity]
     redirect_to_back
   end
-
-  ##############################
-  # Mixed resource methods
-  # These methods can be used for both a singular as well as a plural resource.
-  ##############################
-
-
-  ##
-  # Update is used for Current.scenario as well as params[:id].
-  #
-  # PUT /scenarios/:id
-  # PUT /scenario
-  #
-  # @tested 2010-12-22 jape
-  #
-  def update
-    if request.put?
-      flash[:notice] = t('flash.succesfully_saved')
-
-      @scenario.attributes = params[:scenario]
-      @scenario.copy_scenario_state(Current.scenario)
-      @scenario.save
-      redirect_to_back
-    end
-  end
-
-
-  ##
-  # Edit the current scenario.
-  #
-  # GET /scenarios/:id/edit
-  # GET /scenario/edit
-  #
-  # Is this still used? The page seems broken
-  # - PZ Thu 15 Dec 2011 15:54:31 CET
-  def edit
-    if (!current_user || @scenario.user.nil? || @scenario.user.id != current_user.id)
-      flash[:alert] = "Permission denied"
-      redirect_to_back
-    end
-  end
-
 
   private
 

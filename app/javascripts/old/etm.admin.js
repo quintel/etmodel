982793906b2c61f17a7318a6927af554306ef646
Paolo Zaccagnini <paozac@gmail.com>
2012-03-23 15:00:08 +0100

    table charts: warning on console if gquery is missing

--- app/javascripts/old/etm.admin.js
+++ app/javascripts/old/etm.admin.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  console.log('ready')
-  $('#output_element_serie_color').live('change',function(){      
+  $('#output_element_serie_color').live('change',function(){
     $('.color_hint').css("background-color",$(this).attr('value'));
   });
 });

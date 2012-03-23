2a5d49d550b6280d6e08f8d530994caa02763265
Paolo Zaccagnini <paozac@gmail.com>
2012-03-23 16:00:18 +0100

    predictions: use the new area_code setting

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,9 +1,9 @@
 class PredictionsController < ApplicationController
   before_filter :find_input_element, :only => :index
   before_filter :find_prediction, :only => [:show, :comment, :share]
-  
+
   def index
-    @predictions = @input_element.available_predictions(Current.setting.region)
+    @predictions = @input_element.available_predictions(Current.setting.area_code)
     @prediction  = @predictions.find(params[:prediction_id]) rescue @predictions.first
     @comment = Comment.new
     @comment.commentable = @prediction
@@ -11,7 +11,7 @@ class PredictionsController < ApplicationController
 
     render :layout => 'iframe'
   end
-  
+
   def share
     # Set the locale to nl untill en translations are available
     if I18n.locale.to_s == 'en'
@@ -20,34 +20,32 @@ class PredictionsController < ApplicationController
     end
     @input_element = @prediction.input_element
     @predictions = @input_element.available_predictions('nl') # the area should probably be include inside the url when more then 1 area is available
-    logger.info "predictions #{@predictions.inspect}"
     @comment = Comment.new
     @comment.commentable = @prediction
     @end_year = 2050
     render :layout => 'pages', :action => 'index'
   end
-  
+
   def show
     I18n.locale = 'nl'
-    
     @input_element = @prediction.input_element
     @comment = Comment.new
     @comment.commentable = @prediction
     render :layout => false if request.xhr?
   end
-  
+
   def comment
     @comment = Comment.new(params[:comment])
     @comment.user_id = current_user.id if current_user
     @comment.commentable = @prediction
-    
+
     respond_to do |format|
       if @comment.save
         spawn do #use spawn to create a bg process for sending mail
           Notifier.comment_mail(@comment).deliver
         end
-  
-        format.html { redirect_to predictions_path(:input_element_id => @prediction.input_element_id, :prediction_id => @prediction.id), 
+
+        format.html { redirect_to predictions_path(:input_element_id => @prediction.input_element_id, :prediction_id => @prediction.id),
                         :notice => "Your comment has been added"}
         format.js { }
       else
@@ -57,17 +55,17 @@ class PredictionsController < ApplicationController
       end
     end
   end
-  
+
   private
-  
+
     def find_input_element
       @input_element = InputElement.find(params[:input_element_id])
     end
-    
+
     def find_prediction
       @prediction = Prediction.find(params[:id])
     end
-    
+
     def has_active_scenario?
       Current.setting.api_session_id.present? rescue false
     end

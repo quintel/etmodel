e432f4d82343cfd795935c6fe2745759b7fa5b07
Paolo Zaccagnini <paozac@gmail.com>
2012-03-26 15:32:01 +0200

    dashboard: handle gracefully gquery errors

---
+++ app/coffeescripts/lib/views/constraint_view.coffee
@@ -0,0 +1,104 @@
+class @ConstraintView extends Backbone.View
+  initialize: () =>
+    @id = "constraint_#{@model.get('id')}"
+    @dom_id = "##{@id}"
+    @element = $(@dom_id)
+    @arrow_element = $('.arrow', @dom_id)
+    @element.bind('mousedown', @open_popup)
+    @model.bind('change:result', @render)
+    @model.view = this
+
+  render: () =>
+    if(@model.get("key") == 'total_energy_cost')
+      @render_total_cost_label()
+    formatted_value = @format_result()
+    $('strong', @dom_id).empty().append(formatted_value)
+    @updateArrows()
+    this
+
+  # different behaviour unfortunately
+  render_total_cost_label: () =>
+    return '' if @model.error()
+    value = @model.result() * 1000000000
+    scale = Metric.power_of_thousand(value)
+    unit  = I18n.t('units.currency.' + Metric.power_of_thousand_to_string(scale))
+    label = "(#{unit})"
+    $('.header .sub_header', @dom_id).html(label)
+
+  open_popup: () =>
+    constraint = $(@dom_id)
+    constraint_id = @model.get('id')
+    key = @model.get('key')
+    # TODO: define the popup height somewhere else or increase it for all popups
+    popup_height = if key == 'loss_of_load' then 435 else 400
+    url = $(constraint).attr('href')+"?t="+timestamp()
+    $(constraint).fancybox
+      href: url
+      type: 'iframe'
+      height: popup_height
+      width: 600
+      padding: 0
+
+
+  # Formats the result of calculate_result() for the end-user
+  format_result: () =>
+    result = @model.result()
+    key    = @model.get('key')
+    return '-' if @model.error();
+
+    out = switch key
+      when 'total_energy_cost'
+        Metric.euros_to_string(result * 1000000000)
+      when 'household_energy_cost'
+        I18n.toCurrency(result, { precision: 0, unit: '&euro;' })
+      when 'total_primary_energy', 'employment', 'co2_reduction'
+        # show + prefix if needed
+        Metric.ratio_as_percentage(result, true)
+      when 'net_energy_import'
+        # 1 point precision
+        Metric.ratio_as_percentage(result, false, 1)
+      when 'security_of_supply'
+        Metric.round_number(result, 1)
+      when 'renewable_percentage'
+        Metric.ratio_as_percentage(result)
+      when 'bio_footprint'
+        "#{Metric.round_number(result, 1)}x#{App.settings.country().toUpperCase()}"
+      when 'targets_met'
+        null
+      when 'loss_of_load'
+        Metric.ratio_as_percentage(result, false, 1)
+      when 'renewable_electricity_percentage'
+        Metric.ratio_as_percentage(result)
+      when 'score'
+        parseInt(result,10)
+      else
+        result
+    out
+  # Updates the arrows, if the difference is negative .
+  # @param diff - the difference of old_value and new_value.
+  updateArrows: () =>
+    diff = @model.calculate_diff @model.get('result'), @model.get('previous_result')
+    return false if (diff == undefined || diff == null)
+    arrow_element = $('.arrow', @dom_id)
+    @cleanArrows()
+    if diff > 0
+      newClass = 'arrow_up'
+    else if diff < 0
+      newClass = 'arrow_down'
+    else
+      newClass = 'arrow_neutral'
+
+    arrow_element.addClass(newClass)
+    arrow_element.css('opacity', 1.0)
+
+    # make sure the arrows take their original form after 30 seconds
+    Util.cancelableAction("updateArrows #{@model.get('id')}",
+      => arrow_element.animate({opacity: 0.0}, 1000),
+      {'sleepTime': 30000}
+    )
+
+  cleanArrows:() =>
+    arrow_element = $('.arrow', @dom_id)
+    arrow_element.removeClass('arrow_neutral')
+    arrow_element.removeClass('arrow_down')
+    arrow_element.removeClass('arrow_up')
--- app/javascripts/lib/views/constraint_view.js
+++ app/javascripts/lib/views/constraint_view.js
@@ -1,126 +1,150 @@
-// jqPlot doesn't work when chart_wrapper is inside a element that is hidden (display: none).
-// Therefore, we have to move the .constraint_popup outside the view area (bottom: 8000px),
-// and animate the shadowbox-outer to an opacity of 0.01, because fadeIn/fadeOut sets display: none at the end.
+/* DO NOT MODIFY. This file was compiled Mon, 26 Mar 2012 13:27:33 GMT from
+ * /Users/paozac/Sites/etmodel/app/coffeescripts/lib/views/constraint_view.coffee
+ */
 
-var ConstraintView = Backbone.View.extend({
-  initialize : function() {
-    _.bindAll(this, 'render', 'open_popup', 'render_total_cost_label');
-    this.id = "constraint_"+this.model.get('id');
-    this.dom_id = '#'+this.id;
-    this.element = $(this.dom_id);
-    this.arrow_element = $('.arrow', this.dom_id);
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
-    this.element.bind('mousedown', this.open_popup);
+  this.ConstraintView = (function(_super) {
 
-    this.model.bind('change:result', this.render);
-    this.model.view = this;
-  },
+    __extends(ConstraintView, _super);
 
-  render : function() {
-    if(this.model.get("key") == 'total_energy_cost') {
-      this.render_total_cost_label();
+    function ConstraintView() {
+      this.cleanArrows = __bind(this.cleanArrows, this);
+      this.updateArrows = __bind(this.updateArrows, this);
+      this.format_result = __bind(this.format_result, this);
+      this.open_popup = __bind(this.open_popup, this);
+      this.render_total_cost_label = __bind(this.render_total_cost_label, this);
+      this.render = __bind(this.render, this);
+      this.initialize = __bind(this.initialize, this);
+      ConstraintView.__super__.constructor.apply(this, arguments);
     }
-    var formatted_value = this.format_result();
-    $('strong', this.dom_id).empty().append(formatted_value);
-    this.updateArrows();
-    return this;
-  },
 
-  // different behaviour unfortunately
-  render_total_cost_label : function() {
-    var value = this.model.get('result') * 1000000000;
-    var scale = Metric.power_of_thousand(value);
-    var unit  = I18n.t('units.currency.' + Metric.power_of_thousand_to_string(scale));
-    var label = "(" + unit + ")";
-    $('.header .sub_header', this.dom_id).html(label);
-  },
+    ConstraintView.prototype.initialize = function() {
+      this.id = "constraint_" + (this.model.get('id'));
+      this.dom_id = "#" + this.id;
+      this.element = $(this.dom_id);
+      this.arrow_element = $('.arrow', this.dom_id);
+      this.element.bind('mousedown', this.open_popup);
+      this.model.bind('change:result', this.render);
+      return this.model.view = this;
+    };
 
-  open_popup : function() {
-    var constraint = $(this.dom_id);
-    var constraint_id = this.model.get('id');
-    var key = this.model.get('key');
-    // TODO: define the popup height somewhere else or increase it for all popups
-    var popup_height = (key == 'loss_of_load' ? 435 : 400);
-    var url = $(constraint).attr('href')+"?t="+timestamp();
-    $(constraint).fancybox({
-      'href' : url,
-      'type' : 'iframe',
-      height: popup_height,
-      width: 600,
-      padding: 0
-    });
-  },
+    ConstraintView.prototype.render = function() {
+      var formatted_value;
+      if (this.model.get("key") === 'total_energy_cost') {
+        this.render_total_cost_label();
+      }
+      formatted_value = this.format_result();
+      $('strong', this.dom_id).empty().append(formatted_value);
+      this.updateArrows();
+      return this;
+    };
 
+    ConstraintView.prototype.render_total_cost_label = function() {
+      var label, scale, unit, value;
+      if (this.model.error()) return '';
+      value = this.model.result() * 1000000000;
+      scale = Metric.power_of_thousand(value);
+      unit = I18n.t('units.currency.' + Metric.power_of_thousand_to_string(scale));
+      label = "(" + unit + ")";
+      return $('.header .sub_header', this.dom_id).html(label);
+    };
 
-  // Formats the result of calculate_result() for the end-user
-  format_result : function() {
-    var result = this.model.get('result');
-    var key    = this.model.get('key');
+    ConstraintView.prototype.open_popup = function() {
+      var constraint, constraint_id, key, popup_height, url;
+      constraint = $(this.dom_id);
+      constraint_id = this.model.get('id');
+      key = this.model.get('key');
+      popup_height = key === 'loss_of_load' ? 435 : 400;
+      url = $(constraint).attr('href') + "?t=" + timestamp();
+      return $(constraint).fancybox({
+        href: url,
+        type: 'iframe',
+        height: popup_height,
+        width: 600,
+        padding: 0
+      });
+    };
 
-    switch(key) {
-      case 'total_energy_cost' :
-        return Metric.euros_to_string(result * 1000000000);
-      case 'household_energy_cost':
-        return I18n.toCurrency(result, { precision: 0, unit: '&euro;' });
-      case 'total_primary_energy':
-      case 'employment':
-      case 'co2_reduction':
-        // show + prefix if needed
-        return Metric.ratio_as_percentage(result, true);
-      case 'net_energy_import':
-        // 1 point precision
-        return Metric.ratio_as_percentage(result, false, 1);
-      case 'security_of_supply':
-        return Metric.round_number(result, 1);
-      case 'renewable_percentage':
-        return Metric.ratio_as_percentage(result);
-      case 'bio_footprint':
-        return '' + Metric.round_number(result, 1) +'x'+ App.settings.country().toUpperCase();
-      case 'targets_met':
-        return null;
-      case 'loss_of_load':
-        return Metric.ratio_as_percentage(result, false, 1);
-      case 'renewable_electricity_percentage':
-        return Metric.ratio_as_percentage(result);
-      case 'score':
-        return parseInt(result,10);
-      default:
-        return result;
-    }
-  },
+    ConstraintView.prototype.format_result = function() {
+      var key, out, result;
+      result = this.model.result();
+      key = this.model.get('key');
+      if (this.model.error()) return '-';
+      out = (function() {
+        switch (key) {
+          case 'total_energy_cost':
+            return Metric.euros_to_string(result * 1000000000);
+          case 'household_energy_cost':
+            return I18n.toCurrency(result, {
+              precision: 0,
+              unit: '&euro;'
+            });
+          case 'total_primary_energy':
+          case 'employment':
+          case 'co2_reduction':
+            return Metric.ratio_as_percentage(result, true);
+          case 'net_energy_import':
+            return Metric.ratio_as_percentage(result, false, 1);
+          case 'security_of_supply':
+            return Metric.round_number(result, 1);
+          case 'renewable_percentage':
+            return Metric.ratio_as_percentage(result);
+          case 'bio_footprint':
+            return "" + (Metric.round_number(result, 1)) + "x" + (App.settings.country().toUpperCase());
+          case 'targets_met':
+            return null;
+          case 'loss_of_load':
+            return Metric.ratio_as_percentage(result, false, 1);
+          case 'renewable_electricity_percentage':
+            return Metric.ratio_as_percentage(result);
+          case 'score':
+            return parseInt(result, 10);
+          default:
+            return result;
+        }
+      })();
+      return out;
+    };
 
-  /**
-   * Updates the arrows, if the difference is negative .
-   * @param diff - the difference of old_value and new_value.
-   */
-  updateArrows : function() {
-    diff = this.model.calculate_diff(this.model.get('result'),this.model.get('previous_result'));
-    if (diff == undefined || diff == null) { return false; }
-    var arrow_element = $('.arrow', this.dom_id);
-    this.cleanArrows();
-    var newClass;
-    if (diff > 0) { newClass = 'arrow_up';}
-    else if(diff < 0) { newClass = 'arrow_down'; }
-    else { newClass = 'arrow_neutral'; }
+    ConstraintView.prototype.updateArrows = function() {
+      var arrow_element, diff, newClass,
+        _this = this;
+      diff = this.model.calculate_diff(this.model.get('result'), this.model.get('previous_result'));
+      if (diff === void 0 || diff === null) return false;
+      arrow_element = $('.arrow', this.dom_id);
+      this.cleanArrows();
+      if (diff > 0) {
+        newClass = 'arrow_up';
+      } else if (diff < 0) {
+        newClass = 'arrow_down';
+      } else {
+        newClass = 'arrow_neutral';
+      }
+      arrow_element.addClass(newClass);
+      arrow_element.css('opacity', 1.0);
+      return Util.cancelableAction("updateArrows " + (this.model.get('id')), function() {
+        return arrow_element.animate({
+          opacity: 0.0
+        }, 1000);
+      }, {
+        'sleepTime': 30000
+      });
+    };
 
-    arrow_element.addClass(newClass);
-    arrow_element.css('opacity', 1.0);
+    ConstraintView.prototype.cleanArrows = function() {
+      var arrow_element;
+      arrow_element = $('.arrow', this.dom_id);
+      arrow_element.removeClass('arrow_neutral');
+      arrow_element.removeClass('arrow_down');
+      return arrow_element.removeClass('arrow_up');
+    };
 
-    // make sure the arrows take their original form after 30 seconds
-    Util.cancelableAction("updateArrows" + this.model.get('id'), $.proxy(function() {
-      arrow_element.animate({opacity: 0.0}, 1000);
-    }, this), {'sleepTime': 30000});
-  },
+    return ConstraintView;
 
-  /**
-   * Clean the arrwos
-   */
-  cleanArrows:function() {
-    var arrow_element = $('.arrow', this.dom_id);
-    arrow_element.removeClass('arrow_neutral');
-    arrow_element.removeClass('arrow_down');
-    arrow_element.removeClass('arrow_up');
-  }
+  })(Backbone.View);
 
-
-});
+}).call(this);

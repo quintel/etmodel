532f59bf3a3049d2f070cf16da39f4d03d02f678
Paolo Zaccagnini <paozac@gmail.com>
2012-04-06 13:24:53 +0200

    js: intro cleanup

--- app/assets/javascripts/init/intro.page.js
+++
@@ -1,1 +0,0 @@
-//=require <etm.intro.js>
--- app/assets/javascripts/init/pages.layout.js
+++ app/assets/javascripts/init/pages.layout.js
@@ -4,6 +4,6 @@
 //= require <flowplayer.playlist-3.0.8.min.js>
 //= require <init/always>
 //= require <old>
-
+//= require <intro>
 //= require <lib/util/util>
 

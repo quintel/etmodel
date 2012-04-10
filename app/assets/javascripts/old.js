ef679dfbff440fba770019828c46aeac09b5fcc1
Paolo Zaccagnini <paozac@gmail.com>
2012-04-10 13:15:44 +0200

    js fixes

--- app/assets/javascripts/old.js
+++
@@ -1,7 +0,0 @@
-//= require old/etm.flash
-//= require old/etm.header_menu
-//= require old/etm.house_selection_tool
-//= require old/etm
-//= require old/etm.logos
-//= require old/etm.page_title
-//= require partners

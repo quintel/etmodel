93c5b83681dff64d2cc262bc98401564eaac4bd1
Paolo Zaccagnini <paozac@gmail.com>
2012-04-10 10:12:00 +0200

    fixed tests

--- spec/models/constraint_spec.rb
+++ spec/models/constraint_spec.rb
@@ -1,52 +1,5 @@
 require 'spec_helper'
 
-# test the outputs for _constraint_6.haml
-describe "output" do
-#  before do
-#    Current.gql = @gql = load_gql_fixture
-#    Current.stub!(:gql).and_return(@gql)
-#    @constraints = Constraint.all
-#  end
-#
-#  def find_constraint(key)
-#    @constraints.detect {|c| c.key == key}
-#  end
-#
-#  describe "total_primary_energy" do
-#    subject { find_constraint(:total_primary_energy) }
-#    its(:output) { should == '0.0<small>%</small>' }
-#  end
-#
-#  describe "co2_reduction" do
-#    subject { find_constraint(:co2_reduction) }
-#    its(:output) { should == '+6.5<small>%</small>' }
-#  end
-#
-#  describe "net_energy_import" do
-#    subject { find_constraint(:net_energy_import) }
-#    its(:output) { should == '97.0<small>%</small>' }
-#  end
-#  describe "total_energy_cost" do
-#      subject { find_constraint(:total_energy_cost) }
-#      its(:output) { should == '<small>' + EURO_SIGN + '</small> 41.8' }
-#  end
-#  describe "not_shown" do
-#        subject { find_constraint(:not_shown) }
-#        its(:output) { should == '0.54<small>xNL</small>' }
-#  end
-#  describe "renewable_percentage" do
-#          subject { find_constraint(:renewable_percentage) }
-#          its(:output) { should == '4.9<small>%</small>' }
-#  end
-#  describe "targets_met" do
-#            subject { find_constraint(:targets_met) }
-#            its(:output) { should == '0 / 12' }
-#  end
-#
-end
-
-# ----------------------------------------------------------------------------
-
 describe Constraint do
   it { should validate_presence_of(:group) }
 
@@ -61,6 +14,7 @@ end
 # ----------------------------------------------------------------------------
 
 describe Constraint, '#as_json' do
+  before { Factory :constraint }
   subject { Constraint.first.as_json }
 
   it 'should remove the "constraint" root' do
@@ -77,6 +31,10 @@ end
 describe Constraint, '.for_dashboard' do
   context 'when an array of keys' do
     let(:keys) { %w( total_primary_energy co2_reduction ) }
+    before do
+      keys.each{|k| Factory :constraint, :key => k}
+    end
+
     subject { Constraint.for_dashboard(keys) }
 
     it { should have(keys.length).constraints }
@@ -93,6 +51,9 @@ describe Constraint, '.for_dashboard' do
 
   context 'when given an array with a duplicate key' do
     let(:keys) { %w( total_primary_energy co2_reduction total_primary_energy ) }
+    before do
+      keys.each{|k| Factory :constraint, :key => k}
+    end
     subject { Constraint.for_dashboard(keys) }
 
     it { should have(keys.length).constraints }
@@ -113,7 +74,7 @@ describe Constraint, '.for_dashboard' do
 
   context 'when given an array with an invalid key' do
     let(:keys) { %w( total_primary_energy does_not_exist ) }
-
+    before { Factory :constraint, :key => 'total_primary_energy'}
     it 'should raise an error' do
       expect { Constraint.for_dashboard(keys) }.to \
         raise_error(Constraint::NoSuchConstraint, /does_not_exist/)
@@ -137,6 +98,5 @@ describe Constraint, '.for_dashboard' do
         raise_error(Constraint::IllegalConstraintKey)
     end
   end
-
 end
 

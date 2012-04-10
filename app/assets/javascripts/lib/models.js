222089c791c08c95448669e91bcede8a716f5d9f
Paolo Zaccagnini <paozac@gmail.com>
2012-04-10 12:14:01 +0200

    moving js files

--- app/assets/javascripts/lib/models.js
+++ app/assets/javascripts/lib/models.js
@@ -11,17 +11,17 @@
  */
 
 
-//= require <lib/models/constraint.js>
-//= require <lib/models/chart.js>
-//= require <lib/models/chart_serie.js>
-//= require <lib/models/block_chart_serie.js>
-//= require <lib/models/scatter_chart_serie.js>
-//= require <lib/models/input_element.js>
-//= require <lib/models/input_element_balancer.js>
-//= require <lib/models/metric.js>
-//= require <lib/models/gquery.js>
-//= require <lib/models/peak_load.js>
-//= require <lib/models/scenario.js>
-//= require <lib/models/policy_goal.js>
-//= require <lib/models/setting.js>
-//= require <lib/models/tracker.js>
+//= require lib/models/constraint
+//= require lib/models/chart
+//= require lib/models/chart_serie
+//= require lib/models/block_chart_serie
+//= require lib/models/scatter_chart_serie
+//= require lib/models/input_element
+//= require lib/models/input_element_balancer
+//= require lib/models/metric
+//= require lib/models/gquery
+//= require lib/models/peak_load
+//= require lib/models/scenario
+//= require lib/models/policy_goal
+//= require lib/models/setting
+//= require lib/models/tracker

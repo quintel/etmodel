c322aed4948579240b9c54c00625cde8a474fc0d
Paolo Zaccagnini <paozac@gmail.com>
2012-04-10 12:20:54 +0200

    split etm.js and application.js

--- app/assets/javascripts/etm.js
+++ app/assets/javascripts/etm.js
@@ -1,3 +1,6 @@
+// This file contains all the stuff to run the application
+
+
 //= require jquery
 //= require jquery_ujs
 //= require underscore.min
@@ -9,11 +12,18 @@
 //= require flowplayer-3.2.6.min
 //= require lib/util/util
 //= require jquery.busybox
+//= require lib/models/metric
+//= require jqplot
 //= require lib/util/json2
+//= require jquery.quinn.min
 //= require jquery_plugins
 //= require old
+//= require lib/views
+//= require lib/models
+//= require lib/app
 //= require intro
 //= require jquery.hoverIntent.min
 //= require flowplayer.playlist-3.0.8.min
 //= require fancybox
 //= require testimonials
+//= require i18n

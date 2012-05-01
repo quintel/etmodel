6f416a42ade14e4fc7b28dfa3884e8c58ae0d824
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-05-01 17:01:00 +0200

    Improved Scenarios#show page a bit.

--- app/models/api/scenario.rb
+++ app/models/api/scenario.rb
@@ -1,8 +1,17 @@
 class Api::Scenario < ActiveResource::Base
   self.site = APP_CONFIG[:active_resource_base] || APP_CONFIG[:api_url]
   self.format = :xml
-
+  
   def wattnu?
     title && title =~ /Watt Nu/
   end
+  
+  # description for a locale is enclosed in
+  # <span class='en'>
+  # </span>
+  def description_for_locale(locale)
+    html = Nokogiri::HTML.parse(description)
+    html.css(".#{locale}").inner_html
+  end
+  
 end

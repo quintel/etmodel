832a3bf9c5bf045e1495594afca83ec8738af781
Paolo Zaccagnini <paozac@gmail.com>
2012-05-07 14:41:47 +0200

    enable asset debug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,4 +20,5 @@ Etm::Application.configure do
   #config.cache_store = :memory_store
   config.cache_store = :mem_cache_store
   #config.cache_store = :file_store, '/tmp/cache'
+  config.assets.debug = true
 end

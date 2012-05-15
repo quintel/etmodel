7e39d44d74a787e6a866f96a30403fdea7ecadf9
Paolo Zaccagnini <paozac@gmail.com>
2012-05-15 11:26:24 +0200

    fancybox fix on descriptions popup

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,7 +1,8 @@
 class TranslationsController < ApplicationController
+  layout 'iframe'
 
   #RD: REFACTOR: It does not make sense that in the first the params[:output] is used to set the type and in the second, it is params[:type].
-  
+
   def show
     @translation = Translation.where(:key => params[:id]).first
     if @translation.nil? || @translation.content.nil?

6d14b72973b88677232ccc5aec0832006b43eb80
Paolo Zaccagnini <paozac@gmail.com>
2012-07-03 15:25:53 +0200

    backcasting: fixes

--- app/assets/javascripts/backcasting.js
+++ app/assets/javascripts/backcasting.js
@@ -1,7 +1,7 @@
 //= require jqplot
 
 $(function(){
-  
+
   // Chart series methods
   // Move on the server side
   //
@@ -38,42 +38,42 @@ $(function(){
     chart_data.series.unshift(out);
     chart_data.series_options.unshift({});
   }
-  
+
   // shows the bar with the scenario end year
   var add_reference_bar = function() {
     if(scenario.end_year == 2050) { return; }
-        
+
     // get the series values. It's a doubly nested array
     var values = _.flatten(_.map(chart_data.series, function(x) {
       return _.map(x, function(y) { return y[1]; });
     }));
-    
+
     var min_value = (_.min(values) > 1) ? 0 : _.min(values)
     chart_data.series.push([[scenario.end_year, min_value],[scenario.end_year, _.max(values)]]);
     chart_data.series_options.push({ lineWidth: 3, color: "#FFA013", markerOptions: { show: false}});
   }
-  
+
   var slider_is_available = function() {
     return parent && parent.input_elements
   }
-  
+
   var set_slider_value = function(x) {
     if(!slider_is_available()) { return false; }
     return get_slider().set({ user_value : x });
   }
-  
+
   var update_input_element = function() {
     if(input_element.value_for_prediction !== false) {
       set_slider_value(input_element.value_for_prediction);
     }
   }
-  
+
   // returns the related input element
   var get_slider = function() {
     if(!slider_is_available()) { return false; }
     return parent.input_elements.get(input_element.id);
   }
-  
+
   var plot_chart = function() {
     // let's get the current slider value
     if (scenario.available){
@@ -113,12 +113,12 @@ $(function(){
     plot_chart();
   });
 
-  
+
   // interface stuff
-  
+
   // ajax loading of prediction details
   $(".clickable_prediction").click(function(){
-    
+
     var prediction_id = $(this).attr("prediction_id");
     input_element.value_for_prediction = $(this).data('slider_value');
 
@@ -130,12 +130,12 @@ $(function(){
       $(".prediction_details").empty();
       input_element.value_for_prediction = false;
       return;
-    }    
+    }
     var url = "/predictions/" + prediction_id;
     if (slider_is_available()) {
        url = url + "?end_year="+scenario.end_year;
     };
-    
+
     $(".prediction").busyBox({spinner: '<img src="/assets/layout/ajax-loader.gif" />'});
     $(".prediction_details").load(url, function() {
       $(".prediction").busyBox('close');
@@ -147,18 +147,18 @@ $(function(){
     $(this).find(".inline_description").toggle();
     $(this).toggleClass('active');
   });
-  
+
   // apply prediction
   $("input.apply_prediction").live('click', function(){
     update_input_element();
     parent.$.fancybox.close();
   });
-  
+
   // share prediction
-  $("input.share_prediction").live('click', function(){
+  $("button.share_prediction").live('click', function(){
     $(this).hide();
     $('#share_box').fadeIn('slow');
     $('#share_link').focus().select();
-    
+
   });
 });

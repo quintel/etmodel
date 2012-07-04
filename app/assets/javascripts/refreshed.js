60279eb2b984f40fa9f3bea0c180f8de549dd8a5
Anthony Williams <hi@antw.me>
2012-07-04 21:12:12 +0100

    Root: Make root page header consistent with the rest of the site.

--- app/assets/javascripts/refreshed.js
+++ app/assets/javascripts/refreshed.js
@@ -1,2 +1,10 @@
 //= require html5shiv
 //= require jquery
+
+jQuery( function() {
+    if( $.browser.msie ) {
+        // A 1px increase fixes some bad aliasing when resizing the image
+        // down to non-HiDPI resolutions.
+        $('#header_inside img[src$="@2x.png"]').attr({ width: '401' });
+    }
+} );

71ef27dbf8cd883cba3c228a151945621367758c
Paolo Zaccagnini <paozac@gmail.com>
2012-07-06 13:21:52 +0200

    charts: hide area dependent charts
    Closes #826

--- app/models/area_dependent.rb
+++ app/models/area_dependent.rb
@@ -4,4 +4,5 @@ module AreaDependent
     return false if dependent_on.blank?
     dependent_on ? !Current.setting.area.attributes[dependent_on] : false
   end
+  alias_method :not_allowed_in_this_area, :area_dependent
 end

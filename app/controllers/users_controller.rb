3659c90d18f8f54d7f2933acca1d31d16d6fa808
Paolo Zaccagnini <paozac@gmail.com>
2012-07-09 11:20:50 +0200

    users: redirect to homepage after signup. Closes #846
    Controller clean-up. No need for multiple formats. Removed unused
    actions, too.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,6 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.new
-    render :action=>"new"
-  end
-
   def new
     @user = User.new
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
   def edit
@@ -17,27 +9,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-    respond_to do |format|
-      if @user.save
-        flash[:notice] = I18n.t("flash.register")
-        format.html { redirect_to(root_url) }
-      else
-        format.html { render :controller=>"users", :action=>"new" }
-      end
+    if @user.save
+      redirect_to home_path, :notice => I18n.t("flash.register")
+    else
+      render :new
     end
   end
 
   def update
     @user = current_user
-    @user.attributes = params[:user]
-    @user.save do |result|
-      if result
-        flash[:notice] = I18n.t("flash.edit_profile")
-        format.html { redirect_to(@user) }
-      else
-        format.html { render :action => "edit" }
-      end
+    if @user.update_attributes(params[:user])
+      redirect_to edit_user_path(@user), :notice => I18n.t("flash.edit_profile")
+    else
+      render :edit
     end
   end
-
 end

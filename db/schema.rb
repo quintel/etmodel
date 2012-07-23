179c48cbd76bb3784428f432c102d178c9ef5865
Paolo Zaccagnini <paozac@gmail.com>
2012-07-23 14:05:29 +0200

    db clean-up migration
    Removed old stuff and bad references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120628134148) do
+ActiveRecord::Schema.define(:version => 20120723115153) do
 
   create_table "area_dependencies", :force => true do |t|
     t.string  "dependent_on"

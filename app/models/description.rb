e9bd7b66ccb0081540c876272545abf1dd28f339
Paolo Zaccagnini <paozac@gmail.com>
2012-07-24 10:13:02 +0200

    descriptions: added full-text indexing

--- app/models/description.rb
+++ app/models/description.rb
@@ -20,18 +20,26 @@ class Description < ActiveRecord::Base
 
   # REFACTOR: Validators
 
+  searchable do
+    text :content_en
+    text :content_nl
+    text :short_content_en
+    text :short_content_nl
+  end
+
   def short_content
-    t(:short_content)
+    t :short_content
   end
 
   def content
-    t(:content)
+    t :content
   end
 
   def t(attr_name)
-    send("#{attr_name}_#{I18n.locale.to_s.split('-').first}").andand.html_safe
+    lang = I18n.locale.to_s.split('-').first
+    send("#{attr_name}_#{lang}").andand.html_safe
   end
-  
+
   def title
     s = if describable.respond_to?(:title_for_description)
       describable.title_for_description
@@ -41,7 +49,7 @@ class Description < ActiveRecord::Base
       nil
     end
   end
-  
+
   def embeds_player?
     content.andand.include?("player")  || content.andand.include?("object")
   end

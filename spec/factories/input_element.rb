6cbe34530cab2ce7beea7dbb1f104096682d6bd3
Jesse Kerkhoven <jessekerkhoven@zonnet.nl>
2012-08-27 08:57:53 +0200

    Remove unused input_element#input_id closes #1029

--- spec/factories/input_element.rb
+++ spec/factories/input_element.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :input_element do
     sequence(:key) {|n| "input_element_#{n}" }
-    input_id 0
-    after_create {|i| i.update_attribute(:input_id, i.id)}
   end
 end

7fe9bc2e50c1e73604c7801b18d1e4821527b65d
Paolo Zaccagnini <paozac@gmail.com>
2012-08-28 15:13:52 +0200

    search: don't show hidden sliders. Closes #940

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,8 @@ class SearchController < ApplicationController
   def index
     @query = params[:q]
     @sidebar_items  = SidebarItem.search{|s| s.fulltext @query}.results
-    @input_elements = InputElement.search{|s| s.fulltext @query}.results
-    @slides         = Slide.search{|s| s.fulltext @query}.results
+    @input_elements = InputElement.search{|s|
+      s.fulltext @query}.results.reject{|s| s.slide.nil?}
+    @slides = Slide.search{|s| s.fulltext @query}.results
   end
 end

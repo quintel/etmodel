005f9701283b3dac42379704a5d1952f594ecd87
Paolo Zaccagnini <paozac@gmail.com>
2012-09-04 14:41:14 +0200

    spec request: slightly better
    Fails when running the entire test suite because of recorded responses.
    Works when ran in isolation. Some tests stub and mock stuff, this one
    makes real calls to the dev engine but builds the interface through factories. Ugly and confusing. :(

--- spec/requests/load_preset_scenario_spec.rb
+++ spec/requests/load_preset_scenario_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe "Loading a preset scenario", :js => true do
+  before do
+    ActiveResource::HttpMock.reset!
+    tab = FactoryGirl.create :tab, :key => 'demand'
+    sidebar_item = FactoryGirl.create :sidebar_item, :key => 'households', :tab_id => tab.id
+    slide = FactoryGirl.create :slide, :key => 'foobar', :sidebar_item_id => sidebar_item.id
+  end
+
   it "should create a new scenario from a preset" do
     visit home_path
     click_link "Start an existing scenario"
@@ -13,14 +20,13 @@ describe "Loading a preset scenario", :js => true do
 
     settings['area_code'].should == 'nl'
     settings['end_year'].should == 2030
-    settings['scenario_id'].should == 1
+    settings['preset_scenario_id'].should == 1
 
     new_scenario_id = settings['api_session_id']
     remote_scenario = Api::Scenario.find new_scenario_id
 
-    remote_scenario.scenario_id.should == 1
+    remote_scenario.template.should == 1
     remote_scenario.end_year.should == 2030
     remote_scenario.area_code.should == 'nl'
-
   end
 end

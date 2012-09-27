db01e218a668df59413e3528e095c7f73f4344be
Paolo Zaccagnini <paozac@gmail.com>
2012-09-27 12:30:13 +0200

    converter details: get session_id from App, not globals
    This was making the converter popup fail on the initial page load.
    Closes #1090.

--- app/assets/javascripts/lib/views/input_element_view.js
+++ app/assets/javascripts/lib/views/input_element_view.js
@@ -663,7 +663,7 @@
         var title = $(e.target).data('title');
         var converter = $(e.target).data('converter');
         var url = globals.api_url + "/api/v3/scenarios/"
-          + globals.api_session_id + "/converters/" + converter;
+          + App.settings.get('api_session_id') + "/converters/" + converter;
 
         $.ajax({
           url: url,

34b5860d4098ac3333b0b1e07f06cbe14cdbe99c
Paolo Zaccagnini <paozac@gmail.com>
2012-10-29 11:14:21 +0100

    prevent double js inclusion
    Now application.js is always pulled in, no need to import all the js
    libraries twice

--- app/assets/javascripts/etm.js
+++ app/assets/javascripts/etm.js
@@ -1,35 +1,21 @@
-// This file contains all the stuff to run the application
+// The standard libraries are defined in application.js
 
-
-//= require jquery
-//= require jquery_ujs
-//= require underscore-min
-//= require backbone-min
-//= require modernizr.custom
 //= require ie-d3-shims
 
-//= require_tree ./lib/util
-//= require flowplayer-3.2.6.min
-//= require jquery.busybox
 //= require jqplot
 //= require jquery.quinn.min
-//= require jquery_plugins
 
 // silly sprockets...
 //= require lib/views/base_chart_view
 //= require lib/views/vertical_stacked_bar_chart_view
 //= require_tree ./lib/views
 //= require_tree ./lib/models
-//= require intro
 //= require fancybox
 
 // vendored in jquery-etmodel-rails gem
 //= require jquery.etmodel
 
-//= require i18n
-//= require i18n/translations
 //= require lib/app
-//= require common_interface
 
 //= require d3.v2.min
 //= require_tree ./d3

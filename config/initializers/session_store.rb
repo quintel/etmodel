6e0dbaf26a16d6d297453f5dc8b8ffaae494aeab
Paolo Zaccagnini <paozac@gmail.com>
2012-11-27 17:39:38 +0100

    Sessions: back to active_record_store. Again.
    The memcached/dalli stores still suffer from an annoying bug when saving
    empty hashes to session.
    I'd like to fix action_dispatch but with Rails 4 it has been changed
    quite a bit and spending time on a library that will soon be replaced is
    a waste of time.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-require 'action_dispatch/middleware/session/dalli_store'
-Etm::Application.config.session_store :dalli_store, :key => "_etmodel"
+# require 'action_dispatch/middleware/session/dalli_store'
+Etm::Application.config.session_store :active_record_store, :key => "_etmodel"

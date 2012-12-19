2642ca06915d589e71aea1474264ea997456035c
Paolo Zaccagnini <paozac@gmail.com>
2012-12-19 17:29:57 +0100

    Trash: old tests leftovers

--- spec/factories/link.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :link do
-    parent_id 2#  {|parent| parent.association(:converter) }
-    converter_id 3#  {|child| child.association(:converter) }
-    carrier_id 3
-    link_type 1
-    excel_id 1
-  end
-end

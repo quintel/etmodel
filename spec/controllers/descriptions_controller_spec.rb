4260ae4ff24abddeae2bb6d09bdf9e5176d68719
Paolo Zaccagnini <paozac@gmail.com>
2012-12-19 17:28:20 +0100

    Tests: added a few controller specs

---
+++ spec/controllers/descriptions_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe DescriptionsController do
+  render_views
+  let!(:chart)   { FactoryGirl.create :output_element_with_description}
+  let!(:description)   { chart.description}
+
+  describe "#show" do
+    it "should show the description template" do
+      get :show, :id => description.id
+      expect(response).to be_success
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "#charts" do
+    it "should return the chart description" do
+      get :charts, :id => chart.id
+      expect(response).to be_success
+      expect(response).to render_template(:show)
+    end
+  end
+end
---
+++ spec/factories/description.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :description do
+    content_en 'Content EN'
+    content_nl 'Content NL'
+    short_content_en 'Short Content EN'
+    short_content_nl 'Short Content NL'
+  end
+end

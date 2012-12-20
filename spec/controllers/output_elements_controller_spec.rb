cde3ded2fdf474b5a04adfed9114f77659ccc02a
Paolo Zaccagnini <paozac@gmail.com>
2012-12-20 12:00:23 +0100

    Tests: extend controllers converage

--- spec/controllers/output_elements_controller_spec.rb
+++ spec/controllers/output_elements_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe OutputElementsController do
+  render_views
+
+  describe "#index" do
+    it "should render the page correctly" do
+      get :index
+      expect(response).to be_success
+      expect(response).to render_template(:index)
+    end
+  end
 end

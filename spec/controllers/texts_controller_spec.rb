a80ca0cee2b7dc469f074661b57a12b9bd67572b
Paolo Zaccagnini <paozac@gmail.com>
2012-12-20 10:26:10 +0100

    Tests: added texts_controller_spec
    - minor gem updates

---
+++ spec/controllers/texts_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe TextsController do
+  render_views
+  let!(:text)   { FactoryGirl.create :text, :key => 'foobar'}
+
+  describe "#show" do
+    it "should show the text detail page" do
+      get :show, :id => 'foobar'
+      expect(response).to be_success
+      expect(response).to render_template(:show)
+    end
+  end
+end

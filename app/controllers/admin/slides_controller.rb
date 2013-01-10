24a4155f093378be54e53bf3f53d6978bfe09603
Paolo Zaccagnini <paozac@gmail.com>
2013-01-10 16:03:50 +0100

    Admin: slides_controller fixes

--- app/controllers/admin/slides_controller.rb
+++ app/controllers/admin/slides_controller.rb
@@ -1,63 +1,62 @@
 module Admin
-class SlidesController < BaseController
-  before_filter :find_model, :only => [:edit, :show]
+  class SlidesController < BaseController
+    before_filter :find_model, :only => [:edit, :destroy]
 
-  def index
-    if params[:sidebar_item_id]
-      @sidebar_item = SidebarItem.find params[:sidebar_item_id]
-      @slides = @sidebar_item.slides.ordered
-    else
-      @slides = Slide.order('sidebar_item_id, position')
-    end
-  end
-
-  def new
-    @slide = Slide.new
-    @slide.build_description
-    @slide.build_area_dependency
-  end
-
-  def create
-    @slide = Slide.new(params[:slide])
-    if @slide.save
-      flash[:notice] = "Slide saved"
-      redirect_to admin_slides_url
-    else
-      render :action => 'new'
-    end
-  end
-
-  def update
-    @slide = Slide.find(params[:id])
-    if @slide.update_attributes(params[:slide])
-    flash[:notice] = "Slide updated"
-    redirect_to admin_slides_url
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-  end
-
-  def show
-  end
-
-  def edit
-    @slide.build_description unless @slide.description
-    @slide.build_area_dependency unless @slide.area_dependency
-  end
-
-  private
-
-    def find_model
-      if params[:version_id]
-        @version = Version.find(params[:version_id])
-        @slide = @version.reify
-        flash[:notice] = "Revision"
+    def index
+      if params[:sidebar_item_id]
+        @sidebar_item = SidebarItem.find params[:sidebar_item_id]
+        @slides = @sidebar_item.slides.ordered
       else
-        @slide = Slide.find(params[:id])
+        @slides = Slide.order('sidebar_item_id, position')
       end
     end
+
+    def new
+      @slide = Slide.new
+      @slide.build_description
+      @slide.build_area_dependency
+    end
+
+    def create
+      @slide = Slide.new(params[:slide])
+      if @slide.save
+        flash[:notice] = "Slide saved"
+        redirect_to admin_slides_url
+      else
+        render :action => 'new'
+      end
+    end
+
+    def update
+      @slide = Slide.find(params[:id])
+      if @slide.update_attributes(params[:slide])
+      flash[:notice] = "Slide updated"
+      redirect_to admin_slides_url
+      else
+        render :action => 'edit'
+      end
+    end
+
+    def destroy
+      @slide.destroy
+      redirect_to admin_slides_path, :notice => 'Slide deleted'
+    end
+
+    def edit
+      @slide.build_description unless @slide.description
+      @slide.build_area_dependency unless @slide.area_dependency
+    end
+
+    private
+
+      def find_model
+        if params[:version_id]
+          @version = Version.find(params[:version_id])
+          @slide = @version.reify
+          flash[:notice] = "Revision"
+        else
+          @slide = Slide.find(params[:id])
+        end
+      end
   end
 end
---
+++ spec/controllers/admin/slides_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe Admin::SlidesController do
+  render_views
+
+  let!(:slide) { FactoryGirl.create :slide }
+
+  before(:each) do
+    controller.class.skip_before_filter :restrict_to_admin
+  end
+
+  it "index action should render index template" do
+    get :index
+    response.should render_template(:index)
+  end
+
+  it "new action should render new template" do
+    get :new
+    response.should render_template(:new)
+  end
+
+  describe "create" do
+    it "create action should render new template when model is invalid" do
+      post :create
+      response.should render_template(:new)
+    end
+
+    it "create action should redirect when model is valid" do
+      post :create, :slide => FactoryGirl.attributes_for(:slide)
+      response.should redirect_to(admin_slides_path)
+    end
+  end
+
+  describe "update" do
+    before do
+      @slide = FactoryGirl.create :slide
+    end
+
+    it "update action should render edit template when model is invalid" do
+      put :update, :id => @slide, :slide => {:key => ''}
+      response.should render_template(:edit)
+    end
+
+    it "update action should redirect when model is valid" do
+      put :update, :id => @slide, :slide => {:key => 'abc'}
+      response.should redirect_to(admin_slides_path)
+    end
+  end
+
+  it "edit action should render edit template" do
+    get :edit, :id => slide.id
+    response.should render_template(:edit)
+  end
+
+  it "destroy action should destroy model and redirect to index action" do
+    slide = FactoryGirl.create :slide
+    delete :destroy, :id => slide.id
+    response.should redirect_to(admin_slides_path)
+    Slide.exists?(slide.id).should be_false
+  end
+end

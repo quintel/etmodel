81039b83cc5e5b07866c6c30e79a25138bc0aa53
Paolo Zaccagnini <paozac@gmail.com>
2013-01-10 15:53:21 +0100

    Admin: output_elements_controller clean-up

---
+++ spec/controllers/admin/output_elements_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe Admin::OutputElementsController do
+  render_views
+
+  let!(:output_element) { FactoryGirl.create :output_element }
+
+  before(:each) do
+    controller.class.skip_before_filter :restrict_to_admin
+  end
+
+  it "index action should render index template" do
+    get :index
+    response.should render_template(:index)
+  end
+
+  it "show action should render show template" do
+    get :show, :id => output_element.id
+    response.should render_template(:show)
+  end
+
+  it "new action should render new template" do
+    get :new
+    response.should render_template(:new)
+  end
+
+  describe "create" do
+    it "create action should render new template when model is invalid" do
+      post :create
+      response.should render_template(:new)
+    end
+
+    it "create action should redirect when model is valid" do
+      post :create, :output_element => FactoryGirl.attributes_for(:output_element)
+      response.should redirect_to(admin_output_elements_path)
+    end
+  end
+
+  describe "update" do
+    before do
+      @output_element = FactoryGirl.create :output_element
+    end
+
+    it "update action should render edit template when model is invalid" do
+      put :update, :id => @output_element, :output_element => {:key => ''}
+      response.should render_template(:edit)
+    end
+
+    it "update action should redirect when model is valid" do
+      put :update, :id => @output_element, :output_element => {:key => 'abc'}
+      response.should redirect_to(admin_output_elements_path)
+    end
+  end
+
+  it "edit action should render edit template" do
+    get :edit, :id => output_element.id
+    response.should render_template(:edit)
+  end
+
+  it "destroy action should destroy model and redirect to index action" do
+    output_element = FactoryGirl.create :output_element
+    delete :destroy, :id => output_element.id
+    response.should redirect_to(admin_output_elements_path)
+    OutputElement.exists?(output_element.id).should be_false
+  end
+end

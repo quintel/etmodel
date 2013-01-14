8fa267d75f61c732dc748d4f3c2f850e1d2deccd
Paolo Zaccagnini <paozac@gmail.com>
2013-01-14 12:23:03 +0100

    Completed. Closes #1223

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -13,7 +13,7 @@ class ScenariosController < ApplicationController
     else
       current_user.saved_scenarios
     end
-    @saved_scenarios = items.order("created_at DESC").page(params[:page]).per(15)
+    @saved_scenarios = items.order("created_at DESC").page(params[:page]).per(50)
     @scenarios = @saved_scenarios.map{|s| s.scenario rescue nil}.compact
   end
 
@@ -101,7 +101,8 @@ class ScenariosController < ApplicationController
   end
 
   def compare
-    @scenarios = SavedScenario.find params[:scenario_ids]
+    scenario_ids = params[:scenario_ids] || []
+    @scenarios = scenario_ids.map{|id| Api::Scenario.find id, params: {detailed: true}}
   end
 
   private
--- spec/controllers/scenarios_controller_spec.rb
+++ spec/controllers/scenarios_controller_spec.rb
@@ -8,6 +8,7 @@ describe ScenariosController, :vcr => true do
     mock.stub(:id){"123"}
     mock.stub(:title){"title"}
     mock.stub(:end_year){"2050"}
+    mock.stub(:area_code){"nl"}
     mock.stub(:parsed_created_at){ Time.now }
     mock.stub(:created_at){ Time.now }
     mock

ff5ad2adae3ce597194b7f39a68d7925d3c1b076
Paolo Zaccagnini <paozac@gmail.com>
2013-01-21 16:02:19 +0100

    Search page: fixed error triggered by slides removed from interface
    Closes #1227

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,9 @@ class SearchController < ApplicationController
   def index
     @query = params[:q]
     @sidebar_items  = SidebarItem.search{|s| s.fulltext @query}.results
-    @input_elements = InputElement.search{|s|
-      s.fulltext @query}.results.reject{|s| s.slide.nil?}
-    @slides = Slide.search{|s| s.fulltext @query}.results
+    @input_elements = InputElement.search{|s| s.fulltext @query}.results.
+      reject{|s| s.slide.nil?}
+    @slides = Slide.search{|s| s.fulltext @query}.results.
+      reject(&:removed_from_interface?)
   end
 end

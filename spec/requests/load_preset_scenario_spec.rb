d8f9324df62b96a0ebb7bf3cd289f041a539e568
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-05-06 15:19:50 +0200

    Fix broken tests from e86cd6f

--- spec/requests/load_preset_scenario_spec.rb
+++ spec/requests/load_preset_scenario_spec.rb
@@ -4,10 +4,9 @@ describe "loading a preset scenario", :js => true, :vcr => true do
   fixtures :tabs, :sidebar_items, :slides, :output_elements, :output_element_types
 
   it "should create a new scenario from a preset" do
-    visit home_path
+    visit scenario_path(80)
     # save_and_open_page
-    click_link "Start an existing scenario"
-    click_link "Referentiescenario 2030 gebaseerd op ..."
+    click_link "Open this scenario"
 
     page.should have_content('Household energy demand')
 
@@ -15,12 +14,12 @@ describe "loading a preset scenario", :js => true, :vcr => true do
 
     settings['area_code'].should == 'nl'
     settings['end_year'].should == 2030
-    settings['preset_scenario_id'].should == 1
+    settings['preset_scenario_id'].should == 80
 
     new_scenario_id = settings['api_session_id']
     remote_scenario = Api::Scenario.find new_scenario_id
 
-    remote_scenario.template.should == 1
+    remote_scenario.template.should == 80
     remote_scenario.end_year.should == 2030
     remote_scenario.area_code.should == 'nl'
   end

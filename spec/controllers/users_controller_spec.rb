f3bb63e2430ebccfa024768b2a65ef9e37f77475
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-06-18 10:06:27 +0200

    Adding hashed created_at to verify origin for unsubscribe

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,7 +19,7 @@ describe UsersController do
 
       before do
         @user = FactoryGirl.create(:user, allow_news: true)
-        get :unsubscribe, id: @user.id
+        get :unsubscribe, id: @user.id, h: @user.hash
         @user.reload
       end
 
@@ -41,7 +41,7 @@ describe UsersController do
 
       before do
         @user = FactoryGirl.create(:user, allow_news: false)
-        get :unsubscribe, id: @user.id
+        get :unsubscribe, id: @user.id, h: @user.hash
         @user.reload
       end
 
@@ -51,6 +51,28 @@ describe UsersController do
 
       it 'shows you have been unsubscribed ALREADY' do
         expect(response.body).to match(/already/i)
+      end
+
+    end
+
+    context 'with invalid hash' do
+
+      before do
+        @user = FactoryGirl.create(:user, allow_news: true)
+        get :unsubscribe, id: @user.id, h: 'i-am-a-hacker'
+        @user.reload
+      end
+
+      it 'does not register in the database' do
+        expect(@user.allow_news).to be_true
+      end
+
+      it 'renders a page succesfully' do
+        expect(response).to be_success
+      end
+
+      it 'shows that user has not been unsubscribed' do
+        expect(response.body).to match(/cannot unsubscribe/i)
       end
 
     end

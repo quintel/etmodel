c9513ca03a483bf19f4ef93b6c8cd65bc7eabf46
Anthony Williams <hi@antw.me>
2013-07-31 15:17:31 +0100

    Actually memozing the thing would help

--- app/models/saved_scenario.rb
+++ app/models/saved_scenario.rb
@@ -21,7 +21,7 @@ class SavedScenario < ActiveRecord::Base
 
   def scenario
     begin
-      @scenario = Api::Scenario.find(scenario_id)
+      @scenario ||= Api::Scenario.find(scenario_id)
     rescue ActiveResource::ResourceNotFound
       nil
     end

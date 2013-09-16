855da2c2f1588f7baf5d4162f4c8fcdde2534423
Anthony Williams <hi@antw.me>
2013-09-16 20:15:25 +0100

    Fixes the partners page since the addition of the new API
    * There is no longer a "country" attribute, therefore we cannot restrict
      the partners shown to the visitor.
    
    * The "partner_type" attribute has been renamed to "kind" in the API.
    
    * Simplified the "index" page to only link to each partner on the
      et-model.com website.
    
    * Remove the old "show parter" page and spec.

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,22 +1,20 @@
 class PartnersController < ApplicationController
   def show
-    country = Current.setting.area_code rescue nil
-    @partner = Partner.find_by_slug_localized(params[:id], country)
-    raise ActiveRecord::RecordNotFound if @partner.nil?
+    unless @partner = Partner.find(params[:id])
+      raise ActiveRecord::RecordNotFound
+    end
   end
 
   def index
     @info = Text.where(key: :partner_info).first
-    # regions should use country partners
-    country = Current.setting.country
-    @all_partners = Partner.country(country).unique.sort_by{Kernel.rand}
+    @all_partners = Partner.all.shuffle
     @partner_types = ['general']
-    @partner_types << 'knowledge' if @all_partners.map(&:partner_type).include?('knowledge')
-    @partner_types << 'education' if @all_partners.map(&:partner_type).include?('education')
+    @partner_types << 'knowledge' if @all_partners.map(&:kind).include?('knowledge')
+    @partner_types << 'education' if @all_partners.map(&:kind).include?('education')
     @active_partner_type   = params[:partner_type]
     @active_partner_type ||= 'general'
 
-    @partners = @all_partners.select{|p|p.partner_type == @active_partner_type}
+    @partners = @all_partners.select{|p|p.kind == @active_partner_type}
 
 
   end

2a7ddf41f313ecdea91f2d2945979e7ed980e100
Anthony Williams <hi@antw.me>
2013-09-16 20:15:25 +0100

    Fixes the partners page since the addition of the new API
    * There is no longer a "country" attribute, therefore we cannot restrict
      the partners shown to the visitor.
    
    * The "partner_type" attribute has been renamed to "kind" in the API.
    
    * Simplified the "index" page to only link to each partner on the
      et-model.com website.
    
    * Remove the old "show parter" page and spec.

--- spec/controllers/partners_controller_spec.rb
+++ spec/controllers/partners_controller_spec.rb
@@ -2,21 +2,11 @@ require 'spec_helper'
 
 describe PartnersController do
   render_views
-  let!(:partner)   { FactoryGirl.create :partner, :name => 'foobar', :country => 'nl' }
-
   describe "#index" do
     it "should show the partner list" do
       get :index
       expect(response).to be_success
       expect(response).to render_template(:index)
-    end
-  end
-
-  describe "#show" do
-    it "should show the partner detail page" do
-      get :show, :id => 'foobar'
-      expect(response).to be_success
-      expect(response).to render_template(:show)
     end
   end
 end

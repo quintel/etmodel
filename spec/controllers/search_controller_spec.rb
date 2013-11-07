d73f6b53b1deb239eb37a8ff34ca1775427aee23
Frans van Camp <fvancamp@xs4all.nl>
2013-11-07 17:15:34 +0100

    Made search spec pending following removal of solr

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,6 +4,7 @@ describe SearchController do
   render_views
 
   it "should run a search" do
+    pending
     get :index, :q => 'foo'
     response.should be_success
     response.should render_template(:index)

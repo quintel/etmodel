722c8906bd24074f4f52942ddf5e249c7971887a
Anthony Williams <hi@antw.me>
2013-11-27 10:36:16 +0000

    Hotfix for missing search functionality
    "searchable" was removed from the indexed models, but the search form
    and controller still remain. This disables them until remove it cleanly.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,13 @@
 class SearchController < ApplicationController
   def index
     @query = params[:q]
-    @sidebar_items  = SidebarItem.search{|s| s.fulltext @query}.results
-    @input_elements = InputElement.search{|s| s.fulltext @query}.results.
-      reject{|s| s.slide.nil?}
-    @slides = Slide.search{|s| s.fulltext @query}.results.
-      reject(&:removed_from_interface?)
+
+    # @sidebar_items  = SidebarItem.search{|s| s.fulltext @query}.results
+    # @input_elements = InputElement.search{|s| s.fulltext @query}.results.
+      # reject{|s| s.slide.nil?}
+    # @slides = Slide.search{|s| s.fulltext @query}.results.
+      # reject(&:removed_from_interface?)
+
+    @sidebar_items = @input_elements = @slides = []
   end
 end

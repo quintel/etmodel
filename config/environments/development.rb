6eaac3f784e78352ab5b4e685596d5efb85651ce
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-12-05 09:46:55 +0100

    Whiny nils deprecated in Rails 4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,9 +7,6 @@ Etm::Application.configure do
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local = true
 

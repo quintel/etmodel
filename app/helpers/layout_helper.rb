1e153df1d4a96556f7e3f228c9b2339b22722297
Anthony Williams <hi@antw.io>
2014-08-25 17:06:57 +0100

    Make the settings drop-down nicer

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -84,6 +84,14 @@ module LayoutHelper
     end
   end
 
+  # Creates the language selection drop-down.
+  def language_select
+    languages = [['English',    :en],
+                 ['Nederlands', :nl]]
+
+    select_tag "locale", options_for_select(languages, I18n.locale)
+  end
+
   # Tries mapping a hex string to a human readable colour name
   #
   def color_to_string(hex)

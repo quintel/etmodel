b5f043789605d90720776eed70b024cb468d46ac
Anthony Williams <hi@antw.io>
2014-09-10 09:50:04 +0100

    Energy Productivity -> Energy Efficiency
    Closes #1827

--- app/models/api/scenario.rb
+++ app/models/api/scenario.rb
@@ -1,7 +1,7 @@
 class Api::Scenario < ActiveResource::Base
   PRESET_GROUPS = [
     'Actueel',
-    'Energy Productivity',
+    'Energy Efficiency',
     'Watt Nu?! 2013',
     'Watt Nu?! 2012',
     'EC2013',

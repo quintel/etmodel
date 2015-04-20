c9b34866701d29bce29c563fba0849bc5c0ad11e
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-04-20 17:11:28 +0200

    adding ete scenario mock in the spec support

--- spec/controllers/scenarios_controller_spec.rb
+++ spec/controllers/scenarios_controller_spec.rb
@@ -3,17 +3,7 @@ require 'spec_helper'
 describe ScenariosController, vcr: true do
   render_views
 
-  let(:scenario_mock) {
-    mock = double("api_scenario")
-    mock.stub(:id){"123"}
-    mock.stub(:title){"title"}
-    mock.stub(:end_year){"2050"}
-    mock.stub(:area_code){"nl"}
-    mock.stub(:parsed_created_at){ Time.now }
-    mock.stub(:created_at){ Time.now }
-    mock.stub(:all_inputs){ {} }
-    mock
-  }
+  let(:scenario_mock) { ete_scenario_mock }
 
   before do
     Api::Scenario.stub(:find).and_return scenario_mock

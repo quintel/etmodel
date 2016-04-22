920868c6ea5e2b0a37a50bba24fe5f3afca17083
Gerard Westerhof <gerard@grdw.nl>
2016-04-22 17:26:26 +0200

    Add merit order price curve
    Fixes: #2083

---
+++ db/migrate/20160421084217_add_output_elements_for_merit.rb
@@ -0,0 +1,18 @@
+class AddOutputElementsForMerit < ActiveRecord::Migration
+  def change
+    merit_order_price_curve = OutputElement.create!(
+      group: 'Merit',
+      key: 'merit_order_price_curve',
+      requires_merit_order: true,
+      output_element_type_id: 16
+    )
+
+    OutputElementSerie.create!(
+      label: 'price',
+      color: '#5D7929',
+      gquery: 'merit_order_price_curve',
+      output_element: merit_order_price_curve,
+      unit: "EUR/MWh"
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208130731) do
+ActiveRecord::Schema.define(version: 20160421084217) do
 
   create_table "area_dependencies", force: true do |t|
     t.string  "dependent_on"

80d673d41065f3b685e2c73f81b4329fe43a64ce
Anthony Williams <hi@antw.io>
2016-06-02 11:34:31 +0100

    Permit scaling an already-scaled scenario
    Ref #2149

--- app/helpers/scenario_helper.rb
+++ app/helpers/scenario_helper.rb
@@ -2,4 +2,11 @@ module ScenarioHelper
   def current_scenario_scaled?
     Current.setting.scaling.present?
   end
+
+  def scaling_sector_checkbox(name, default = true)
+    scaled  = Current.setting.scaling.present?
+    checked = scaled ? Current.setting.scaling[:"has_#{ name }"] : true
+
+    check_box_tag("has_#{ name }", '1', checked, disabled: false)
+  end
 end

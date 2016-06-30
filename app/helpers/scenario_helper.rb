cd0c1ae7255188906b3ed763b5353a38dc77b10e
Anthony Williams <hi@antw.io>
2016-06-30 12:20:20 +0200

    Scenario scaling form respects default sectors
    Agriculture and industry should be disabled by default.

--- app/helpers/scenario_helper.rb
+++ app/helpers/scenario_helper.rb
@@ -5,7 +5,7 @@ module ScenarioHelper
 
   def scaling_sector_checkbox(name, default = true)
     scaled  = Current.setting.scaling.present?
-    checked = scaled ? Current.setting.scaling[:"has_#{ name }"] : true
+    checked = scaled ? Current.setting.scaling[:"has_#{ name }"] : default
 
     check_box_tag("has_#{ name }", '1', checked, disabled: false)
   end

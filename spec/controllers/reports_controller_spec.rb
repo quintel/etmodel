ccc3fcfc9607868f344efd1e2507f0596fc8f795
Anthony Williams <hi@antw.io>
2017-03-28 16:24:45 +0100

    Add tests for showing report with a permalink

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -21,6 +21,11 @@ describe ReportsController, vcr: true do
       before { session[:setting] = Setting.new(api_session_id: 648_695) }
       before { FactoryGirl.create(:tab, key: 'demand') }
 
+      it 'uses the scenario' do
+        get :show, params: { id: 'sample' }
+        expect(response.body).to include('"api_session_id":648695')
+      end
+
       it 'renders the report' do
         get :show, params: { id: 'sample' }
 
@@ -31,6 +36,16 @@ describe ReportsController, vcr: true do
       it 'renders 404 when the specified report does not exist' do
         expect { get :show, params: { id: 'four-oh-four' } }
           .to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'with a specified scenario' do
+      before { session[:setting] = Setting.new(api_session_id: 648_695) }
+      before { FactoryGirl.create(:tab, key: 'demand') }
+
+      it 'starts a new scenario with the specified ID' do
+        get :show, params: { id: 'sample', scenario_id: 648_695 }
+        expect(response.body).to include('"api_session_id":655091')
       end
     end
   end

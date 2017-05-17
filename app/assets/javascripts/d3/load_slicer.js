c1f2f323bf509242aff542acadd5a7fe71455000
Anthony Williams <hi@antw.io>
2017-05-17 14:00:14 +0100

    Simplify MeritTransformator
    Does not need to know about the series, since all it does is slice or
    downsample arrays of values. Reduced Downsampler and LoadSlicer to
    functions.
    
    Working only with an array of values -- instead of series -- makes
    the functions simpler and easier to reuse.
    
    MeritTransformator is now a "static" object since it doesn't need to
    retain any state.
    
    Adjusted code style (two-space indentation) for consistency with other
    ETM JS files and ESLint.

--- app/assets/javascripts/d3/load_slicer.js
+++
@@ -1,9 +0,0 @@
-var LoadSlicer = (function () {
-    'use strict';
-
-    return {
-        slice: function sliceValues(serie, range) {
-            return serie.values.slice((range - 1) * 168, range * 168);
-        }
-    };
-}());

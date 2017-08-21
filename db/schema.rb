c23ed22cf5528ab8e4bfb5035f3057785e544908
Anthony Williams <hi@antw.io>
2017-08-21 15:43:03 +0100

    Add migration and texts for the heat deficit chart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811153729) do
+ActiveRecord::Schema.define(version: 20170821102752) do
 
   create_table "area_dependencies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string  "dependent_on"

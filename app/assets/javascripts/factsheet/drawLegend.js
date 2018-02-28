591d0bf674f7ca4377233a51fe0693213165b134
Anthony Williams <hi@antw.io>
2018-02-28 10:04:34 +0000

    Factsheet: Include values for present in the summary

--- app/assets/javascripts/factsheet/drawLegend.js
+++ app/assets/javascripts/factsheet/drawLegend.js
@@ -4,20 +4,32 @@
  * Given the definition for a chart, returns an array of <div/> elements
  * describing each series in the chart.
  */
-function drawLegend(definition) {
+function drawLegend(definition, periods) {
+  var showPeriods = periods || ['future'];
+
   var list = definition.map(function(series) {
-    return $('<tr/>').append(
-      $('<td class="key"/>').append(
-        $('<span class="legend-item" />').css('background-color', series.color),
-        ' ',
-        I18n.t('factsheet.series.' + series.key)
-      ),
-      $('<td class="value" />')
-        .attr('data-query', series.key)
-        .attr('data-as', 'TJ')
-        .attr('data-no-unit', true)
-        .html('&mdash;')
-    );
+    var key = series.key.replace(/_/g, '-');
+
+    return $('<tr/>')
+      .addClass(key)
+      .append(
+        $('<td class="key"/>').append(
+          $('<span class="legend-item" />').css(
+            'background-color',
+            series.color
+          ),
+          ' ',
+          I18n.t('factsheet.series.' + series.key)
+        ),
+        showPeriods.map(function(period) {
+          return $('<td class="value" />')
+            .attr('data-query', series.key)
+            .attr('data-as', 'TJ')
+            .attr('data-no-unit', true)
+            .attr('data-period', period)
+            .html('&mdash;');
+        })
+      );
   });
 
   list.reverse();

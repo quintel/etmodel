4c1f0b281ebb6bb8458ec025792c7e068ccb6fcc
Anthony Williams <hi@antw.io>
2018-08-07 10:37:40 +0100

    Add "Storage in reservoirs" flexibility option

--- app/models/flexibility_order.rb
+++ app/models/flexibility_order.rb
@@ -2,6 +2,7 @@ class FlexibilityOrder
   def self.all
     %w(power_to_power
        electric_vehicle
+       pumped_storage
        power_to_heat
        power_to_heat_industry
        power_to_gas

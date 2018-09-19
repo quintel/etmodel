5a765483c007bfd39536635b989d55480a66649a
Anthony Williams <hi@antw.io>
2018-09-19 16:01:43 +0100

    Improve empty chart messages
    * Set a standard height of 330px which fills the standard chart element.
      Computing the height dynamically was inconsistent.
    
    * Hide date pickers when "empty chart" message is shown.
    
    * Adjust styles: blue background, typography changes.

--- app/assets/javascripts/lib/util/empty_chart_message.js
+++ app/assets/javascripts/lib/util/empty_chart_message.js
@@ -3,20 +3,23 @@
 var EmptyChartMessage = (function () {
     'use strict';
 
-    var template = function (chart_key, container) {
+    var template = function (chartKey, container) {
         var wrapper,
             annotation,
             existingWrapper = container.find(".empty-wrapper");
-
-        if (I18n.lookup('output_elements.empty.' + chart_key)) {
-            annotation = $("<div>").text(I18n.t("output_elements.empty." + chart_key))
-                .addClass("annotation")
-        }
 
         if (existingWrapper.length > 0) {
             wrapper = existingWrapper;
         } else {
             wrapper = $("<div>").addClass("empty-wrapper");
+
+            if (I18n.lookup('output_elements.empty.' + chartKey)) {
+                annotation = $("<div>")
+                    .text(I18n.t("output_elements.empty." + chartKey))
+                    .addClass("annotation");
+
+                wrapper.addClass('has-annotation');
+            }
 
             wrapper.append(
                 $("<p>").text(I18n.t("output_elements.common.empty")),
@@ -25,6 +28,7 @@ var EmptyChartMessage = (function () {
 
             container.append(wrapper);
         }
+
         return wrapper;
     };
 
@@ -35,8 +39,6 @@ var EmptyChartMessage = (function () {
                 isEmpty   = chart.is_empty();
 
             container.toggleClass("empty", isEmpty);
-
-            wrapper.height(chart.height);
             wrapper.toggle(isEmpty);
         }
     };

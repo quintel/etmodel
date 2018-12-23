56f248e3c233cc3a42b117f34e734a8e165c19e1
Anthony Williams <hi@antw.io>
2018-12-23 15:09:11 +0000

    Fix 500 when chart description does not exist
    Closes ETMODEL-8

--- spec/controllers/descriptions_controller_spec.rb
+++ spec/controllers/descriptions_controller_spec.rb
@@ -1,22 +1,46 @@
 require 'rails_helper'
 
 describe DescriptionsController do
+  render_views
+
   let!(:chart)   { FactoryBot.create :output_element_with_description}
   let!(:description)   { chart.description}
 
   describe "#show" do
     it "should show the description template" do
       get :show, params: { id: description.id }
-      expect(response).to be_success
+
+      expect(response).to be_successful
       expect(response).to render_template(:show)
+
+      expect(response.body).to_not be_empty
+    end
+
+    it 'renders nothing if no description exists' do
+      get :show, params: { id: -1 }
+
+      expect(response).to be_successful
+      expect(response.body).to be_empty
     end
   end
 
   describe "#charts" do
     it "should return the chart description" do
       get :charts, params: { id: chart.id }
-      expect(response).to be_success
+
+      expect(response).to be_successful
       expect(response).to render_template(:show)
+
+      expect(response.body).to_not be_empty
+    end
+
+    it 'renders nothing if the chart has no description' do
+      chart.description.destroy
+
+      get :charts, params: { id: chart.id }
+      expect(response).to be_successful
+
+      expect(response.body).to be_empty
     end
   end
 end

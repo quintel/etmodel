781323c05aa9093b88a2ce7fbcbcc0672ba64dfa
marliekeverweij <32833996+marliekeverweij@users.noreply.github.com>
2019-01-25 14:00:32 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_28_115253) do
-=======
 ActiveRecord::Schema.define(version: 20181204152301) do
->>>>>>> Add buildings heat demand and production chart
 
   create_table "area_dependencies", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.string "dependent_on"

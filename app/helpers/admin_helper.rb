7978339019f6ee397135d0b527adc802cd8a6a04
Anthony Williams <hi@antw.io>
2019-02-01 11:01:57 +0000

    Show old invalid option in area dependency select
    Ref #2938

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,4 +3,27 @@ module AdminHelper
     base = Colors::COLORS.values.include?(selected) ? [] : ([[selected] * 2])
     Colors::COLORS.to_a + base
   end
+
+  # Creates the list of area dependency options which may be used in a form in
+  # the admin section.
+  #
+  # If the area dependency is for an invalid attribute, the option will be shown
+  # with a suitable warning.
+  #
+  # Returns an array.
+  def area_dependency_options(model)
+    base_options     = Api::Area::DEPENDABLE_ATTRIBUTES
+    model_dependency = model.area_dependency&.dependent_on.presence
+
+    if model_dependency && !base_options.include?(model_dependency)
+      base_options.map { |v| [v, v] } + [
+        [
+          "#{model_dependency} - unsupported attribute, will always be hidden",
+          model_dependency
+        ]
+      ]
+    else
+      base_options
+    end
+  end
 end

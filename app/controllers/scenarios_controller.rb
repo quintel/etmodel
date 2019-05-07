5bed28ce9b8f8a6a4ef412683aa087d2e00498b3
Anthony Williams <hi@antw.dev>
2019-05-07 15:05:46 +0100

    Use %i[] arrays in ScenariosController

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -2,10 +2,10 @@ class ScenariosController < ApplicationController
   include MainInterfaceController.new(:play, :play_multi_year_charts)
 
   before_action :ensure_valid_browser
-  before_action :find_scenario, only: [:show, :load, :play_multi_year_charts]
-  before_action :require_user, only: [:index, :new, :merge]
+  before_action :find_scenario, only: %i[show load play_multi_year_charts]
+  before_action :require_user, only: %i[index new merge]
   before_action :redirect_compare, only: :compare
-  before_action :setup_comparison, only: [:compare, :weighted_merge]
+  before_action :setup_comparison, only: %i[compare weighted_merge]
   before_action :store_last_etm_page, :prevent_browser_cache, only: :play
   before_action :myc_content_security_policy, only: :play_multi_year_charts
 

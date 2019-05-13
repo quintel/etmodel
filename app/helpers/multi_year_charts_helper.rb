3af80cb0b6e32e0f59912efb6af07e672ac792b0
Anthony Williams <hi@antw.dev>
2019-05-13 13:50:43 +0100

    MYC helper: Scenarios may not exist
    In development mode, scenarios may not exist on ETEngine. Check that the
    scenario is available before checking if it may be loaded.

--- app/helpers/multi_year_charts_helper.rb
+++ app/helpers/multi_year_charts_helper.rb
@@ -3,8 +3,10 @@ module MultiYearChartsHelper
   def can_use_as_myc_scenario?(saved_scenario)
     scenario = saved_scenario.scenario
 
-    scenario.loadable? &&
+    scenario && (
+      scenario.loadable? &&
       scenario.end_year == 2050 &&
       scenario.area_code == 'nl'
+    )
   end
 end

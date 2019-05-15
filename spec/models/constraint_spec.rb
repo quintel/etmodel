382bf1edc106ff214942efc5ccfc081559881d2f
Anthony Williams <hi@antw.dev>
2019-05-15 16:43:24 +0100

    Code style adjustments in constraint spec

--- spec/models/constraint_spec.rb
+++ spec/models/constraint_spec.rb
@@ -9,130 +9,127 @@ describe Constraint do
 
   it { is_expected.not_to allow_value('').for(:group) }
   it { is_expected.not_to allow_value('invalid_group').for(:group) }
-end
 
-# ----------------------------------------------------------------------------
+  describe '#as_json' do
+    let(:json) { described_class.first.as_json }
 
-describe Constraint, '#as_json' do
-  before { FactoryBot.create :constraint }
-  subject { Constraint.first.as_json }
+    before { FactoryBot.create(:constraint) }
 
-  it 'should remove the "constraint" root' do
-    expect(subject).not_to have_key('constraint')
+    it 'removes the "constraint" root' do
+      expect(json).not_to have_key('constraint')
+    end
+
+    it { expect(json).to have_key('id') }
+    it { expect(json).to have_key('key') }
+    it { expect(json).to have_key('gquery_key') }
   end
 
-  it { is_expected.to have_key('id') }
-  it { is_expected.to have_key('key') }
-  it { is_expected.to have_key('gquery_key') }
-end
-
-# ----------------------------------------------------------------------------
-
-describe Constraint, '.for_dashboard' do
-  let!(:default_constraints) do
-    [FactoryBot.create(:constraint, position: 0)]
-  end
-
-  context 'when an array of keys' do
-    let(:keys) { %w[total_primary_energy co2_reduction] }
-    let(:constraints) { Constraint.for_dashboard(keys) }
-
-    before do
-      keys.each{ |k| FactoryBot.create(:constraint, key: k) }
+  describe '.for_dashboard' do
+    let!(:default_constraints) do
+      [FactoryBot.create(:constraint, position: 0)]
     end
 
-    it 'returns the same as for_dashboard!' do
-      expect(constraints).to eq(Constraint.for_dashboard!(keys))
-    end
-  end
+    context 'when an array of keys' do
+      let(:keys) { %w[total_primary_energy co2_reduction] }
+      let(:constraints) { described_class.for_dashboard(keys) }
 
-  context 'when given an invalid key' do
-    let(:keys) { %w[nope] }
-    let(:constraints) { Constraint.for_dashboard(keys) }
+      before do
+        keys.each{ |k| FactoryBot.create(:constraint, key: k) }
+      end
 
-    it 'returns the default constraints' do
-      expect(constraints).to eq(default_constraints)
-    end
-  end
-
-  context 'when given an empty key' do
-    let(:keys) { [''] }
-    let(:constraints) { Constraint.for_dashboard(keys) }
-
-    it 'returns the default constraints' do
-      expect(constraints).to eq(default_constraints)
-    end
-  end
-end
-
-describe Constraint, '.for_dashboard!' do
-  context 'when an array of keys' do
-    let(:keys) { %w( total_primary_energy co2_reduction ) }
-    before do
-      keys.each{|k| FactoryBot.create :constraint, key: k}
+      it 'returns the same as for_dashboard!' do
+        expect(constraints).to eq(described_class.for_dashboard!(keys))
+      end
     end
 
-    subject { Constraint.for_dashboard!(keys) }
+    context 'when given an invalid key' do
+      let(:keys) { %w[nope] }
+      let(:constraints) { described_class.for_dashboard(keys) }
 
-    it { expect(subject.length).to eq(keys.length) }
-
-    it 'should return the total_primary_energy constraint first' do
-      expect(subject[0]).to eql(Constraint.find_by_key(keys[0]))
+      it 'returns the default constraints' do
+        expect(constraints).to eq(default_constraints)
+      end
     end
 
-    it 'should return the co2_reduction constraint second' do
-      expect(subject[1]).to eql(Constraint.find_by_key(keys[1]))
+    context 'when given an empty key' do
+      let(:keys) { [''] }
+      let(:constraints) { described_class.for_dashboard(keys) }
+
+      it 'returns the default constraints' do
+        expect(constraints).to eq(default_constraints)
+      end
     end
   end
 
-  context 'when given an array with a duplicate key' do
-    let(:keys) { %w( total_primary_energy co2_reduction total_primary_energy ) }
-    before do
-      FactoryBot.create(:constraint, key: 'total_primary_energy')
-      FactoryBot.create(:constraint, key: 'co2_reduction')
+  describe '.for_dashboard!' do
+    let(:constraints) { described_class.for_dashboard!(keys) }
+
+    context 'when an array of keys' do
+      let(:keys) { %w[total_primary_energy co2_reduction] }
+
+      before do
+        keys.each { |k| FactoryBot.create(:constraint, key: k) }
+      end
+
+      it { expect(constraints.length).to eq(keys.length) }
+
+      it 'returns the total_primary_energy constraint first' do
+        expect(constraints[0]).to eql(described_class.find_by_key(keys[0]))
+      end
+
+      it 'returns the co2_reduction constraint second' do
+        expect(constraints[1]).to eql(described_class.find_by_key(keys[1]))
+      end
     end
-    subject { Constraint.for_dashboard!(keys) }
 
-    it { expect(subject.length).to eq(2) }
+    context 'when given an array with a duplicate key' do
+      let(:keys) { %w[total_primary_energy co2_reduction total_primary_energy] }
 
-    it 'should return the total_primary_energy constraint first' do
-      expect(subject[0]).to eql(Constraint.find_by_key(keys[0]))
+      before do
+        FactoryBot.create(:constraint, key: 'total_primary_energy')
+        FactoryBot.create(:constraint, key: 'co2_reduction')
+      end
+
+      it { expect(constraints.length).to eq(2) }
+
+      it 'returns the total_primary_energy constraint first' do
+        expect(constraints[0]).to eql(described_class.find_by_key(keys[0]))
+      end
+
+      it 'returns the co2_reduction constraint second' do
+        expect(constraints[1]).to eql(described_class.find_by_key(keys[1]))
+      end
+
+      it 'returns nothing third' do
+        expect(constraints[2]).to be_nil
+      end
     end
 
-    it 'should return the co2_reduction constraint second' do
-      expect(subject[1]).to eql(Constraint.find_by_key(keys[1]))
+    context 'when given an array with an invalid key' do
+      let(:keys) { %w[total_primary_energy does_not_exist] }
+
+      before { FactoryBot.create(:constraint, key: 'total_primary_energy') }
+
+      it 'raises an error' do
+        expect { constraints }
+          .to raise_error(Constraint::NoSuchConstraint, /does_not_exist/)
+      end
     end
 
-    it 'should return nothing third' do
-      expect(subject[2]).to be_nil
+    context 'when given an array with a blank key' do
+      let(:keys) { ['total_primary_energy', ''] }
+
+      it 'raises an error' do
+        expect { constraints }.to raise_error(Constraint::IllegalConstraintKey)
+      end
     end
-  end
 
-  context 'when given an array with an invalid key' do
-    let(:keys) { %w( total_primary_energy does_not_exist ) }
-    before { FactoryBot.create :constraint, key: 'total_primary_energy'}
-    it 'should raise an error' do
-      expect { Constraint.for_dashboard!(keys) }.to \
-        raise_error(Constraint::NoSuchConstraint, /does_not_exist/)
-    end
-  end
+    context 'when given an array with a nil key' do
+      let(:keys) { ['total_primary_energy', nil] }
 
-  context 'when given an array with a blank key' do
-    let(:keys) { [ 'total_primary_energy', '' ] }
-
-    it 'should raise an error' do
-      expect { Constraint.for_dashboard!(keys) }.to \
-        raise_error(Constraint::IllegalConstraintKey)
-    end
-  end
-
-  context 'when given an array with a nil key' do
-    let(:keys) { [ 'total_primary_energy', nil ] }
-
-    it 'should raise an error' do
-      expect { Constraint.for_dashboard!(keys) }.to \
-        raise_error(Constraint::IllegalConstraintKey)
+      it 'raise an error' do
+        expect { constraints}.to raise_error(Constraint::IllegalConstraintKey)
+      end
     end
   end
 end
-

b9f2c60ed99949773ee2df8fa4f7e8ce7ccbccae
Anthony Williams <hi@antw.dev>
2019-07-08 15:25:30 +0100

    Allow MYC to use scenarios from any region
    Ref #3081

--- app/helpers/multi_year_charts_helper.rb
+++ app/helpers/multi_year_charts_helper.rb
@@ -11,11 +11,6 @@ module MultiYearChartsHelper
 
   def can_use_as_myc_scenario?(saved_scenario)
     scenario = saved_scenario.scenario
-
-    scenario && (
-      scenario.loadable? &&
-      scenario.end_year == 2050 &&
-      scenario.area_code == 'nl'
-    )
+    scenario && (scenario.loadable? && scenario.end_year == 2050)
   end
 end

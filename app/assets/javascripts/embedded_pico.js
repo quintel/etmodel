4a49ba7935520e4de1464c81308d4f4e69b3ead9
Steven Kemp <steven.kemp@quintel.com>
2019-09-03 13:59:03 +0200

    improved code style and components a little

--- app/assets/javascripts/embedded_pico.js
+++ app/assets/javascripts/embedded_pico.js
@@ -2,20 +2,17 @@
 embeddedPico = {
   attach: function(){
     document.getElementById('close-modal')
-            .addEventListener('click', embeddedPico.close,          false)
-    window.addEventListener("message", embeddedPico.receiveMessage, false);
+            .addEventListener('click',   embeddedPico.close,          false)
+    window.addEventListener(  'message', embeddedPico.receiveMessage, false)
   },
 
   close: function(event){
-    console.log(close)
     document.getElementsByClassName("modal__backdrop")[0].remove()
-    if(event != undefined){
-      event.preventDefault()
-    }
+    if(event != undefined){ event.preventDefault() }
   },
 
   updateInlandWindTurbine: function(new_value){
-    ie = App.input_elements
+    var ie = App.input_elements
             .find_by_key('capacity_of_energy_power_wind_turbine_inland')
     ie.set({user_value: new_value})
   },
@@ -26,21 +23,16 @@ embeddedPico = {
     if(message["action"] === "updateInlandWindTurbine"){
       embeddedPico.updateInlandWindTurbine(message.power)
 
-      console.log("before close ")
-
       // close modal
-
       embeddedPico.close()
-      console.log("after close")
-      console.log("before destructing listener")
 
-      // destruct eventlistener after use.
+      // destruct event listener after use.
       event.currentTarget
            .removeEventListener(event.type, embeddedPico.receiveMessage)
 
-      console.log("after destructing listener")
+    // Log something ifthe postMessage action is isn't handled.
     } else {
-      console.log("Unmanaged postMessage action")
+      console.log("Unhandled postMessage action")
     }
   }
 }
--- app/assets/javascripts/pico.js
+++ app/assets/javascripts/pico.js
@@ -18,5 +18,24 @@ var pico = {
   inMegaWatt: function(turbines){
     // source: V(energy_power_wind_turbine_inland, electricity_output_capacity)
     return turbines * 3.0
+  },
+
+  main: function(opts){
+    pico.areaType = opts.areaType,
+    pico.areaName = opts.areaName
+    //load the html template
+    initPico()
+
+    //initialise the map
+    initPicomap();
+
+    //initialise the windturbine module with a specified area
+    var selectedArea = {
+      areatype: pico.areaType,
+      areaname: pico.areaName
+      //areatype:'gemeente', areaname:'Neder-Betuwe', areacode:'1740'
+      }
+    console.log(selectedArea)
+    picoInitWindenergy( selectedArea )
   }
 }

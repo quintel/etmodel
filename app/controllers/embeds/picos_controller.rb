3f67b1946fa03ae37121e1e6d95e2d1772a8b36a
Steven Kemp <steven.kemp@quintel.com>
2019-09-05 15:29:40 +0200

    removed a redundant http request and factored out old code

--- app/controllers/embeds/picos_controller.rb
+++ app/controllers/embeds/picos_controller.rb
@@ -4,21 +4,10 @@ module Embeds
   # Generates an embedable version of pico
   class PicosController < ApplicationController
     layout false
-
+    
     def show
-      respond_to do |format|
-        format.html do
-          set_scenario_area
-          @pico_module = params[:pico_module]
-        end
-      end
-    end
-
-    private
-
-    def set_scenario_area
-      scenario = Api::Scenario.find(Current.setting.api_session_id)
-      @area = Embeds::PicoArea.find_by_area_code(scenario.area_code)
+      @area = Embeds::PicoArea.find_by_area_code(Current.setting.area_code)
+      @pico_module = params[:pico_module]
     end
   end
 end

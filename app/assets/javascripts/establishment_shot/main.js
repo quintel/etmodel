d2ac3b7a6fe43b654ea8eee76db5ef581070256d
noracato <ncschinkel@gmail.com>
2019-10-28 10:19:15 +0100

    added 2 space indents

--- app/assets/javascripts/establishment_shot/main.js
+++ app/assets/javascripts/establishment_shot/main.js
@@ -1,75 +1,75 @@
 var EstablishmentShot = {};
 
 EstablishmentShot.Main = (function() {
-    'use strict';
+  'use strict';
 
-    function prepare(data) {
-        var key,
-            quantity,
-            query,
-            result = data;
+  function prepare(data) {
+    var key,
+        quantity,
+        query,
+        result = data;
 
-        for (key in data.gqueries) {
-            query = data.gqueries[key];
+    for (key in data.gqueries) {
+        query = data.gqueries[key];
 
-            if (Quantity.isSupported(query.unit)) {
-                quantity = new Quantity(query.present, query.unit).smartScale();
+      if (Quantity.isSupported(query.unit)) {
+        quantity = new Quantity(query.present, query.unit).smartScale();
 
-                result.gqueries[key] = {
-                    present: quantity.value,
-                    unit: quantity.unit.name
-                }
-            } else {
-                result.gqueries[key] = query;
-            }
+        result.gqueries[key] = {
+            present: quantity.value,
+            unit: quantity.unit.name
         }
-
-        return result;
+      } else {
+        result.gqueries[key] = query;
+      }
     }
 
-    function finished(data) {
-        EstablishmentShot.TextUpdater.update(this, data);
-        EstablishmentShot.ChartRenderer.render(this, data);
+    return result;
+  }
 
-        this.scope.find('.overview').show();
-        $('a.scenario-link').attr('href', '/scenarios/' + data.scenario.id + '/load');
-        $('span.loading').remove();
+  function finished(data) {
+    EstablishmentShot.TextUpdater.update(this, data);
+    EstablishmentShot.ChartRenderer.render(this, data);
+
+    this.scope.find('.overview').show();
+    $('a.scenario-link').attr('href', '/scenarios/' + data.scenario.id + '/load');
+    $('span.loading').remove();
+  }
+
+  function updateScenario(data) {
+    EstablishmentShot.ScenarioUpdater
+      .updateScenario(this, data.id)
+      .done(finished.bind(this))
+      .fail(EstablishmentShot.ErrorHandler.display.bind(this));
+  }
+
+  function createScenario() {
+    EstablishmentShot.ScenarioCreator.create(this)
+      .done(updateScenario.bind(this))
+      .fail(EstablishmentShot.ErrorHandler.display.bind(this));
+  }
+
+  function getQueries() {
+    return this.scope.find('span[data-query]').map(function () {
+      return $(this).data('query');
+    }).toArray();
+  }
+
+  Main.prototype = {
+    render: function () {
+      this.queries = getQueries.call(this)
+        .concat(EstablishmentShot.Charts.getQueries());
+
+      createScenario.call(this);
     }
+  };
 
-    function updateScenario(data) {
-        EstablishmentShot.ScenarioUpdater
-            .updateScenario(this, data.id)
-            .done(finished.bind(this))
-            .fail(EstablishmentShot.ErrorHandler.display.bind(this));
-    }
+  function Main(scope) {
+    this.scope = scope;
+    this.data  = $(scope).data();
+    this.area  = this.data.area;
+    this.host  = this.data.host + '/api/v3/scenarios';
+  }
 
-    function createScenario() {
-        EstablishmentShot.ScenarioCreator.create(this)
-            .done(updateScenario.bind(this))
-            .fail(EstablishmentShot.ErrorHandler.display.bind(this));
-    }
-
-    function getQueries() {
-        return this.scope.find('span[data-query]').map(function () {
-            return $(this).data('query');
-        }).toArray();
-    }
-
-    Main.prototype = {
-        render: function () {
-            this.queries = getQueries.call(this)
-                .concat(EstablishmentShot.Charts.getQueries());
-
-            createScenario.call(this);
-        }
-    };
-
-    function Main(scope) {
-        this.scope = scope;
-        this.data  = $(scope).data();
-        this.area  = this.data.area;
-        this.host  = this.data.host + '/api/v3/scenarios';
-    }
-
-    return Main;
+  return Main;
 }());

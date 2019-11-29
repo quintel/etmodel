quintel/etmodel

commit 96353e6f2e749ac0867671e9f9d467fc6efc7852

FlexibilityOrder uses ETEngine API entirely
Instead of defining the options in ETModel, read the options from the
ETEngine API and render the list dynamically.

app/assets/javascripts/lib/util/flexibility_order.js
@@ -1,0 +1,96 @@
+/* globals _ $ App I18n Sortable */
+
+(function(window) {
+  var optionTemplate = _.template(
+    '<li data-id="<%= id %>">' +
+      '  <%- name %>' +
+      '  <span class="fa fa-bars></span>' +
+      '</li>'
+  );
+
+  /**
+   * Given an element and a list of flexibility options, renders a list element
+   * which may be used by Sortable.
+   */
+  function renderOptions(element, options) {
+    options.forEach(function(optionKey) {
+      element.append(
+        optionTemplate({
+          id: optionKey,
+          name: I18n.t('output_elements.flexibility_options.' + optionKey)
+        })
+      );
+    });
+  }
+
+  /**
+   * Handles setting up the user-sortable flexibility order, and triggers
+   * fetching and receiving the order from ETEngine.
+   */
+  var FlexibilityOrder = function(element) {
+    this.element = $(element);
+    this.lastGood = null;
+    this.url = App.scenario.url_path() + '/flexibility_order';
+  };
+
+  /**
+   * Renders the flexibility order, setting up Sortable and necessary callbacks.
+   */
+  FlexibilityOrder.prototype.render = function() {
+    var self = this;
+    var sortableEl = this.element.find('.sortable');
+
+    this.element.addClass('loading');
+
+    var xhr = $.ajax({
+      url: this.url,
+      type: 'GET'
+    });
+
+    xhr.success(function(data) {
+      renderOptions(sortableEl, data.order);
+      self.element.removeClass('loading');
+
+      Sortable.create(sortableEl[0], {
+        ghostClass: 'ghost',
+        animation: 150,
+        store: {
+          get: function() {
+            self.lastGood = data.order;
+            return data.order;
+          },
+          set: function(sortable) {
+            self.update(sortable);
+          }
+        }
+      });
+    });
+
+    /**
+     * Receives a sortable instance and triggers an update to ETEngine.
+     */
+    FlexibilityOrder.prototype.update = function(sortable) {
+      var self = this;
+      var options = sortable.toArray();
+
+      var xhr = $.ajax({
+        url: this.url,
+        type: 'PUT',
+        data: { flexibility_order: { order: options } }
+      });
+
+      xhr.success(function() {
+        App.call_api();
+        self.lastGood = options;
+      });
+
+      xhr.error(function() {
+        if (self.lastGood) {
+          sortable.sort(self.lastGood);
+        }
+      });
+    };
+  };
+
+  window.FlexibilityOrder = FlexibilityOrder;
+})(window);

app/assets/stylesheets/sortable.sass
@@ -14,7 +14,7 @@ ul.sortable li, #flexibility-options .curtailment
   list-style-type: none
   margin-bottom: -1px
   overflow: hidden
-  padding: 10px 14px 10px 50px
+  padding: 10px 14px 10px 53px
   position: relative
 
   &:before
@@ -26,12 +26,12 @@ ul.sortable li, #flexibility-options .curtailment
     content: counter(section)
     display: inline-block
     left: 0px
-    padding: 10px 14px 0
+    padding: 10px 0 0
     position: absolute
     right: 0px
     text-align: center
     top: 0px
-    width: 10px
+    width: 41px
 
   span.fa
     color: #BBB
@@ -72,6 +72,9 @@ ul.sortable li
       content: counter(section)
 
 #flexibility-options
+  .loading-message
+    display: none
+
   .curtailment
     border-radius: 3px
     color: #999
@@ -87,3 +90,12 @@ ul.sortable li
   .curtailment, .sortable li:last-child
     border-bottom-color: #ccc
     box-shadow: 0 1px 0 #ddd
+
+  &.loading
+    .loading-message
+      background: asset-url("layout/ajax-loader.gif") center center no-repeat
+      display: block
+      height: 100px
+
+    .sortable, .curtailment
+      display: none !important

app/assets/javascripts/lib/app.coffee
@@ -53,8 +53,8 @@ class @AppView extends Backbone.View
     wrapper = $('#accordion_wrapper')
 
     # Create flexibility order.
-    if (sortable = wrapper.find('ul.sortable')).length
-      new FlexibilityOrder(sortable[0]).render()
+    if (flex_order = wrapper.find('#flexibility-options')).length
+      new FlexibilityOrder(flex_order).render()
 
     if (curve_upload = wrapper.find('.curve-upload')).length
       CustomCurveChooserView.setupWithWrapper(curve_upload).render();

app/assets/javascripts/lib/util/flexibility_order.coffee
@@ -1,38 +1,0 @@
-class @FlexibilityOrder
-  constructor: (@element) ->
-    @lastGood = null
-
-  url: ->
-    "#{App.scenario.url_path()}/flexibility_order"
-
-  update: (sortable) =>
-    options = sortable.toArray()
-
-    $.ajax
-      url: @url(),
-      type: 'POST',
-      data:
-        flexibility_order:
-          order: options
-      success: =>
-        App.call_api()
-        @lastGood = options
-      error: (e,f) =>
-        if @lastGood
-          sortable.sort(@lastGood)
-
-  render: =>
-    $.ajax
-      url: @url()
-      type: 'GET'
-      success: (data) =>
-        Sortable.create @element,
-          ghostClass: 'ghost'
-          animation: 150
-          store:
-            get: (_sortable) =>
-              @lastGood = data.order
-              data.order
-
-            set: (sortable) =>
-              @update(sortable)

app/models/flexibility_order.rb
@@ -1,14 +1,0 @@
-class FlexibilityOrder
-  def self.all
-    %w(household_batteries
-       mv_batteries
-       electric_vehicle
-       opac
-       pumped_storage
-       power_to_heat
-       power_to_heat_industry
-       power_to_gas
-       power_to_kerosene
-       export)
-  end
-end

app/views/scenarios/slides/_flexibility_flexibility_flexibility_order.html.haml
@@ -1,9 +1,7 @@
 #flexibility-options.sortable-container
+  .loading-message
+
   %ul.sortable
-    - FlexibilityOrder.all.each do |flexibility_option|
-      %li{data: { id: flexibility_option }}
-        = t("output_elements.flexibility_options.#{ flexibility_option }")
-        %span.fa.fa-bars
 
   .curtailment
     = t('output_elements.flexibility_options.curtailment')

config/i18n-js.yml
@@ -20,6 +20,7 @@ translations:
       - '*.output_elements.import_export_capacity_chart'
       - '*.output_elements.import_export_renewables_chart'
       - '*.output_elements.sankey_labels'
+      - '*.output_elements.flexibility_options'
       - '*.input_elements.common'
       - '*.constraints.co2_reduction.*'
       - '*.constraints.greenhouse_gas.*'

commit 5ecb158bfd48276f282167f96186053e0f179003

Fix flexibility "bars" icon I broke in 96353e6

app/assets/javascripts/lib/util/flexibility_order.js
@@ -4,8 +4,8 @@
   var optionTemplate = _.template(
     '<li data-id="<%= id %>">' +
       '  <%- name %>' +
+      '  <span class="fa fa-bars"></span>' +
       '  <div class="flexibility-options__capacity"><%- capacity %></div>' +
-      '  <span class="fa fa-bars></span>' +
       '</li>'
   );
 

app/assets/stylesheets/sortable.sass
@@ -38,6 +38,7 @@ ul.sortable li, #flexibility-options .curtailment
     float: right
     line-height: 18px
     margin-top: 2px
+    margin-left: 10px
 
 ul.sortable li
   cursor: grab
@@ -91,7 +92,6 @@ ul.sortable li
     .fa
       color: #ccc
       font-size: 17px
-      margin-right: 1px
 
   .curtailment, .sortable li:last-child
     border-bottom-color: #ccc
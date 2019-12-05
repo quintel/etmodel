b9051da99ad061397d2b632906a417d9547450ac
noracato <ncschinkel@gmail.com>
2019-12-05 15:21:39 +0100

    Add/remove empty lines

--- spec/controllers/saved_scenario_reports_controller_spec.rb
+++ spec/controllers/saved_scenario_reports_controller_spec.rb
@@ -5,26 +5,18 @@ describe SavedScenarioReportsController, vcr: true do
   let!(:user_scenario) { FactoryBot.create :saved_scenario, user: user, id: 648695 }
 
   describe 'GET show' do
-
     context 'ovm.csv' do
       before(:each){ get :show, params: { report_name: 'ovm.csv',
                                           saved_scenario_id: user_scenario.id } }
-
       it 'has yml' do
         expect(assigns(:yml)).to_not be_empty
       end
-
       it 'has queries' do
         expect(assigns(:queries)).to_not be_empty
       end
-
       it 'has an api_response with queries' do
         expect(assigns(:query_api_response)).to_not be_empty
       end
     end
   end
-
-
-
-
 end

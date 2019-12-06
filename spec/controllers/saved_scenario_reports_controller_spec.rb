4edcff90e023984fb21a59a2b75062ea165798e3
Steven Kemp <steven.kemp@quintel.com>
2019-12-06 13:57:34 +0100

    fixed tests

--- spec/controllers/saved_scenario_reports_controller_spec.rb
+++ spec/controllers/saved_scenario_reports_controller_spec.rb
@@ -5,23 +5,27 @@ require 'rails_helper'
 describe SavedScenarioReportsController, vcr: true do
   let(:user) { FactoryBot.create :user }
   let!(:user_scenario) do
-    FactoryBot.create :saved_scenario, user: user, id: 648695
+    FactoryBot.create :saved_scenario, user: user
   end
 
   describe 'GET show' do
     context 'ovm.csv' do
       before(:each) do
-        get :show, params: { report_name: 'ovm.csv',
-                             saved_scenario_id: user_scenario.id }
+        get :show,
+            format: :csv,
+            params: { report_name: 'ovm', saved_scenario_id: user_scenario.id }
       end
-      it 'has yml' do
-        expect(assigns(:yml)).to_not be_empty
+
+      subject { response }
+
+      it { is_expected.to have_http_status(200) }
+
+      it 'has a report_template' do
+        expect(assigns(:report_template)).to_not be_empty
       end
+
       it 'has queries' do
         expect(assigns(:queries)).to_not be_empty
-      end
-      it 'has an api_response with queries' do
-        expect(assigns(:query_api_response)).to_not be_empty
       end
     end
   end

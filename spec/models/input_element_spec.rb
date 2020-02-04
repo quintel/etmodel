1b0eac853658a8e682043484319299cecbc75785
Steven Kemp <steven.kemp@quintel.com>
2020-02-04 11:04:56 +0100

    uncommented failing tests

--- spec/models/input_element_spec.rb
+++ spec/models/input_element_spec.rb
@@ -52,22 +52,22 @@ describe InputElement do
     it { is_expected.to include ie }
   end
 
-  # describe '#url_components' do
-  #   context 'when the input a slide, sidebar item, and tab' do
-  #     let(:slide) { Slide.visible.first }
-  #     let(:input) { FactoryBot.create(:input_element, slide: slide) }
+  describe '#url_components' do
+    context 'when the input a slide, sidebar item, and tab' do
+      let(:slide) { Slide.visible.first }
+      let(:input) { FactoryBot.create(:input_element, slide: slide) }
 
-  #     it 'returns the slide URL components' do
-  #       expect(input.url_components).to eq(slide.url_components)
-  #     end
-  #   end
+      it 'returns the slide URL components' do
+        expect(input.url_components).to eq(slide.url_components)
+      end
+    end
 
-  #   context 'when the input has no slide' do
-  #     let(:input) { FactoryBot.create(:input_element, slide: nil) }
+    context 'when the input has no slide' do
+      let(:input) { FactoryBot.create(:input_element, slide: nil) }
 
-  #     it 'returns an empty array' do
-  #       expect(input.url_components).to eq([])
-  #     end
-  #   end
-  # end
+      it 'returns an empty array' do
+        expect(input.url_components).to eq([])
+      end
+    end
+  end
 end
--- spec/models/slide_spec.rb
+++ spec/models/slide_spec.rb
@@ -49,39 +49,39 @@ describe Slide do
 
 
 
-  # describe '#url_components' do
-  #   context 'when the slide has a sidebar item and tab' do
-  #     let(:slide) { FactoryBot.create(:slide) }
+  describe '#url_components' do
+    context 'when the slide has a sidebar item and tab' do
+      let(:slide) { FactoryBot.create(:slide) }
 
-  #     it 'returns the tab key first' do
-  #       expect(slide.url_components[0]).to eq(slide.sidebar_item.tab.key)
-  #     end
+      it 'returns the tab key first' do
+        expect(slide.url_components[0]).to eq(slide.sidebar_item.tab.key)
+      end
 
-  #     it 'returns the sidebar item key second' do
-  #       expect(slide.url_components[1]).to eq(slide.sidebar_item.key)
-  #     end
+      it 'returns the sidebar item key second' do
+        expect(slide.url_components[1]).to eq(slide.sidebar_item.key)
+      end
 
-  #     it 'returns the slide name last' do
-  #       expect(slide.url_components[2]).to eq(slide.short_name)
-  #     end
-  #   end
+      it 'returns the slide name last' do
+        expect(slide.url_components[2]).to eq(slide.short_name)
+      end
+    end
 
-  #   context 'when the slide has no sidebar item' do
-  #     let(:slide) { FactoryBot.create(:slide, sidebar_item: nil) }
+    context 'when the slide has no sidebar item' do
+      let(:slide) { FactoryBot.create(:slide, sidebar_item: nil) }
 
-  #     it 'returns an empty array' do
-  #       expect(slide.url_components).to eq([])
-  #     end
-  #   end
+      it 'returns an empty array' do
+        expect(slide.url_components).to eq([])
+      end
+    end
 
-  #   context 'when the sidebar item has no tab' do
-  #     let(:slide) do
-  #       Slide.new(sidebar_item: SidebarItem.new(tab_id: nil))
-  #     end
+    context 'when the sidebar item has no tab' do
+      let(:slide) do
+        Slide.new(sidebar_item: SidebarItem.new(tab_id: nil))
+      end
 
-  #     it 'returns an empty array' do
-  #       expect(slide.url_components).to eq([])
-  #     end
-  #   end
-  # end
+      it 'returns an empty array' do
+        expect(slide.url_components).to eq([])
+      end
+    end
+  end
 end

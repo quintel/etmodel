d1f201ea414476f35c8e479f7f58274441758ff8
Anthony Williams <hi@antw.dev>
2020-02-05 15:56:34 +0000

    Update InputElement.Balancer code style
    Updated per modern ESLint rules

--- app/assets/javascripts/lib/models/input_element_balancer.js
+++ app/assets/javascripts/lib/models/input_element_balancer.js
@@ -1,8 +1,10 @@
-(function (window) {
+/* globals _ Backbone */
+
+(function(window) {
   'use strict';
 
   // Input groups which require all the sliders to be sent to ETengine.
-  var SEND_ENTIRE_GROUP = [ 'coal_lce', 'gas_lce', 'nuclear_lce' ];
+  var SEND_ENTIRE_GROUP = ['coal_lce', 'gas_lce', 'nuclear_lce'];
 
   /**
    * Creates a balancer instance.
@@ -22,20 +24,20 @@
    *       subordinates are the sliders whose values are changes to ensure
    *       that the group remains balanced.
    */
-  function Balancer (options) {
+  function Balancer(options) {
     _.bindAll(this, 'begin', 'drag', 'change', 'abort');
 
-    this.options   = _.clone(options || {});
-    this.max       = this.__getMax();
+    this.options = _.clone(options || {});
+    this.max = this.__getMax();
     this.precision = 1;
 
-    this.views      = [];
-    this.quinns     = [];
+    this.views = [];
+    this.quinns = [];
     this.quinnOrder = [];
 
-    this.oValues    = null;
+    this.oValues = null;
 
-    this.masterId     = null;
+    this.masterId = null;
     this.subordinates = null;
   }
 
@@ -51,8 +53,8 @@
 
   // Gets the Balancer with the given name. If one does not exist, it
   // will be created with the given options.
-  Balancer.get = function (name, options) {
-    if (! Balancer.balancers.hasOwnProperty(name)) {
+  Balancer.get = function(name, options) {
+    if (!Object.prototype.hasOwnProperty.call(Balancer.balancers, name)) {
       Balancer.balancers[name] = new Balancer(options);
     }
 
@@ -70,21 +72,21 @@
    * Adds a slider to be balanced. Note that `add` expects an
    * InputElementView, _not_ an InputElement.
    */
-  Balancer.prototype.add = function (inputView) {
+  Balancer.prototype.add = function(inputView) {
     var quinn = inputView.quinn;
 
     quinn.balanceId = _.uniqueId('balance_');
 
-    quinn.on('begin',  this.begin);
-    quinn.on('drag',   this.drag);
+    quinn.on('begin', this.begin);
+    quinn.on('drag', this.drag);
     quinn.on('change', this.change);
-    quinn.on('abort',  this.abort);
+    quinn.on('abort', this.abort);
 
     this.views.push(inputView);
     this.quinns.push(inputView.quinn);
     this.quinnOrder.push(quinn.balanceId);
 
-    this.max       = this.__getMax();
+    this.max = this.__getMax();
     this.precision = this.__getPrecision();
 
     return this;
@@ -94,9 +96,9 @@
    * Triggered when the user alteres a slider; performs balancing of the
    * subordinates.
    */
-  Balancer.prototype.doBalance = function (newValue, quinn) {
-    var amountChanged = newValue - this.oValues.value(quinn),
-        valuesBeforeBalancing, flex, sLength, slider, prevValue, i;
+  Balancer.prototype.doBalance = function(newValue, quinn) {
+    var amountChanged = newValue - this.oValues.value(quinn);
+    var sLength = this.subordinates.length;
 
     // Return quickly: if the amount changed is larger than the balancer
     // allows as the change is impossible.
@@ -104,13 +106,13 @@
       return false;
     }
 
-    if ((sLength = this.subordinates.length) === 0) {
+    if (sLength === 0) {
       // Return quickly if all none of the subordinate sliders can be moved
       // any further.
       return false;
     }
 
-    valuesBeforeBalancing = new OriginalValues(this.subordinates);
+    var valuesBeforeBalancing = new OriginalValues(this.subordinates);
 
     // Flex is the amount of "value" which needs to be adjusted for. e.g.
     //
@@ -121,17 +123,17 @@
     //  If slider 1 is moved to 25, the flex is -25 since in order to balance
     //  the sliders we need to subtract 25 from the subordinates.
     //
-    flex = this.oValues.sumOf(this.subordinates) + newValue;
+    var flex = this.oValues.sumOf(this.subordinates) + newValue;
     flex = this.snapValue(this.max - flex);
 
-    for (i = 0; i < sLength; i++) {
+    for (var i = 0; i < sLength; i++) {
       // The balancer seeks to assign the full flex amount to one slider; if
       // this can't be done, it moves on to the next slider, and keeps going
       // until either all of the flex has been assigned, or we run out of
       // sliders.
 
-      slider    = this.subordinates[i];
-      prevValue = this.oValues.value(slider);
+      var slider = this.subordinates[i];
+      var prevValue = this.oValues.value(slider);
 
       slider.setTentativeValue(prevValue + flex);
 
@@ -167,18 +169,19 @@
    * In these cases, the balancer will invoke `balanceToClosestEquilibrium` to
    * find the closest acceptable value.
    */
-  Balancer.prototype.balanceToClosestEquilibrium = function (quinn, target) {
-    var isIncreasing = (target > quinn.previousValue),
-        lower        = (isIncreasing ? quinn.previousValue : target),
-        upper        = (isIncreasing ? target : quinn.previousValue),
-        midPoint, knownGood, previous;
+  Balancer.prototype.balanceToClosestEquilibrium = function(quinn, target) {
+    var isIncreasing = target > quinn.previousValue;
+    var lower = isIncreasing ? quinn.previousValue : target;
+    var upper = isIncreasing ? target : quinn.previousValue;
+    var knownGood;
+    var previous;
 
-    while (true) {
-      midPoint = this.snapValue((lower + upper) / 2);
+    for (;;) {
+      var midPoint = this.snapValue((lower + upper) / 2);
 
       if (midPoint === previous) {
         // We have run out of values to test.
-        return (knownGood != null) ? knownGood : false;
+        return knownGood != null ? knownGood : false;
       }
 
       if (this.doBalance(midPoint, quinn)) {
@@ -201,23 +204,22 @@
    * _vast_ number of floating point errors which occur when a group contains
    * three or more sliders.
    */
-  Balancer.prototype.snapValue = function (value) {
+  Balancer.prototype.snapValue = function(value) {
     return parseFloat(value.toFixed(this.precision));
   };
 
   /**
    * Returns if the given Quinn instance is the current master slider.
    */
-  Balancer.prototype.isMaster = function (quinn) {
-    return (this.masterId !== null &&
-            this.masterId === quinn.balanceId);
+  Balancer.prototype.isMaster = function(quinn) {
+    return this.masterId !== null && this.masterId === quinn.balanceId;
   };
 
   /**
    * Resetting a single slider in a group resets them all to their original
    * values.
    */
-  Balancer.prototype.resetAll = function () {
+  Balancer.prototype.resetAll = function() {
     try {
       this.isDisabled = true;
       _.invoke(this.views, 'resetValue', false);
@@ -228,7 +230,7 @@
 
   // ## Event-Handling -------------------------------------------------------
 
-  Balancer.prototype.begin = function (value, quinn) {
+  Balancer.prototype.begin = function(value, quinn) {
     if (this.isDisabled) {
       return true;
     }
@@ -236,20 +238,20 @@
     // If no slider is already being adjusted, then this is when the
     // user begins moving a slider.
     if (this.masterId === null) {
-      this.masterId     = quinn.balanceId;
+      this.masterId = quinn.balanceId;
       this.subordinates = this.__getSubordinates();
-      this.oValues      = new OriginalValues(this.quinns);
+      this.oValues = new OriginalValues(this.quinns);
 
       this.__runOnSubordinates('start');
 
       this.trigger('begin');
     }
 
-    // Otherwise is is an begin event being fired when a subordinate slider is
+    // Otherwise it is a "begin" event being fired when a subordinate slider is
     // being balanced; ignore it.
   };
 
-  Balancer.prototype.drag = function (value, quinn) {
+  Balancer.prototype.drag = function(value, quinn) {
     // Fired when a slider value is changed. We only care about changes to the
     // master slider, and subordinates are ignored.
     if (this.isMaster(quinn)) {
@@ -269,7 +271,7 @@
     }
   };
 
-  Balancer.prototype.change = function (value, quinn) {
+  Balancer.prototype.change = function(value, quinn) {
     if (this.isMaster(quinn)) {
       this.__sliderUsed(quinn);
       this.__finish('resolve');
@@ -277,7 +279,7 @@
     }
   };
 
-  Balancer.prototype.abort = function (value, quinn) {
+  Balancer.prototype.abort = function(value, quinn) {
     if (this.isMaster(quinn)) {
       this.__finish('reject');
       this.trigger('abort');
@@ -290,11 +292,11 @@
    * Returns all the sliders except the one with which the user is
    * interacting and those which are disabled.
    */
-  Balancer.prototype.__getSubordinates = function () {
-    var self = this, subs;
+  Balancer.prototype.__getSubordinates = function() {
+    var self = this;
 
-    subs = _.select(this.quinns, function (quinn) {
-      return (! self.isMaster(quinn) && ! quinn.isDisabled);
+    var subs = _.select(this.quinns, function(quinn) {
+      return !self.isMaster(quinn) && !quinn.isDisabled;
     });
 
     if (this.quinns.length <= 2) {
@@ -304,7 +306,7 @@
     // The subs are now re-ordered so that the most recently used sliders are
     // at the end of the array. Otherwise doBalance is somewhat biased towards
     // the first slider.
-    return _.sortBy(subs, function (quinn) {
+    return _.sortBy(subs, function(quinn) {
       return _.indexOf(self.quinnOrder, quinn.balanceId);
     });
   };
@@ -313,7 +315,7 @@
    * Marks a slider as used, pushing it to the back of the quinnOrder array.
    * This prevents doBalance favouring balancing one slider over the others.
    */
-  Balancer.prototype.__sliderUsed = function (quinn) {
+  Balancer.prototype.__sliderUsed = function(quinn) {
     if (this.quinns.length <= 2) {
       return true;
     }
@@ -328,15 +330,21 @@
    * Returns the maximum permitted cumulative value of the sliders which are
    * members of the group. This is cached as this.max.
    */
-  Balancer.prototype.__getMax = function () {
+  Balancer.prototype.__getMax = function() {
     if (this.options.max) {
       return this.options.max;
     }
 
     // No explicit max, just use the total of all the sliders (no balancing).
-    return _.reduce(this.quinns, function (sum, quinn) {
-      return (sum + quinn.range[1]);
-    }, 0) / this.quinns.length;
+    return (
+      _.reduce(
+        this.quinns,
+        function(sum, quinn) {
+          return sum + quinn.range[1];
+        },
+        0
+      ) / this.quinns.length
+    );
   };
 
   /**
@@ -345,44 +353,47 @@
    * the smallest slider step value, so that we can do all balancing using
    * integers.
    */
-  Balancer.prototype.__getPrecision = function () {
-    return _.max(_.map(this.views, function (view) {
-      // Inputs in groups may have rounding errors (etmodel#1023) if
-      // insufficient precision is used. So, we add extra bits of precision
-      // in hope that this is enough to keep the group balancing.
-      return view.conversion.precision + Balancer.EXTRA_PRECISION;
-    })) || 0;
+  Balancer.prototype.__getPrecision = function() {
+    return (
+      _.max(
+        _.map(this.views, function(view) {
+          // Inputs in groups may have rounding errors (etmodel#1023) if
+          // insufficient precision is used. So, we add extra bits of precision
+          // in hope that this is enough to keep the group balancing.
+          return view.conversion.precision + Balancer.EXTRA_PRECISION;
+        })
+      ) || 0
+    );
   };
 
   /**
    * Finishes editing slider values by resetting this.masterId and
    * this.subordinates, and calling 'method' on the subordinates.
    */
-  Balancer.prototype.__finish = function (method) {
-    var groupName = this.views[0].model.get('share_group'),
-        i, length;
+  Balancer.prototype.__finish = function(method) {
+    var groupName = this.views[0].model.get('share_group');
 
     this.__runOnSubordinates(method);
 
     if (method === 'resolve' && _.include(SEND_ENTIRE_GROUP, groupName)) {
       // ETengine wants the whole group to be marked dirty.
-      for (i = 0, length = this.views.length; i < length; i++) {
+      for (var i = 0, length = this.views.length; i < length; i++) {
         this.views[i].model.markDirty();
       }
     }
 
-    this.masterId     = null;
+    this.masterId = null;
     this.subordinates = null;
-    this.oValues      = null;
+    this.oValues = null;
   };
 
   /**
    * Runs a method on all the subordinates.
    */
-  Balancer.prototype.__runOnSubordinates = function (method) {
-    var subLength = this.subordinates.length, i;
+  Balancer.prototype.__runOnSubordinates = function(method) {
+    var subLength = this.subordinates.length;
 
-    for (i = 0; i < subLength; i++) {
+    for (var i = 0; i < subLength; i++) {
       this.subordinates[i][method]();
     }
   };
@@ -393,14 +404,12 @@
    * Given an array of Quinn instances, OriginalValues keeps track of the
    * value of each slider at the time the OriginalValues instance was created.
    */
-  function OriginalValues (quinns) {
-    var i;
-
+  function OriginalValues(quinns) {
     this.length = quinns.length;
     this.quinns = quinns;
     this.values = {};
 
-    for (i = 0; i < this.length; i++) {
+    for (var i = 0; i < this.length; i++) {
       this.values[quinns[i].balanceId] = quinns[i].model.value;
     }
   }
@@ -409,7 +418,7 @@
    * Given a Quinn instance, returns the value of that instance at the time
    * the OriginalValues was initialized.
    */
-  OriginalValues.prototype.value = function (quinn) {
+  OriginalValues.prototype.value = function(quinn) {
     return this.values[quinn.balanceId];
   };
 
@@ -417,10 +426,11 @@
    * Given an array of Quinns, returns the sum of their values at the time the
    * OriginalValues was initialized.
    */
-  OriginalValues.prototype.sumOf = function (sliders) {
-    var sLength = sliders.length, sum = 0, i;
+  OriginalValues.prototype.sumOf = function(sliders) {
+    var sLength = sliders.length;
+    var sum = 0;
 
-    for (i = 0; i < sLength; i++) {
+    for (var i = 0; i < sLength; i++) {
       sum += this.value(sliders[i]);
     }
 
@@ -430,18 +440,13 @@
   /**
    * Reverts the Quinn instances back to their original value.
    */
-  OriginalValues.prototype.revert = function () {
-    var i;
-
-    for (i = 0; i < this.length; i++) {
-      this.quinns[i].setTentativeValue(
-        this.values[ this.quinns[i].balanceId ]
-      );
+  OriginalValues.prototype.revert = function() {
+    for (var i = 0; i < this.length; i++) {
+      this.quinns[i].setTentativeValue(this.values[this.quinns[i].balanceId]);
     }
   };
 
   // # Globals ---------------------------------------------------------------
 
   window.InputElement.Balancer = Balancer;
-
 })(window);

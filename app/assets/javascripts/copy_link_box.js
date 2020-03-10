df1ea06c9ff5ec65623d337b4e464e4331d54ec5
noracato <ncschinkel@gmail.com>
2020-03-10 13:09:24 +0100

    Change css class names

--- app/assets/javascripts/copy_link_box.js
+++ app/assets/javascripts/copy_link_box.js
@@ -1,11 +1,11 @@
 // Functions to toggle the copy link box for sharing
 // a scenario on the saved scenario page
 $(document).ready( function() {
-  $('.copy_link a').click(function() {
-    $('.copy_box').show();
+  $('.copy-link a').click(function() {
+    $('.copy-box').show();
     $('.url input').focus();
   });
   $('.cross').click(function() {
-    $('.copy_box').hide();
+    $('.copy-box').hide();
   });
 });

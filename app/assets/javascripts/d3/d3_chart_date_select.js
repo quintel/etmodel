a090daebebeb06cd30bb4589ef3a4923a932922c
Anthony Williams <hi@antw.dev>
2020-03-16 13:22:51 +0000

    Format D3ChartDateSelect

--- app/assets/javascripts/d3/d3_chart_date_select.js
+++ app/assets/javascripts/d3/d3_chart_date_select.js
@@ -1,81 +1,85 @@
-var D3ChartDateSelect = (function () {
-    'use strict';
+/* globals $ App d3 */
 
-    var epoch = new Date(0),
-        msInWeek = 604800000;
+var D3ChartDateSelect = (function() {
+  'use strict';
 
-    function buildOption(i) {
-        var nextIndex  = (i + 1),
-            timeFormat = d3.time.format("%b %d"),
-            msOffset   = (msInWeek * i),
-            start      = new Date(epoch.getDate() + msOffset),
-            end        = new Date(start.getDate() + msOffset + msInWeek),
-            optionText = (timeFormat(start) + " - " + timeFormat(end));
+  var epoch = new Date(0),
+    msInWeek = 604800000;
 
-        this.weeks[nextIndex] = [start, end];
+  function buildOption(i) {
+    var nextIndex = i + 1,
+      timeFormat = d3.time.format('%b %d'),
+      msOffset = msInWeek * i,
+      start = new Date(epoch.getDate() + msOffset),
+      end = new Date(start.getDate() + msOffset + msInWeek),
+      optionText = timeFormat(start) + ' - ' + timeFormat(end);
 
-        return '<option value="' + nextIndex + '">' + optionText + '</option>';
+    this.weeks[nextIndex] = [start, end];
+
+    return '<option value="' + nextIndex + '">' + optionText + '</option>';
+  }
+
+  function createOptions() {
+    var i,
+      options = '<option value="0">Whole year</option>';
+
+    for (i = 0; i < 52; i += 1) {
+      options += buildOption.call(this, i);
     }
 
-    function createOptions() {
-        var i,
-            options = '<option value="0">Whole year</option>';
+    return options;
+  }
 
-        for (i = 0; i < 52; i += 1) {
-            options += buildOption.call(this, i);
-        }
+  function updateMeritChartsDate(s, val) {
+    this.selectBox.val(val);
+    this.updateChart && this.updateChart();
+  }
 
-        return options;
+  function setMeritChartsDate() {
+    App.settings.set('merit_charts_date', $(this).val());
+  }
+
+  function buildSelectBox() {
+    return $('<select/>')
+      .addClass('d3-chart-date-select')
+      .append(createOptions.call(this))
+      .val(App.settings.get('merit_charts_date') || '1')
+      .on('change', setMeritChartsDate);
+  }
+
+  D3ChartDateSelect.prototype = {
+    selectBox: undefined,
+    weeks: [[epoch, new Date(1970, 11, 30)]],
+
+    draw: function(updateChart) {
+      this.updateChart = updateChart;
+      this.selectBox = buildSelectBox.call(this);
+
+      this.scope.append(this.selectBox);
+
+      App.settings.on(
+        'change:merit_charts_date',
+        updateMeritChartsDate.bind(this)
+      );
+    },
+
+    getCurrentRange: function() {
+      return this.weeks[this.val()];
+    },
+
+    val: function() {
+      return parseInt(this.selectBox.val(), 10);
+    },
+
+    isWeekly: function() {
+      return this.val() > 0;
     }
+  };
 
-    function updateMeritChartsDate(s, val) {
-        this.selectBox.val(val);
-        this.updateChart && this.updateChart();
-    }
+  function D3ChartDateSelect(scope, range) {
+    this.scope = $(scope);
+    this.range = range;
+  }
 
-    function setMeritChartsDate() {
-        App.settings.set('merit_charts_date', $(this).val());
-    }
-
-    function buildSelectBox() {
-        return $("<select/>")
-            .addClass("d3-chart-date-select")
-            .append(createOptions.call(this))
-            .val(App.settings.get('merit_charts_date') || '1')
-            .on('change', setMeritChartsDate);
-    }
-
-    D3ChartDateSelect.prototype = {
-        selectBox: undefined,
-        weeks: [ [ epoch, new Date(1970, 11, 30) ] ],
-        draw: function (updateChart) {
-            this.updateChart = updateChart;
-            this.selectBox = buildSelectBox.call(this);
-
-            this.scope.append(this.selectBox);
-
-            App.settings.on('change:merit_charts_date',
-                updateMeritChartsDate.bind(this));
-
-        },
-
-        getCurrentRange: function () {
-            return this.weeks[this.val()];
-        },
-
-        val: function () {
-            return parseInt(this.selectBox.val(), 10);
-        },
-
-        isWeekly: function () {
-            return this.val() > 0;
-        }
-    };
-
-    function D3ChartDateSelect(scope, range) {
-        this.scope = $(scope);
-        this.range = range;
-    }
-
-    return D3ChartDateSelect;
-}());
+  return D3ChartDateSelect;
+})();

2a83e0056aaf94f56d85d72ffd76dd5527b6640d
Stev-0 <steven.kemp@quintel.com>
2020-04-07 11:01:34 +0200

    fix tests by adding protected to scenario mocks

--- spec/support/mocks/ete_scenario.rb
+++ spec/support/mocks/ete_scenario.rb
@@ -17,6 +17,7 @@ def ete_scenario_mock
   allow(mock).to receive(:errors).and_return([])
   allow(mock).to receive(:use_fce).and_return(nil)
   allow(mock).to receive(:scaling).and_return(nil)
+  allow(mock).to receive(:protected?).and_return(false)
   allow(mock).to receive(:user_values)  do
     double('user_values', attributes: { foo: :bar })
   end

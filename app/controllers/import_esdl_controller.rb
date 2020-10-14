90756c18d98241434bde3d6cd645b55d74dbda3c
Anthony Williams <hi@antw.dev>
2020-10-14 12:27:33 +0100

    Disable ESDL feature when config is absent

--- app/controllers/import_esdl_controller.rb
+++ app/controllers/import_esdl_controller.rb
@@ -3,6 +3,10 @@
 # Only index and create. Calls a service that generates an ETE scenario based on the
 # specified ESDL file. The service returns the scenario_id when (fully) succesful.
 class ImportEsdlController < ApplicationController
+  before_action :ensure_esdl_enabled
+
+  def index; end
+
   def create
     result = CreateEsdlScenario.call(params[:esdl_file])
 
@@ -11,5 +15,11 @@ class ImportEsdlController < ApplicationController
     else
       redirect_to load_scenario_path(id: result.value['scenario_id'])
     end
+  end
+
+  private
+
+  def ensure_esdl_enabled
+    redirect_to(root_url) unless APP_CONFIG[:esdl_api_url]
   end
 end

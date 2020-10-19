652a72978b054b406a24c2faee10cba695364193
Anthony Williams <hi@antw.dev>
2020-10-19 15:38:15 +0100

    Upgrade to Rails 6
    * Drops Compass/Blueprint which is no longer compatiable.
    
    * Updates AuthLogic from v4 to v6, which means adding e-mail and
      password validations.
    
    * Moves Sass partials to have a leading "_" in the name. Adjust the
      manifest.js to reference the top-level CSS files explicitly.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe User do
   it { is_expected.to validate_presence_of :name }
-  it { is_expected.to belong_to :teacher }
+  it { is_expected.to belong_to(:teacher).optional }
   it { is_expected.to have_many :students }
 end

1b43b9a49aa2ff12b75337924c3a4b580289b164
Nora Schinkel <ncschinkel@gmail.com>
2020-11-23 12:27:42 +0100

    Second iteration of login-to-Mondaine-Drive design

--- app/assets/javascripts/esdl.js
+++ app/assets/javascripts/esdl.js
@@ -10,11 +10,28 @@ $(function() {
       form.find('input[type=submit]').remove();
       form.find('.wait').show();
     });
+
+    esdlForm.find('.upload_file').hover(function() {
+      $('#mondaine_drive').addClass('soften');
+    }, function(){
+      $('#mondaine_drive').removeClass('soften');
+    });
   }
 });
 
+$(function() {
+  var mondaineDrive = $('#mondaine_drive');
+
+  if (mondaineDrive.length) {
+    mondaineDrive.hover( function() {
+      $('#import_esdl .upload_file').addClass('soften');
+    }, function(){
+      $('#import_esdl .upload_file').removeClass('soften');
+    });
+  }
+});
+
+
 function startBrowsingMondaineDrive() {
-  console.log($('.login_to_drive'));
-  console.log('bleh');
   $('.login_to_drive').hide();
 }

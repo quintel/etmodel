b7a2469543c48819c4870a1e661de8893a419980
Nora Schinkel <ncschinkel@gmail.com>
2021-01-11 10:10:13 +0100

    Mock esdl-suite config for rspec

--- spec/support/esdl_suite_helper.rb
+++ spec/support/esdl_suite_helper.rb
@@ -2,6 +2,8 @@
 
 module EsdlSuiteHelper
   def stub_esdl_suite_open_id_methods(valid_nonce: true, valid_code: true)
+    setup_app_config
+
     stub_discovery
     stub_access_token(valid_code)
     stub_id_token(valid_nonce)
@@ -14,7 +16,7 @@ module EsdlSuiteHelper
   end
 
   def provider_url
-    'https://idm.hesi.energy/auth/realms/esdl-mapeditor'
+    'https://esdl-suite/auth/'
   end
 
   def redirect_url
@@ -26,6 +28,13 @@ module EsdlSuiteHelper
   end
 
   private
+
+  def setup_app_config
+    allow(APP_CONFIG).to receive(:[]).with(:esdl_suite_url).and_return(provider_url)
+    allow(APP_CONFIG).to receive(:[]).with(:esdl_suite_client_id).and_return(client_id)
+    allow(APP_CONFIG).to receive(:[]).with(:esdl_suite_client_secret).and_return('secret')
+    allow(APP_CONFIG).to receive(:[]).with(:esdl_suite_redirect_url).and_return(redirect_url)
+  end
 
   def stub_discovery
     allow(OpenIDConnect::Discovery::Provider::Config)

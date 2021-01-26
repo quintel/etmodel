quintel/etmodel

commit 597dc71bddeaa632c44402876157436cbf7d17df

Remove ES6 code from assets pipeline. Again.

app/assets/javascripts/lib/views/input_element_view.js
@@ -217,8 +217,8 @@
    * sorted array of spec groups and attributes.
    */
   var specsToList = function (data) {
-    var groups = _.sortBy(Object.entries(data), ([key], index) => {
-      switch (key) {
+    var groups = _.sortBy(Object.entries(data), function (group, index) {
+      switch (group.key) {
         case 'technical':
           return 0;
         case 'cost':

commit a3b362d6bcfeb73b23c3c929063deb8f0a848913

Fix sorting of groups in node properties table.
I broke it in 597dc71.

Fixes #3569

app/assets/javascripts/lib/views/input_element_view.js
@@ -218,7 +218,7 @@
    */
   var specsToList = function (data) {
     var groups = _.sortBy(Object.entries(data), function (group, index) {
-      switch (group.key) {
+      switch (group[0]) {
         case 'technical':
           return 0;
         case 'cost':
0ade674ec8ac353a818b7e3bb8cfa4a0a2750094
Anthony Williams <hi@antw.dev>
2021-01-26 16:42:31 +0000

    Add support for demand and production per sector chart
    This re-uses the solar curtailment chart to allow toggling which series
    is shown. By default the chart will populate the list of selectable
    series based on the series in the chart, optionally grouped according
    to their `group` attribute.
    
    The chart may define a custom `config.series_selections` option
    overriding this behaviour, which the specified options being used
    instead.
    
    Ref #3559

--- app/assets/javascripts/d3/d3_chart_serie_select.js
+++ app/assets/javascripts/d3/d3_chart_serie_select.js
@@ -1,26 +1,83 @@
-var D3ChartSerieSelect = (function () {
+/* globals _ $ I18n */
 
-  function createOptions(options){
-    return options.map(function(option) {
-      return '<option value=' + option + '>' +
-      I18n.t("output_element_series.labels." + option) +
-      '</option>';
+var D3ChartSerieSelect = (function () {
+  /**
+   * Takes an array of options and partitions them into two arrays.
+   *
+   * The first array contains the collection of all options which do not belong to a group. The
+   * second contains two-element tuples, the first element being the localized group name, and the
+   * second the sorted list of options belonging to that group.
+   */
+  function partitionOptionsByGroup(options) {
+    const grouped = [];
+    const ungrouped = [];
+
+    options.forEach(function (option) {
+      (option.group ? grouped : ungrouped).push(option);
     });
+
+    let groupedCollection = Object.entries(
+      _.groupBy(grouped, function (option) {
+        return I18n.t('output_element_series.groups.' + option.group);
+      })
+    );
+
+    groupedCollection.sort(function (a, b) {
+      return a[0].localeCompare(b[0]);
+    });
+
+    return [ungrouped, groupedCollection];
   }
 
-  function buildSelectBox(options, updateChart){
-    return $("<select/>")
-      .addClass("d3-chart-date-select")
+  function buildOption(option) {
+    return (
+      '<option value=' +
+      option.match +
+      '>' +
+      (option.name || I18n.t('output_element_series.labels.' + option.match)) +
+      '</option>'
+    );
+  }
+
+  function createOptions(options) {
+    var partitioned = partitionOptionsByGroup(options);
+    var elements = [];
+
+    partitioned[0].forEach(function (option) {
+      elements.push(buildOption(option));
+    });
+
+    partitioned[1].forEach(function (group) {
+      var groupName = group[0];
+      var groupOptions = group[1];
+
+      var optgroup = $('<optgroup />').attr('label', groupName);
+
+      groupOptions.forEach(function (option) {
+        optgroup.append(buildOption(option));
+      });
+
+      elements.push(optgroup);
+    });
+
+    return elements;
+  }
+
+  function buildSelectBox(options, updateChart) {
+    return $('<select/>')
+      .addClass('d3-chart-select')
+      .addClass('d3-chart-serie-select')
       .append(createOptions(options))
       .on('change', updateChart);
   }
 
   D3ChartSerieSelect.prototype = {
     selectBox: undefined,
+
     draw: function (updateChart) {
       this.selectBox = buildSelectBox(this.options, updateChart);
       this.scope.append(this.selectBox);
-    }
+    },
   };
 
   function D3ChartSerieSelect(scope, options) {
@@ -29,4 +86,4 @@ var D3ChartSerieSelect = (function () {
   }
 
   return D3ChartSerieSelect;
-}());
+})();

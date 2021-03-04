9a9d35451e92a90655a5ad7f73ebf3c47e54bac2
Anthony Williams <hi@antw.dev>
2021-03-04 13:04:12 +0000

    Change some survey columns to be TEXT
    MySQL (5) was complaining about the row size exceeding 64KB.

--- db/migrate/20210222105558_create_surveys.rb
+++ db/migrate/20210222105558_create_surveys.rb
@@ -6,10 +6,10 @@ class CreateSurveys < ActiveRecord::Migration[6.0]
       # Answers
       t.string  :background, limit: 256
       t.integer :how_often
-      t.string  :typical_tasks, limit: 8192
+      t.text    :typical_tasks, limit: 8192
       t.integer :how_easy
       t.integer :how_useful
-      t.string  :feedback, limit: 8192
+      t.text    :feedback, limit: 8192
 
       t.timestamps
     end

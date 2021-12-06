7ba5d93ee6854924ba92bc6b21747e56db3b2cf5
Anthony Williams <hi@antw.dev>
2021-12-06 17:02:03 +0000

    Update deployment with docker and related configs

--- config/initializers/action_mailer_config.rb
+++ config/initializers/action_mailer_config.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-if (Rails.env.staging? || Rails.env.production?)
+if Rails.env.staging? || Rails.env.production?
+  # Old Capistrano deployments.
   if ENV['MAILER_SMTP_SETTINGS'].present?
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = JSON.parse(ENV['MAILER_SMTP_SETTINGS']).symbolize_keys
     ActionMailer::Base.default_url_options = { host: ENV['MAILER_HOST'] }
     ActionMailer::Base.asset_host = ENV['MAILER_HOST']
   elsif File.file?(Rails.root.join('config/email.yml'))
+    # New Docker-based deployments.
     email_settings = YAML.safe_load(File.open(Rails.root.join('config/email.yml')))
 
     if email_settings[Rails.env].present?

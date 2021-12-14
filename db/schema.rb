bd9a21d8ae30c42ce66549e2cb38038f1bb7dd0d
Nora Schinkel <ncschinkel@gmail.com>
2021-12-14 10:00:11 +0100

    Update area codes of SavedScenarios to the improved EU dataset area codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_120247) do
+ActiveRecord::Schema.define(version: 2021_12_14_085738) do
 
   create_table "area_dependencies", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "dependent_on"

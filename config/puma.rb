3a02e64c65265fbccbeb0623555cfadb639c471c
Anthony Williams <hi@antw.dev>
2021-12-15 16:55:24 +0000

    Use Puma in Cluster mode
    ETModel is not currently threadsafe

--- config/puma.rb
+++ config/puma.rb
@@ -6,8 +6,8 @@
 # thread pools should be configured to match the maximum value specified for Puma. Default is set to
 # 5 threads for minimum and maximum; this matches the default thread size of Active Record.
 #
-# ETEngine is not thread-safe.
-# threads 5, 5
+# ETModel is not thread-safe due to `Current` and similar classes.
+threads 1, 1
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch('PORT', 3000)
@@ -28,8 +28,7 @@ workers ENV.fetch('WEB_CONCURRENCY', 0)
 # Use the `preload_app!` method when specifying a `workers` number. This directive tells Puma to
 # first boot the application and load code before forking the application. This takes advantage of
 # Copy On Write process behavior so workers use less memory.
-#
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart

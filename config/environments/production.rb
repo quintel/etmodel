5807bb4c401248c0950950ae3870b80d77ca7fe2
Anthony Williams <hi@antw.dev>
2021-12-16 13:34:47 +0000

    Configure production memcached server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store
+  config.cache_store = :mem_cache_store, ENV['MEMCACHE_SERVERS']
 
   # Used to generate URLs back to the app.
   config.action_mailer.default_url_options = {

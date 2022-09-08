d25495aea0b198dcaf46a6d6b6e054e4e6860a29
Anthony Williams <hi@antw.dev>
2022-09-08 12:22:25 +0100

    Allow MYC to fetch slide data from the browser

--- app/controllers/input_elements_controller.rb
+++ app/controllers/input_elements_controller.rb
@@ -2,6 +2,8 @@
 
 # A controller for the retrieving the input_element resource.
 class InputElementsController < ApplicationController
+  before_action :myc_content_security_policy, only: :by_slide
+
   # Responds with a JSON array containing the name and key of each slide which
   # has at least one slider, and belongs to a sidebar item and tab. The slide
   # information also includes a brief summary of each slider.
@@ -9,7 +11,25 @@ class InputElementsController < ApplicationController
   # GET /input_elements/by_slide
   def by_slide
     # Include only sliders which are visible in the UI.
-    slides = Slide.ordered.select(&:visible_with_inputs?)
-    render(json: SlidePresenter.collection(slides))
+    json = Rails.cache.fetch("input_elements/by_slide?locale=#{I18n.locale}") do
+      slides = Slide.ordered.select(&:visible_with_inputs?)
+      SlidePresenter.collection(slides).to_json
+    end
+
+    render(json:)
+  end
+
+  private
+
+  # Internal: For requests originating in the "multi-year charts" application, we must loading the
+  # input elements from the same domain as the application.
+  def myc_content_security_policy
+    url = Settings.multi_year_charts_url
+
+    return unless url
+
+    response.set_header('Access-Control-Allow-Origin', url)
+    response.set_header('Access-Control-Allow-Methods', 'GET')
+    response.set_header('Access-Control-Allow-Headers', 'Accept, Accept-Language, Content-Type')
   end
 end

959a89547c0d7ddda2000301c04e21446cf8de02
Anthony Williams <hi@antw.dev>
2022-10-25 17:39:50 +0100

    Improve contact us mailer
    - Brings in some of the templating previously used by the feedback form.
    - Adds some basic information about the locale and user agent.
    - Prefills the user name and e-mail when signed in.
    - Hides the contact page if no `feedback_url` is configured.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,6 +2,7 @@
 
 # Shows the contact form, and sends messages to the e-mail address.
 class ContactController < ApplicationController
+  before_action :require_feedback_email
   skip_before_action :initialize_current
 
   invisible_captcha(
@@ -11,14 +12,18 @@ class ContactController < ApplicationController
   )
 
   def index
-    @message = ContactUsMessage.new
+    @message = ContactUsMessage.new(name: current_user&.name, email: current_user&.email)
   end
 
   def send_message
     @message = ContactUsMessage.from_params(params)
 
     if @message.valid?
-      ContactUsMailer.contact_email(@message).deliver
+      ContactUsMailer.contact_email(
+        @message,
+        locale: I18n.locale,
+        user_agent: request.env['HTTP_USER_AGENT']
+      ).deliver
 
       flash[:notice] = t('pages.contact.success_flash')
       redirect_to contact_url
@@ -28,6 +33,10 @@ class ContactController < ApplicationController
   end
 
   private
+
+  def require_feedback_email
+    redirect_to(root_url) unless Settings.feedback_email
+  end
 
   def send_feedback_spam
     @message = ContactUsMessage.from_params(params)
--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -2,12 +2,14 @@
 
 # Notifies quintel of contact being sent
 class ContactUsMailer < ActionMailer::Base
-  def contact_email(message)
+  def contact_email(message, locale: nil, user_agent: nil)
     @message = message
+    @locale = locale
+    @user_agent = user_agent
 
     mail(
-      to: 'info@energytransitionmodel.com',
-      from: message.email,
+      to: Settings.feedback_email,
+      from: Settings.feedback_email,
       reply_to: message.email,
       subject: 'ETM Feedback'
     )

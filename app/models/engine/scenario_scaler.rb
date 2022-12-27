aa3976af14f24ce103a8ae7c709d37973e4cdaef
Anthony Williams <hi@antw.dev>
2022-12-27 19:32:28 +0000

    Allow scenario scaling values to be coerced to floats

--- app/models/engine/scenario_scaler.rb
+++ app/models/engine/scenario_scaler.rb
@@ -5,8 +5,8 @@ module Engine
     transform_keys(&:to_sym)
 
     attribute :area_attribute,  Dry::Types['strict.string']
-    attribute :value,           Dry::Types['strict.integer']
-    attribute :base_value,      Dry::Types['strict.integer']
+    attribute :value,           Dry::Types['coercible.float']
+    attribute :base_value,      Dry::Types['coercible.float']
     attribute :has_agriculture, Dry::Types['strict.bool']
     attribute :has_industry,    Dry::Types['strict.bool']
     attribute :has_energy,      Dry::Types['strict.bool']

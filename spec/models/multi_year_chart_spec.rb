e575debb20b0bb4f8817319d678c95935cb0e69f
noracato <ncschinkel@gmail.com>
2023-03-24 15:06:03 +0100

    Destroy discarded MYC after 60 days

--- spec/models/multi_year_chart_spec.rb
+++ spec/models/multi_year_chart_spec.rb
@@ -3,5 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe MultiYearChart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.destroy_old_discarded!' do
+    it 'does not destroy a myc which is not discarded' do
+      myc = FactoryBot.create(:multi_year_chart)
+
+      expect { described_class.destroy_old_discarded! }
+        .not_to change { described_class.exists?(myc.id) }
+        .from(true)
+    end
+
+    it 'does not destroy a recently discarded myc' do
+      myc = FactoryBot.create(:multi_year_chart, discarded_at: Time.zone.now)
+
+      expect { described_class.destroy_old_discarded! }
+        .not_to change { described_class.exists?(myc.id) }
+        .from(true)
+    end
+
+    it 'does not delete a discarded myc on the threshold of being old' do
+      myc = FactoryBot.create(
+        :multi_year_chart,
+        discarded_at: (MultiYearChart::AUTO_DELETES_AFTER - 10.seconds).ago
+      )
+
+      expect { described_class.destroy_old_discarded! }
+        .not_to change { described_class.exists?(myc.id) }
+        .from(true)
+    end
+
+    it 'destroys an old discarded myc' do
+      myc = FactoryBot.create(
+        :multi_year_chart,
+        discarded_at: (MultiYearChart::AUTO_DELETES_AFTER + 10.seconds).ago
+      )
+
+      expect { described_class.destroy_old_discarded! }
+        .to change { described_class.exists?(myc.id) }
+        .from(true).to(false)
+    end
+  end
 end

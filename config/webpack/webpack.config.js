5636cccb0dfb21b50de389a802af919fb3d5d396
noracato <ncschinkel@gmail.com>
2023-06-20 16:47:12 +0200

    Packing just the js

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,11 +1,21 @@
 const path    = require("path")
 const webpack = require("webpack")
 
+
+// // Extracts CSS into .css file
+// // TODO: only do this in production
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// // Removes exported JavaScript files from CSS-only entries
+// // in this example, entry.custom will create a corresponding empty custom.js file
+// const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
+
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    app: "./app/javascript/packs/app.ts",
+    survey: "./app/javascript/packs/survey.ts"
   },
   output: {
     filename: "[name].js",
@@ -26,44 +36,23 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|eot|woff2|woff|ttf|svg)$/i,
+        type: 'asset/resource',
         use: 'file-loader',
       },
-    ],
-  },
-}
-
-// Extracts CSS into .css file
-// TODO: only do this in production
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// Removes exported JavaScript files from CSS-only entries
-// in this example, entry.custom will create a corresponding empty custom.js file
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-
-module.exports = {
-  entry: {
-    // add your css or sass entries
-    application: [
-      './app/assets/javascripts/application.js',
-      './app/assets/stylesheets/application.css.sass',
-    ],
-    // custom: './app/assets/stylesheets/custom.scss',
-  },
-  module: {
-    rules: [
-      // Add CSS/SASS/SCSS rule with loaders
-      {
-        test: /\.(?:sa|sc|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
+      // // Add CSS/SASS/SCSS rule with loaders
+      // {
+      //   test: /\.(?:sa|sc|c)ss$/i,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      // },
     ],
   },
   resolve: {
     // Add additional file types
-    extensions: ['.js', '.ts', '.tsx','.sass', '.css'],
+    extensions: ['.js', '.ts', '.tsx',],//'.sass', '.css'],
   },
-  plugins: [
-    // Include plugins
-    new RemoveEmptyScriptsPlugin(),
-    new MiniCssExtractPlugin(),
-  ],
-};
+  // plugins: [
+  //   // Include plugins
+  //   new RemoveEmptyScriptsPlugin(),
+  //   new MiniCssExtractPlugin(),
+  // ],
+}

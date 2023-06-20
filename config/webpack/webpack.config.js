5e0f1f8a42528cdd03be5aafef27d64d4681bf2f
noracato <ncschinkel@gmail.com>
2023-06-20 15:09:11 +0200

    New yarn dependencies

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -16,5 +16,54 @@ module.exports = {
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx|)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|eot|woff2|woff|ttf|svg)$/i,
+        use: 'file-loader',
+      },
+    ],
+  },
 }
+
+// Extracts CSS into .css file
+// TODO: only do this in production
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+// in this example, entry.custom will create a corresponding empty custom.js file
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
+module.exports = {
+  entry: {
+    // add your css or sass entries
+    application: [
+      './app/assets/javascripts/application.js',
+      './app/assets/stylesheets/application.css.sass',
+    ],
+    // custom: './app/assets/stylesheets/custom.scss',
+  },
+  module: {
+    rules: [
+      // Add CSS/SASS/SCSS rule with loaders
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    // Add additional file types
+    extensions: ['.js', '.ts', '.tsx','.sass', '.css'],
+  },
+  plugins: [
+    // Include plugins
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};

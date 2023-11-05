5c428446a7ccd670870fe54e8fc07034a3de86ec
Thomas <thomas.hassing@quintel.com>
2023-11-05 22:31:04 +0100

    More test fixing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe User do
   it { is_expected.to validate_presence_of(:name) }
 
-  it { is_expected.to have_many(:saved_scenarios).dependent(:destroy) }
+  it { is_expected.to have_many(:saved_scenarios).through(:saved_scenario_users) }
   it { is_expected.to have_many(:multi_year_charts).dependent(:destroy) }
   it { is_expected.to have_one(:esdl_suite_id).dependent(:destroy) }
   it { is_expected.to have_one(:survey).dependent(:destroy) }

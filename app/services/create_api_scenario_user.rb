551a2ba8400accd53dcedd4298fb443130c1510e
Thomas <thomas.hassing@quintel.com>
2023-11-21 08:30:49 +0100

    Add invitation arguments to (Saved)ScenarioUser synchronisation

--- app/services/create_api_scenario_user.rb
+++ app/services/create_api_scenario_user.rb
@@ -4,20 +4,19 @@
 class CreateAPIScenarioUser
   include Service
 
-  # Creates a ScenarioUser for multiple Scenarios.
-  def self.call_with_ids(http_client, ids, scenario_user)
-    ids.each { |id| call(http_client, id, scenario_user) }
-  end
-
-  def initialize(http_client, id, scenario_user)
+  def initialize(http_client, id, scenario_user, invitation_args = nil)
     @http_client = http_client
     @id = id
     @scenario_user = scenario_user
+    @invitation_args = invitation_args
   end
 
   def call
     ServiceResult.success(
-      @http_client.post("/api/v3/scenarios/#{@id}/users", scenario_users: [@scenario_user]).body
+      @http_client.post(
+        "/api/v3/scenarios/#{@id}/users",
+        { scenario_users: [@scenario_user], invitation_args:  @invitation_args, origin: 'etmodel' }
+      ).body
     )
   rescue Faraday::ResourceNotFound
     ServiceResult.failure('Scenario not found')

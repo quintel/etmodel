0ea40e0dceec9f56de04b52f31ca883d0954db41
noracato <ncschinkel@gmail.com>
2024-06-14 13:36:36 +0200

    When user is badly coupled to scenario, show as nameless user

--- app/helpers/saved_scenario_user_helper.rb
+++ app/helpers/saved_scenario_user_helper.rb
@@ -9,4 +9,14 @@ module SavedScenarioUserHelper
   def only_owner?(saved_scenario, saved_scenario_user)
     saved_scenario.single_owner? && current_scenario_user?(saved_scenario_user)
   end
+
+  def display_user_name_for(saved_scenario_user)
+    if saved_scenario_user.user_email.present?
+      saved_scenario_user.user_email
+    elsif saved_scenario_user.user.present?
+      saved_scenario_user.user.name
+    else
+      t('scenario.users.nameless')
+    end
+  end
 end

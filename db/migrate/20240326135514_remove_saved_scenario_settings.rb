c0676e3676b20a0bf113cac70e188e15adec355c
noracato <ncschinkel@gmail.com>
2024-10-04 16:02:33 +0200

    We don't need migrations for an empty db

--- db/migrate/20240326135514_remove_saved_scenario_settings.rb
+++
@@ -1,6 +0,0 @@
-class RemoveSavedScenarioSettings < ActiveRecord::Migration[7.0]
-  def change
-    # This attribute has been deprectaed and unused for years, let's remove it
-    remove_column :saved_scenarios, :settings
-  end
-end

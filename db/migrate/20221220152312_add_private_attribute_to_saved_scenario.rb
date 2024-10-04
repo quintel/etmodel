8292e571d0e33754f1a7268e935e4c16d3eb2400
noracato <ncschinkel@gmail.com>
2024-10-04 16:02:33 +0200

    We don't need migrations for an empty db

--- db/migrate/20221220152312_add_private_attribute_to_saved_scenario.rb
+++
@@ -1,5 +0,0 @@
-class AddPrivateAttributeToSavedScenario < ActiveRecord::Migration[7.0]
-  def change
-    add_column :saved_scenarios, :private, :boolean, default: false, after: :end_year
-  end
-end
--- db/migrate/20231104110117_change_default_saved_scenario_user_id.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultSavedScenarioUserId < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :saved_scenarios, :user_id
-  end
-end

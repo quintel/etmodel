f1d9d53c724fe720b086301d05a2a16e032ec978
noracato <ncschinkel@gmail.com>
2024-12-13 17:05:02 +0100

    Remove old migrations

--- db/migrate/20220503145118_add_discarded_at_to_saved_scenarios.rb
+++
@@ -1,6 +0,0 @@
-class AddDiscardedAtToSavedScenarios < ActiveRecord::Migration[7.0]
-  def change
-    add_column :saved_scenarios, :discarded_at, :datetime
-    add_index :saved_scenarios, :discarded_at
-  end
-end

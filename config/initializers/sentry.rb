255e73f630837745ee727158ace552d1c9289983
noracato <ncschinkel@gmail.com>
2024-12-17 15:55:19 +0100

    Tag Sentry release

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,9 @@
 
 if Settings.sentry_dsn
   Sentry.init do |config|
+    # Set release version
+    config.release = 'stable.01'
+
     config.dsn = Settings.sentry_dsn
     config.enabled_environments = %w[production staging]
   end

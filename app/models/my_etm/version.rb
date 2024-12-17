3906330e917b321a275980ce270c2240d9ee2d16
noracato <ncschinkel@gmail.com>
2024-12-17 12:14:27 +0100

    Add Activeresource for versions to fetch from MyETM API

---
+++ app/models/my_etm/version.rb
@@ -0,0 +1,11 @@
+module MyEtm
+  class Version < ActiveResource::Base
+    self.site = "#{Settings.idp_url}/api/v1"
+
+    def self.all_other_versions
+      Rails.cache.fetch(:api_versions) do
+        find(:all).reject { |v| v.tag == ETModel::Version::TAG }
+      end
+    end
+  end
+end

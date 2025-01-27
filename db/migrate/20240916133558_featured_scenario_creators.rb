35bb083147e832294307599d7679025a00edf47d
noracato <ncschinkel@gmail.com>
2025-01-27 16:38:30 +0100

    Disable useless migrations

--- db/migrate/20240916133558_featured_scenario_creators.rb
+++ db/migrate/20240916133558_featured_scenario_creators.rb
@@ -1,15 +1,15 @@
 class FeaturedScenarioCreators < ActiveRecord::Migration[7.1]
   def up
     add_column :featured_scenarios, :featured_owner, :string
-    FeaturedScenario.reset_column_information
-    FeaturedScenario.all.each do |fs|
-      ss = fs.saved_scenario
-      if ss.owners.any?
-        fs.update_column(:featured_owner, ss.owners.first.user.name)
-      else
-        fs.update_column(:featured_owner, "Unknown owner")
-      end
-    end
+    # FeaturedScenario.reset_column_information
+    # FeaturedScenario.all.each do |fs|
+    #   ss = fs.saved_scenario
+    #   if ss.owners.any?
+    #     fs.update_column(:featured_owner, ss.owners.first.user.name)
+    #   else
+    #     fs.update_column(:featured_owner, "Unknown owner")
+    #   end
+    # end
   end
 
   def down

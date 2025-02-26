a294ac8ec5953ae957ba249a94eec68955761747
louispt1 <louisparkestalbot1@gmail.com>
2025-02-26 17:03:57 +0100

    Draw secret key base from env in cookie rotation policy

--- config/initializers/cookie_rotation.rb
+++ config/initializers/cookie_rotation.rb
@@ -6,7 +6,7 @@
 # See https://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#key-generator-digest-class-changing-to-use-sha256
 Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
   salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
-  secret_key_base = Rails.application.secrets.secret_key_base
+  secret_key_base = Rails.application.credentials.secret_key_base || ENV["SECRET_KEY_BASE"]
 
   key_generator = ActiveSupport::KeyGenerator.new(
     secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1

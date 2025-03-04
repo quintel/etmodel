b66267813f318f233b042c0230562f49d23e412c
noracato <ncschinkel@gmail.com>
2025-03-04 13:19:19 +0100

    Ignore prerelease in version dropdown

--- app/models/my_etm/version.rb
+++ app/models/my_etm/version.rb
@@ -1,12 +1,13 @@
 module MyEtm
   class Version < ActiveResource::Base
     self.site = "#{Settings.identity.issuer}/api/v1"
+    PRERELEASES = ['2025-01'].freeze
 
     def self.all_other_versions
       return [] if Settings.version == "beta" # Don't show versions in beta
 
       Rails.cache.fetch(:api_versions) do
-        find(:all).reject { |v| v.tag == ETModel::Version::TAG }
+        find(:all).reject { |v| v.tag == ETModel::Version::TAG || PRERELEASES.include?(v.tag) }
       end
     end
   end

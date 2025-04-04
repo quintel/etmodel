1dc73eaae068ab42647b23b533f07c1bbf045b0e
noracato <ncschinkel@gmail.com>
2025-04-04 14:58:44 +0200

    Remove prerelease of 2025-01

--- app/models/my_etm/version.rb
+++ app/models/my_etm/version.rb
@@ -1,13 +1,12 @@
 module MyEtm
   class Version < ActiveResource::Base
     self.site = "#{Settings.identity.issuer}/api/v1"
-    PRERELEASES = ['2025-01'].freeze
 
     def self.all_other_versions
       return [] if Settings.version == "beta" # Don't show versions in beta
 
       Rails.cache.fetch(:api_versions) do
-        find(:all).reject { |v| v.tag == ETModel::Version::TAG || PRERELEASES.include?(v.tag) }
+        find(:all).reject { |v| v.tag.to_s == ETModel::Version::TAG }
       end
     end
   end

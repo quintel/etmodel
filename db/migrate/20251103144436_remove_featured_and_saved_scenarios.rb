e9d8760aabe5afb6d902d5458248658111c79f8c
Nora Schinkel <ncschinkel@gmail.com>
2025-11-03 16:28:35 +0100

    Actually drop the tables

--- db/migrate/20251103144436_remove_featured_and_saved_scenarios.rb
+++ db/migrate/20251103144436_remove_featured_and_saved_scenarios.rb
@@ -1,4 +1,11 @@
 class RemoveFeaturedAndSavedScenarios < ActiveRecord::Migration[7.1]
-  def change
+  def up
+    drop_table :featured_scenarios
+    drop_table :saved_scenarios
+    drop_table :saved_scenario_users
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end

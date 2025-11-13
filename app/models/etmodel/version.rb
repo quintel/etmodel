afbc3aacdbb83c26b480ef6163aa9a1f2e4c9aaf
louispt1 <81332401+louispt1@users.noreply.github.com>
2025-11-13 10:34:09 +0100

    Update version to 2025.11.13

--- app/models/etmodel/version.rb
+++ app/models/etmodel/version.rb
@@ -9,12 +9,12 @@ module ETModel
     MAJOR = 2025
 
     # Month of release.
-    MINOR = 10
+    MINOR = 11
 
     # Day of release. This is not used in version numbers but is required to build the DATE_VERSION
     # so that we know the exact date. This should be the day on which the minor version was released,
     # not incremented for each small change.
-    DAY = 2
+    DAY = 13
 
     STRING = format('%<major>d.%<minor>02d', major: MAJOR, minor: MINOR)
     DATE = Date.new(MAJOR, MINOR, DAY)

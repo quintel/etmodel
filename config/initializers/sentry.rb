c0bb3acd3e661d10e0ced55739c818462031d30a
aaccensi <andreu.accensi@gmail.com>
2026-01-20 14:39:38 +0100

    Add Sentry JS profiling.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 if Settings.sentry_dsn
   Sentry.init do |config|
     # Set release version
-    config.release = 'stable.01'
+    config.release = Settings.version
 
     config.dsn = Settings.sentry_dsn
     config.enabled_environments = %w[production staging]

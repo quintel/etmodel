7b1201a15997b9862cb5c2fc495267fa4b37a4a0
aaccensi <30655366+aaccensi@users.noreply.github.com>
2026-03-18 10:07:35 +0100

    Retire energy mix entirely and replace scenario report by retire notice. (#4668)
    To respect the passed scenario id for old energy mix links both routes and controller still exist and cannot be deleted until the retire notice is also removed in the future.
    
    ---------
    
    Co-authored-by: Mathijs Bijkerk <mathijs.bijkerk@quintel.com>

--- app/controllers/energy_mix_controller.rb
+++ app/controllers/energy_mix_controller.rb
@@ -2,12 +2,16 @@
 
 # Controller for Over Morgens fabulous Energy Mix infographic
 class EnergyMixController < ApplicationController
-  layout 'energy_mix'
-
   def show
-    @scenario = FetchAPIScenario.call(engine_client, params[:id]).or do
+    scenario = FetchAPIScenario.call(engine_client, params[:id]).or do
       return render_not_found
     end
+
+    # Load the scenario into the session so the report page can query it.
+    Current.setting = Setting.load_from_scenario(scenario)
+    Current.setting.api_session_id = scenario.id
+
+    redirect_to report_path('main')
 
   rescue ActiveResource::ResourceNotFound
     # No such scenario.

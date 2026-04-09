8d3093ffbd7e92484ef60522fed9c7eec7de170d
robindesmit-quintel <robin.desmit@quintel.com>
2026-04-09 10:41:08 +0200

    Adapting locales to move biomass emissions

--- db/cache_schema.rb
+++ db/cache_schema.rb
@@ -10,15 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 1) do
-  create_table "solid_cache_entries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "byte_size", null: false
-    t.datetime "created_at", null: false
-    t.binary "key", limit: 1024, null: false
-    t.bigint "key_hash", null: false
-    t.binary "value", size: :long, null: false
-    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
-  end
+ActiveRecord::Schema[8.1].define(version: 0) do
 end

cdaca845f3f8af57346293cced5c0952a4e661f4
aaccensi <andreu.accensi@gmail.com>
2026-05-12 12:08:04 +0200

    Remove Sentry browser profiling

--- app/javascript/sentry.ts
+++ app/javascript/sentry.ts
@@ -3,7 +3,6 @@ import * as Sentry from '@sentry/browser';
 declare const globals: {
   sentry_dsn?: string;
   sentry_traces?: number;
-  sentry_profiles?: number;
   release?: string;
   env?: string;
 };
@@ -17,16 +16,9 @@ if (globals && globals.sentry_dsn && enabledEnvs.includes(globals.env)) {
     environment: globals.env,
     integrations: [
       Sentry.browserTracingIntegration(),
-      Sentry.browserProfilingIntegration(),
     ],
 
     // Percentage of transactions to capture for tracing
     tracesSampleRate: globals.sentry_traces,
-
-    // Percentage of sampled transactions to profile
-    profileSessionSampleRate: globals.sentry_profiles,
-
-    // Automatically start/stop profiler based on tracing spans
-    profileLifecycle: 'trace',
   });
 }

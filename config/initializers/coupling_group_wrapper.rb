37418e982a6ae3e116ef1b811145a306d8664cb3
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2026-06-25 17:15:35 +0200

    Change liquid fuels model name

--- config/initializers/coupling_group_wrapper.rb
+++ config/initializers/coupling_group_wrapper.rb
@@ -1,5 +1,5 @@
 COUPLING_GROUP_LABELS = {
   'external_model_industry' => 'Carbon Transition Model',
   'external_model_interconnectors' => 'Plexos',
-  'external_model_liquid_fuels'=> 'Liquid Fuels Extension'
+  'external_model_liquid_fuels'=> 'Liquid Fuels detailed settings'
 }.freeze
